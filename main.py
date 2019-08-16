53250c4cb77a4638165389a91e66bacbb11c2bf1
Alex Tucker <alex@floop.org.uk>
2019-08-16 18:03:25 +0100

    Extract three datasets, parsing the label of the slice to figure out the dimensions.

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ dist
 # +
 from io import TextIOWrapper
 from itertools import accumulate, zip_longest
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 from decimal import *
 
 def make_parser(fieldwidths):
@@ -75,7 +75,7 @@ struct = None
 label = None
 observations = []
 
-slices = {}
+slices = defaultdict(list)
 
 while line:
     line_type = line[0:2]
@@ -87,7 +87,7 @@ while line:
                 'struct': struct,
                 'observations': observations
             }
-            slices[header.identifier] = slice
+            slices[header.identifier].append(slice)
         observations = []
         header = TableHeader._make(parse_92(line))
     elif line_type == '93':
@@ -103,12 +103,13 @@ while line:
     else:
         break
     line = data.readline()[:-1]
-slices[header.identifier] = {
+
+slices[header.identifier].append({
     'label': label,
     'header': header,
     'struct': struct,
     'observations': observations
-}
+})
 
 # +
 AV_STERLING_EXCHANGE = 'Average Sterling exchange rate: '
@@ -118,107 +119,143 @@ COUNTRIES = set(['estonia', 'ww', 'cyprus', 'latvia', 'lithuania', 'malta', 'slo
                 'hungary', 'czech-republic', 'kuwait', 'eu', 'non-eu',
                 'poland', 'singapore', 'russia', 'indonesia', 'extra-eu-27',
                 'total-eu-27', 'saudi-arabia', 'croatia', 'italy', 'israel',
-                 'south-africa', 'taiwan', 'india', 'united-states-inc-puerto-rico', 'turkey',
+                'south-africa', 'taiwan', 'india', 'united-states-inc-puerto-rico', 'turkey',
                 'mexico', 'switzerland', 'hong-kong', 'ww',
                 'philippines', 'pakistan', 'brazil', 'emu-19', 'bulgaria', 'romania', 'iceland',
                  'australia', 'thailand', 'united-arab-emirates', 'new-zealand', 'greece', 'egypt', 'rw'])
 DIRECTION = set(['imports', 'exports', 'balance', 'ex', 'im', 'bal', 'terms-of-trade'])
 BASIS = set(['bop'])
 ADJUST = set(['nsa', 'sa'])
-GOODS = set(['total', 'food-live-animals-0', 'meat-meat-preparations-01', 'unspecified-goods-9'])
 CP = set(['cp', 'cvm', 'tons', 'idef', 'av-value-per-ton'])
 TIG = 'Trade in Goods'
 TT = 'total trade'
 
-#TradeInProduct = namedtuple('TradeInProduct', 'country, product, direction, basis, cp, adjustment')
+TradeInProduct = namedtuple('TradeInProduct', 'country, product, direction, basis, cp, adjustment')
+SterlingEffectiveERI = namedtuple('SterlingEffectiveERI', 'measure base')
+ExchangeRate = namedtuple('ExchangeRate', 'currency')
+import re
+ERI_RE = re.compile(r'Sterling effective exchange rate index: ([^(]*) \(([^)]*)\)')
+
+sitc = {}
+currency_label = {}
+country_label = {}
+product_label = {}
 
 def dim_val(t):
     if t.startswith(TIG) and t[len(TIG)+1] != ':':
         t = t[0:len(TIG)] + ':' + t[len(TIG)+1:]
     elif t.lower().startswith(TT):
         t = TIG + ':' + t
-    dims = [pathify(d.strip()) for d in t.split(':')]
+    labels = [d.strip() for d in t.split(':')]
+    dims = [pathify(l) for l in labels]
     if dims[0] == 'average-sterling-exchange-rate':
-        return AverageExchange({'average-sterling-exchange-rate': dims[1]}
+        currency_label[dims[1]] = labels[1]
+        return ExchangeRate(dims[1])
     elif dims[0] == 'sterling-effective-exchange-rate-index':
-        return {'sterling-effective-exchange-rate-index': dims[1]}
+        match = ERI_RE.match(t)
+        if match:
+            return SterlingEffectiveERI(match.group(1), match.group(2))
+        raise(Exception(t))
     elif dims[0] == 'trade-in-goods':
         if dims[1] in COUNTRIES and dims[3] in DIRECTION and dims[4] in BASIS and dims[5] in CP and dims[6] in ADJUST:
-                'country': dims[1],
-                'product': 'total-goods' if dims[2] == 'total' else dims[2],
-                'direction': dims[3],
-                'basis': dims[4],
-                'cp': dims[5],
-                'adjustment': dims[6]
-            }
+            country_label[dims[1]] = labels[1]
+            product = 'total-goods' if dims[2] == 'total' else dims[2]
+            product_label[product] = labels[2]
+            return TradeInProduct(
+                country=dims[1], product='total-goods' if dims[2] == 'total' else dims[2],
+                direction=dims[3], basis=dims[4], cp=dims[5], adjustment=dims[6])
         elif dims[2] in COUNTRIES and dims[3] in DIRECTION and dims[4] in BASIS and dims[5] in CP and dims[6] in ADJUST:
-            return {
-                'country': dims[2],
-                'product': 'total-goods' if dims[1] == 'total' else dims[1],
-                'direction': dims[3],
-                'basis': dims[4],
-                'cp': dims[5],
-                'adjustment': dims[6]
-            }
+            country_label[dims[2]] = labels[2]
+            product = 'total-goods' if dims[1] == 'total' else dims[1]
+            product_label[product] = labels[1]
+            return TradeInProduct(
+                country=dims[2], product='total-goods' if dims[1] == 'total' else dims[1],
+                direction=dims[3], basis=dims[4], cp=dims[5], adjustment=dims[6])
         # ['trade-in-goods', 'emu-19', 'balance', 'bop', 'cp', 'sa']
         elif dims[1] in COUNTRIES and dims[2] in DIRECTION and dims[3] in BASIS and dims[4] in CP and dims[5] in ADJUST:
-            return {
-                'country': dims[1],
-                'product': 'total-goods',
-                'direction': dims[2],
-                'basis': dims[3],
-                'cp': dims[4],
-                'adjustment': dims[5]
-            }            
+            country_label[dims[1]] = label[1]
+            return TradeInProduct(
+                country=dims[1], product='total-goods', direction=dims[2],
+                basis=dims[3], cp=dims[4], adjustment=dims[5])
         else:
             raise(Exception(dims))
     elif dims[0] == 'trade-in-goods-t' and dims[1] in COUNTRIES and dims[2] in DIRECTION and dims[3] in BASIS and dims[4] in CP and dims[5] in ADJUST:
-        return {
-            'country': dims[1],
-            'product': 'total-goods',
-            'direction': dims[2],
-            'basis': dims[3],
-            'cp': dims[4],
-            'adjustment': dims[5]
-        }
+        country_label[dims[1]] = label[1]
+        return TradeInProduct(
+            country=dims[1], product='total-goods', direction=dims[2],
+            basis=dims[3], cp=dims[4], adjustment=dims[5])
     elif dims[0] == 'trade-in-services-ts' and dims[1] == 'ww' and dims[2] in DIRECTION and dims[3] in BASIS and dims[4] in CP and dims[5] in ADJUST:
-        return {
-            'country': dims[1],
-            'product': 'total-services',
-            'direction': dims[2],
-            'basis': dims[3],
-            'cp': dims[4],
-            'adjustment': dims[5]
-        }
+        country_label[dims[1]] = label[1]
+        return TradeInProduct(
+            country=dims[1], product='total-services', direction=dims[2], basis=dims[3],
+            cp=dims[4], adjustment=dims[5])
     elif dims[0] in set(['eu-2004', 'non-eu-2004']) and dims[1] in BASIS and dims[2] in DIRECTION and dims[5].startswith('sitc'):
-        return {
-            'country': dims[0],
-            'product': dims[4],
-            'direction': dims[2],
-            'basis': dims[1],
-            'adjustment': dims[3],
-            'sitc': dims[5]
-        }
+        country_label[dims[0]] = label[0]
+        product_label[dims[4]] = label[4]
+        sitc[dims[4]] = dims[5]
+        return TradeInProduct(
+            country=dims[0], product=dims[4], direction=dims[2], basis=dims[1], adjustment=dims[3], cp='cp')
     # ['bop', 'ex', 'sa', 'ships-and-aircraft-sna', 'sitc-792-793']
     elif dims[0] in BASIS and dims[1] in DIRECTION and dims[2] in ADJUST and dims[4].startswith('sitc'):
-        return {
-            'country': 'ww',
-            'product': dims[3],
-            'direction': dims[1],
-            'basis': dims[0],
-            'adjustment': dims[2],
-            'sitc': dims[4]
-        }        
+        sitc[dims[3]] = dims[4]
+        product_label[dims[3]] = label[3]
+        return TradeInProduct(
+            country='ww', product=dims[3], direction=dims[1], basis=dims[0], cp='cp',
+            adjustment=dims[2])
     else:
         raise(Exception(dims))
 
-for csid, s in slices.items():
-    dims = dim_val(s['label'])
-    
 
-set(s['dims']['product'] for csid, s in slices.items() if 'product' in s['dims'])
+
+# +
+TradeObservation = namedtuple('TradeObservation', ('period',) + TradeInProduct._fields + ('value',))
+SterlingEffectiveERIObservation = namedtuple('SterlingEffectiveERIObservation', 'period rate measure base')
+ExchangeRateObservation = namedtuple('ExchangeRateCountryObservation', 'period currency rate')
+
+product_data = []
+exchange_currency_data = []
+effective_exchange_data = []
+
+def period(periodicity, year_start, i):
+    if periodicity == 'A':
+        return f"year/{int(year_start) + i}"
+    elif periodicity == 'Q':
+        return f"quarter/{int(year_start) + (i // 4)}-Q{(i % 4) + 1}"
+    elif periodicity == 'M':
+        return f"month/{int(year_start) + (i // 12)}-{((i % 12) + 1):02}"
+
+for csid, slice_list in slices.items():
+    for s in slice_list:
+        for i, v in enumerate(s['observations']):
+            dim_vals = dim_val(s['label'])
+            p = period(s['header'].periodicity, s['struct'].year_start, i)
+            if isinstance(dim_vals, TradeInProduct):
+                product_data.append(TradeObservation._make(
+                    (p,) + dim_vals + (v, )))
+            elif isinstance(dim_vals, SterlingEffectiveERI):
+                effective_exchange_data.append(SterlingEffectiveERIObservation(
+                    p, v, dim_vals.measure, dim_vals.base))
+            elif isinstance(dim_vals, ExchangeRate):
+                exchange_currency_data.append(ExchangeRateObservation(
+                    p, dim_vals.currency, v))
+
+product_observations = pd.DataFrame(data=product_data)
+effective_eri_observations = pd.DataFrame(data=effective_exchange_data)
+exchange_currency_observations = pd.DataFrame(data=exchange_currency_data)
 # -
 
-slices['OKRM']
+product_observations.replace(
+    {'product': {'-t': 'total-goods'}}, inplace=True)
+
+# +
+out = Path('out')
+out.mkdir(exist_ok=True)
+
+product_observations.to_csv(out / 'product-observations.csv', index=False)
+effective_eri_observations.to_csv(out / 'effective-eri-observations.csv', index=False)
+exchange_currency_observations.to_csv(out / 'exchange-currency-observations.csv', index=False)
+# -
+
+currency_label
 
 
