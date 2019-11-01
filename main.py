74291b53369e28d72553a53ccdd500545992bb7c
LPerryman <leigh.perryman@gsscogs.uk>
2019-11-01 11:27:10 +0000

    add EU - Non-EU column to stop duplicates

--- main.py
+++ main.py
@@ -39,14 +39,13 @@ def process_tab(t):
 
 table = pd.concat(process_tab(f'{t}.py') for t in ['T1', 'T2', 'T3', 'T4', 'T5'])
 table.count()
-# + {}
+# -
 #sorted(table)
 #table = table[(table['Marker'] != 'residual-trade')]
 #table = table.drop_duplicates()
 #table.count()
 #t = table[(table['NUTS Geography'] == 'nuts2/ea-other') & (table['HMRC Partner Geography'] == 'C') & (table['Value'] == 127)]
-#t
-# -
+tidy =tidy[['Year', 'NUTS Geography','HMRC Partner Geography','Flow','SITC 4','Measure Type', 'Value', 'Unit','Marker']]
 
 
 out = Path('out')
