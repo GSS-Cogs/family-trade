8dde9e8c05abfd7994e7c146d22f69edbce84de8
Alex Tucker <alex@floop.org.uk>
2019-12-05 17:37:35 +0000

    Also output alternative CSVW/transform

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.4'
-#       jupytext_version: 1.2.4
+#       jupytext_version: 1.1.1
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
@@ -34,7 +34,8 @@ tables.append(new_table)
 from pathlib import Path
 out = Path('out')
 out.mkdir(exist_ok=True)
-pd.concat(tables).drop_duplicates().to_csv(out / 'observations.csv', index = False)
+tidy = pd.concat(tables).drop_duplicates()
+tidy.to_csv(out / 'observations.csv', index = False)
 
 # The metadata is more problematic; for now we just use the latter dataset's metadata.
 #
@@ -47,6 +48,16 @@ with open(out / 'dataset.trig', 'wb') as metadata:
      metadata.write(scraper.generate_trig())
 csvw = CSVWMetadata('https://gss-cogs.github.io/ref_trade/')
 csvw.create(out / 'observations.csv', out / 'observations.csv-schema.json')
+
+# Generate alternative output for direct CSVW transformation. Note that these files won't be included in the upload to PMD.
+
+tidy['Measure Type'] = tidy['Measure Type'].astype('category')
+tidy['Measure Type'].cat.categories = tidy['Measure Type'].cat.categories.map(
+    lambda x: pathify(x).replace('-', '_'))
+tidy.to_csv(out / 'observations.csv', index = False)
+csvw.create(out / 'observations.csv', out / 'observations.csv-metadata.json', with_transform=True,
+            base_url='http://gss-data.org.uk/data/', base_path='gss_data/trade/ons-uk-trade-in-services',
+            dataset_metadata=scraper.dataset.as_quads())
 
 
 
