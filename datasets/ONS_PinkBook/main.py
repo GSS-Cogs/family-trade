6770fa67a696017ce516ecef0c3234aa1c6fbcce
VTula2000 <36479963+VTula2000@users.noreply.github.com>
2020-04-06 09:31:04 +0100

    Code update and review

--- datasets/ONS_PinkBook/main.py
+++ datasets/ONS_PinkBook/main.py
@@ -25,38 +25,56 @@ tabs = { tab.name: tab for tab in scraper.distributions[0].as_databaker() }
 
 list(tabs)
 
+sheetname = ['3.2','3.3','3.4','3.5','3.6','3.7','3.8','3.9','3.10']
+
 next_table = pd.DataFrame()
+for sheet in sheetname:
+    tab = tabs[sheet]
+    cell = tab.excel_ref('B3')
+    code = cell.shift(1,0).fill(DOWN).is_not_blank().is_not_whitespace()
+    Year = cell.fill(RIGHT).is_not_blank().is_not_whitespace()            
+    observations = Year.fill(DOWN).is_not_blank().is_not_whitespace() 
+    Flow = cell.fill(DOWN).one_of(['Exports (Credits)', 'Imports (Debits)', 'Balances'])
+    Dimensions = [
+                HDimConst('Geography', 'K02000001'),
+                HDim(Year,'Year', DIRECTLY,ABOVE),
+                HDim(code,'CDID',DIRECTLY,LEFT),
+                HDimConst('Unit', 'gbp-miilion'), 
+                HDimConst('Measure Type','GBP Total'),
+                HDim(Flow,'Flow',CLOSEST, ABOVE)           
 
-# +
-# %%capture
+    ]
+    c1 = ConversionSegment(observations, Dimensions, processTIMEUNIT=True)
+    new_table = c1.topandas()
+    new_table['Year'] = new_table['Year'].map(lambda cell:cell.replace('.0', ''))
+    new_table['Year'] = new_table['Year'].str.strip()
+    new_table['Geography'] = new_table['Geography'].str.strip()
+    new_table['CDID'] = new_table['CDID'].str.strip()
+    new_table['Flow'] = new_table['Flow'].str.strip()
+    import numpy as np
+    new_table['OBS'].replace('', np.nan, inplace=True)
+    new_table.rename(index= str, columns={'OBS': 'Value'}, inplace=True)
+    new_table['Flow'] = new_table['Flow'].map(
+        lambda x: {
+            'Exports (Credits)': 'Exports',
+            'Imports (Debits)': 'Imports',
+            'Balances': 'Balance'}.get(x, x))
+    next_table = pd.concat([next_table, new_table])
 
-# %run "3.2.py"
-next_table = pd.concat([next_table, new_table])
-# %run "3.3.py"
-next_table = pd.concat([next_table, new_table])
-# %run "3.4.py"
-next_table = pd.concat([next_table, new_table])
-# %run "3.5.py"
-next_table = pd.concat([next_table, new_table])
-# %run "3.6.py"
-next_table = pd.concat([next_table, new_table])
-# %run "3.7.py"
-next_table = pd.concat([next_table, new_table])
-# %run "3.8.py"
-next_table = pd.concat([next_table, new_table])
-# %run "3.9.py"
-next_table = pd.concat([next_table, new_table])
-# %run "3.10.py"
-next_table = pd.concat([next_table, new_table])
-# -
+PBclassification_table_url = 'https://drive.google.com/uc?export=download&id=1uNwmZHgq7ERqD5wcND4W2sGHXRJyP2CR'
+temp_table = pd.read_excel(PBclassification_table_url, sheet_name = 0)
+next_table = pd.merge(next_table, temp_table, how = 'left', left_on = 'CDID', right_on = 'cdid')
+next_table.rename(index= str, columns= {'BPM6':'Pink Book Services'}, inplace = True)
 
-next_table['Unit'] = next_table['Unit'].map(
-    lambda x: { '£ Million' : 'gbp-million' 
-        }.get(x, x))
+next_table[next_table.cdid.isnull() == True]['CDID'].unique()
+
+next_table = next_table[['Geography','Year','CDID','Pink Book Services','Flow','Measure Type','Value','Unit','DATAMARKER']]
+
+next_table = next_table[next_table['Pink Book Services'].isnull() == False]
 
 next_table['Marker'] = next_table['DATAMARKER'].map(
     lambda x: { 'NA' : 'not-available' ,
-               '-' : 'nil-or-less-than-a-million'
+               ' -' : 'nil-or-less-than-a-million'
         }.get(x, x))
 
 next_table['Pink Book Services'] = next_table['Pink Book Services'].astype(str)
