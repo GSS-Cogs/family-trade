0b329328331efcbfed613ff73d531ccf2a2573d0
mikeAdams <mikelovesbooks@gmail.com>
2020-04-17 16:03:08 +0100

    first 100 rows, no Net Mass measure type

--- datasets/ONS_MRETS/main.py
+++ datasets/ONS_MRETS/main.py
@@ -387,7 +387,7 @@ for name, details in outputs.items():
     
     # TEMPORARY FOR DEBUGGING
     details["data"] = details["data"].drop("Decimals", axis=1).sample(n=100, random_state=1)
-    details["data"] = details["data"][details["data"]["Measure Type"] == "Current Price"]
+    details["data"] = details["data"][details["data"]["Measure Type"] != "Net Mass"]
     
     details["data"].drop_duplicates().to_csv(out / f'{OBS_ID}.csv', index = False)
 
