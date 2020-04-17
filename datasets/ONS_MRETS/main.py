0a79e3e23f5bc276a0543f60ab16788d788b8742
mikeAdams <mikelovesbooks@gmail.com>
2020-04-17 14:50:34 +0100

    drop decimals for now, 100 row sample

--- datasets/ONS_MRETS/main.py
+++ datasets/ONS_MRETS/main.py
@@ -385,6 +385,9 @@ for name, details in outputs.items():
     
     print(df["Measure Type"].unique())
     
+    # TEMPORARY FOR DEBUGGING - drop Decimals,100 row slice
+    details["data"] = details["data"].drop("Decimals", axis=1).sample(n=100, random_state=1)
+    
     details["data"].drop_duplicates().to_csv(out / f'{OBS_ID}.csv', index = False)
 
     scraper.dataset.family = 'trade'
