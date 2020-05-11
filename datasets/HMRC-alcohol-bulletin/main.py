cee132d840aea3b0fc1fc71dd47323e6eae43078
Shannon Black <shannon1995black@gmail.com>
2020-05-11 14:34:17 +0100

    fixes

--- datasets/HMRC-alcohol-bulletin/main.py
+++ datasets/HMRC-alcohol-bulletin/main.py
@@ -44,25 +44,31 @@ next_table = next_table.replace({'Measure Type' : { 'quantities-consumption' : '
 
 next_table = next_table.replace({'Alcohol Category' : { 'UK Beer Production' : 'total-beer', 'UK Alcohol Production' : 'total-alcohol-production',
                                                        'Alcohol Clearances' : 'total-alcohol-clearances', 'Cider Clearances' : 'total-cider-clearances', 
-                                                       'Still Wine' : 'still', 'Sparkling Wine' : 'sparkling', 'Ready-to-Drink ' : 'rtd', 'Spirits-Based RTDs' : 'spirit-based-rtds', 
-                                                       'Breweries Producing 5000 Hls Or Less' : 'breweries-5000-less'
+                                                       'Still Wine' : 'still', 'Sparkling Wine' : 'sparkling', 'Ready-to-Drink' : 'rtd', 'Spirits-Based RTDs' : 'spirit-based-rtds', 
+                                                       'Breweries Producing 5000 Hls Or Less' : 'breweries-5000-less', 'Still Cider' : 'Still', 'Sparkling Cider' : 'Sparkling',
+                                                       'Breweries Producing 5000 Hls Or Less2' : 'breweries-5000-less'
                               }})
 
-next_table = next_table.replace({'Alcohol Content' : { 'Various': 'all','Over 1.2%, up to and including 4.0%' : 'over-1-2-up-to-and-incl-4-0',
+next_table = next_table.replace({'Alcohol Content' : { 'Various': 'all', 'various': 'all','Over 1.2%, up to and including 4.0%' : 'over-1-2-up-to-and-incl-4-0',
                                                       'Over 4.0%, up to and including 5.5%' : 'over-4-0-up-to-and-incl-5-5',
                                                       'Over 5.5%, up to and including 15.0%' : 'over-5-5-up-to-and-incl-15-0',
-                                                      'Over 15.0%, up to and including 22.0%' : 'over-15-0-up-to-and-incl-22',
+                                                      'Over 15.0%, up to and including 22%' : 'over-15-0-up-to-and-incl-22',
                                                       'From 8.5%, up to and including 15.0%': 'from-8-5-up-to-and-incl-15-0',
-                                                      
+                                                      'Exceeding 1.2% but less than 6.9% abv' : 'exceeding-1-2-but-less-than-6-9-abv',
+                                                      'At least 6.9% but not exceeding 7.5% abv' : 'at-least-6-9-but-not-exceeding-7-5-abv',
+                                                      'Exceeding 7.5% but less than 8.5% abv' : 'exceeding-7-5-but-less-than-8-5-abv',
+                                                      'Exceeding 5.5% but less than 8.5% abv' : 'exceeding-5-5-but-less-than-8-5-abv'      
                                                       
                               }})
+
 
 
 # -
 
+next_table['Alcohol Content'].unique()
+
 next_table['Alcohol Category'] = next_table['Alcohol Category'].map(lambda x: pathify(x))
 next_table['Alcohol Content'] = next_table['Alcohol Content'].map(lambda x: pathify(x))
-next_table
 
 # +
 destinationFolder = Path('out')
