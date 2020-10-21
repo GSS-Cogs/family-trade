f42b32720cd3396ffa406d11c6189452d447b42d
Alex Tucker <alex@floop.org.uk>
2020-10-21 17:58:49 +0100

    Sync from Airtable. Update dataset table.

--- datasets/apply-templates.js
+++ datasets/apply-templates.js
@@ -1,6 +1,20 @@
-const dataset = window.location.search.substring(1);
+let dataset = window.location.search.substring(1);
+let spec = false;
+if (dataset.startsWith('spec=')) {
+    spec = true;
+    dataset = dataset.substring(5);
+}
 
 function datasetFetcher(endpoint, landingPages) {
+    let filteredURLs = landingPages.filter(p => {
+        try {
+            new URL(p);
+            return true;
+        } catch (err) {
+            console.warn(`Invalid landing page URL "${p}"`);
+            return false;
+        }
+    });
     return $.post({
         url: endpoint,
         data: {
@@ -14,7 +28,7 @@ SELECT DISTINCT ?page ?ds ?label ?modified WHERE {
     ?ds dct:modified ?modified
   }
 } VALUES (?page) {
-${landingPages.map(x => `(<${x}>)`).join(' ')}
+${filteredURLs.map(x => `(<${(new URL(x)).href}>)`).join(' ')}
 }`
         },
         dataType: 'json',
@@ -32,62 +46,80 @@ ${landingPages.map(x => `(<${x}>)`).join(' ')}
 };
 
 if (dataset) {
-    Handlebars.registerHelper('ifObject', function(item, options) {
+    Handlebars.registerHelper('ifObject', function (item, options) {
         if (typeof item === "object") {
             return options.fn(this);
         } else {
             return options.inverse(this);
         }
     });
-    $.get({
-        url: "etl.hbs",
-        dataType: "html"
-    }, function(source) {
-        const template = Handlebars.compile(source);
-        $.getJSON('info.json', function(mainInfo) {
-            $.getJSON(dataset + "/info.json", function (info) {
-                let fetchDatasets, lastPublished=null;
-                if (mainInfo.hasOwnProperty('sparql')) {
-                    fetchDatasets = datasetFetcher(mainInfo.sparql, [info.landingPage]);
-                } else {
-                    fetchDatasets = $.Deferred();
-                    fetchDatasets.resolve([]);
-                }
-                fetchDatasets.done(function(datasets) {
-                    if (mainInfo.hasOwnProperty('pmd')) {
-                        datasets = datasets.map(function(ds) {
-                            if ((ds.modified !== null) && ((lastPublished === null) || (lastPublished < ds.modified))) {
-                                lastPublished = ds.modified;
-                            }
-                            return {
-                                uri: mainInfo.pmd + '/resource?uri=' + encodeURIComponent(ds.uri),
-                                label: ds.label,
-                                modified: ds.modified
-                            }
-                        });
-                    }
+    if (spec) {
+        $.get({url: "spec.hbs", dataType: "html"}, function (source) {
+            const template = Handlebars.compile(source);
+            $.getJSON('info.json', function (mainInfo) {
+                let spec_url = mainInfo.jenkins.base + '/' + mainInfo.jenkins.path.map(function (p) {
+                    return 'job/' + p;
+                }).join('/') + dataset + '/lastBuild/artifact/datasets/' + dataset + '/out/spec.json';
+                $.getJSON(spec_url, function (spec_obj) {
                     $("#body").html(template({
                         "dataset_path": dataset,
                         "main": mainInfo,
-                        "dataset": info,
-                        "jenkins_path": mainInfo.jenkins.path.map(function (p) {
-                            return 'job/' + p;
-                        }).join('/'),
-                        "jenkins_buildicon": 'buildStatus/icon?job=' + encodeURIComponent(mainInfo.jenkins.path.join('/') + '/'),
-                        "datasets": datasets,
-                        "issue_badge_base": `https://img.shields.io/github/issues/detail/state${(new URL(mainInfo.github)).pathname}`
+                        "spec": spec_obj
                     }));
                 });
-                document.title = info.title;
             });
         });
-    });
+    } else {
+        $.get({
+            url: "etl.hbs",
+            dataType: "html"
+        }, function (source) {
+            const template = Handlebars.compile(source);
+            $.getJSON('info.json', function (mainInfo) {
+                $.getJSON(dataset + "/info.json", function (info) {
+                    let fetchDatasets, lastPublished = null;
+                    if (mainInfo.hasOwnProperty('sparql')) {
+                        fetchDatasets = datasetFetcher(mainInfo.sparql, [info.landingPage]);
+                    } else {
+                        fetchDatasets = $.Deferred();
+                        fetchDatasets.resolve([]);
+                    }
+                    fetchDatasets.done(function (datasets) {
+                        if (mainInfo.hasOwnProperty('pmd')) {
+                            datasets = datasets.map(function (ds) {
+                                if ((ds.modified !== null) && ((lastPublished === null) || (lastPublished < ds.modified))) {
+                                    lastPublished = ds.modified;
+                                }
+                                return {
+                                    uri: mainInfo.pmd + '/resource?uri=' + encodeURIComponent(ds.uri),
+                                    label: ds.label,
+                                    modified: ds.modified
+                                }
+                            });
+                        }
+                        $("#body").html(template({
+                            "dataset_path": dataset,
+                            "main": mainInfo,
+                            "dataset": info,
+                            "jenkins_path": mainInfo.jenkins.path.map(function (p) {
+                                return 'job/' + p;
+                            }).join('/'),
+                            "jenkins_buildicon": 'buildStatus/icon?job=' + encodeURIComponent(mainInfo.jenkins.path.join('/') + '/'),
+                            "datasets": datasets,
+                            "issue_badge_base": `https://img.shields.io/github/issues/detail/state${(new URL(mainInfo.github)).pathname}`
+                        }));
+                    });
+                    document.title = info.title;
+                });
+            });
+        });
+    }
 } else {
     Handlebars.registerHelper('rowClass', function(f, i) {
       if (i.hasOwnProperty('families') && !i.families.includes(f)) {
         return 'text-danger';
       } else if (i.hasOwnProperty('extract') && i.extract.hasOwnProperty('stage')) {
-        if (i.extract.stage == 'Prioritized') {
+        if (i.extract.stage === 'Prioritized') {
           return 'text-body';
         } else {
           return 'text-muted';
@@ -104,7 +136,38 @@ if (dataset) {
         $.getJSON('info.json', function(info) {
             document.title = "Dataset family: " + info.family;
             const fetches = info.pipelines.map(function (pipeline) {
-                return $.getJSON(pipeline + '/info.json');
+                return $.getJSON(pipeline + '/info.json')
+                    .then(function (dsinfo) {
+                        return $.ajax({url: pipeline + '/spec.md', method: 'HEAD'})
+                            .then(function () {
+                                dsinfo.spec = pipeline + '/spec';
+                                return dsinfo;
+                            }, function() {
+                                return $.Deferred().resolve(dsinfo).promise();
+                            });
+                        })
+                    .then(function(dsinfo) {
+                        return $.ajax({url: pipeline + '/flowchart.ttl', method: 'HEAD'})
+                            .then(function () {
+                                dsinfo.flowchart = 'specflowcharts.html?' + pipeline + '/flowchart.ttl';
+                                return dsinfo;
+                            }, function() {
+                                return $.Deferred().resolve(dsinfo).promise();
+                            });
+                    })
+                    .then(function(dsinfo) {
+                        let spec_url = info.jenkins.base + '/' + info.jenkins.path.map(function (p) {
+                            return 'job/' + p;
+                        }).join('/') + pipeline + '/lastBuild/artifact/datasets/' + pipeline + '/out/spec.json';
+                        return $.ajax({url: spec_url, method: 'HEAD'})
+                            .then(function () {
+                                dsinfo.jenkinsSpec = '?spec=' + pipeline;
+                                return dsinfo;
+                            }, function() {
+                                return $.Deferred().resolve(dsinfo).promise();
+                            });
+
+                    });
             });
             $.when.apply($, fetches).then(function() {
                 const allInfo = arguments;
@@ -112,7 +175,7 @@ if (dataset) {
                     return {
                         'directory': pipeline,
                         'number': i + 1,
-                        'info': allInfo[i][0]
+                        'info': allInfo[i]
                     };
                 });
                 let fetchDatasets;
@@ -125,10 +188,14 @@ if (dataset) {
                 fetchDatasets.done(function(datasets) {
                     collected = collected.map(p => {
                         p.datasets = datasets.filter(ds => {
-                            if (typeof(p.info.landingPage) == 'string') {
-                                return ds.landingPage === p.info.landingPage
+                            if (p.info.hasOwnProperty('landingPage') && p.info.landingPage !== null) {
+                                if (typeof (p.info.landingPage) == 'string') {
+                                    return ds.landingPage === p.info.landingPage
+                                } else {
+                                    return p.info.landingPage.includes(ds.landingPage)
+                                }
                             } else {
-                                return p.info.landingPage.includes(ds.landingPage)
+                                return false
                             }
                         }).filter(function(ds, i, s) {
                             return s.findIndex(d => d.uri === ds.uri) === i
@@ -146,7 +213,7 @@ if (dataset) {
                                 }, null);
                         if (info.hasOwnProperty('pmd')) {
                             p.datasets = p.datasets.map(ds => {
-                                ds.uri = info.pmd + '/resource?uri=' + encodeURIComponent(ds.uri);
+                                ds.uri = info.pmd + '/cube/explore?uri=' + encodeURIComponent(ds.uri);
                                 return ds;
                             });
                         };
@@ -159,6 +226,7 @@ if (dataset) {
                     $("#body").html(template({
                         "family": info.family,
                         "github": info.github,
+                        "githubcogs": info.githubcogs,
                         "jenkins_base": info.jenkins.base,
                         "jenkins_path": info.jenkins.path.map(function (p) {
                             return 'job/' + p;
