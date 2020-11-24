b0360bf25e133c812fef046efc2eafc3c44e0049
Shannon Black <shannon1995black@gmail.com>
2020-11-24 16:50:42 +0000

    Added in cube class, trace, codelists and mapped columns in info.json

--- datasets/ONS-GDP-at-current-prices-real-time-database-YBHA/old_main.py
+++ datasets/ONS-GDP-at-current-prices-real-time-database-YBHA/old_main.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -14,26 +13,16 @@
 #     name: python3
 # ---
 
-# +
-from gssutils import * 
-import json 
-from urllib.parse import urljoin
+from gssutils import *
+from gssutils.metadata import THEME
+scraper = Scraper('https://www.ons.gov.uk/economy/grossdomesticproductgdp/datasets/realtimedatabaseforukgdpybha')
+scraper
 
-cubes = Cubes("info.json")
-trace = TransformTrace()
-df = pd.DataFrame()
+# +
+dist = scraper.distributions[0]
+tabs = (t for t in dist.as_databaker())
 tidied_sheets = []
-# -
 
-info = json.load(open('info.json')) 
-scraper = Scraper(seed="info.json")   
-scraper 
-
-distribution = scraper.distributions[0]
-tabs = (t for t in distribution.as_databaker())
-
-
-# +
 def left(s, amount):
     return s[:amount]
 def right(s, amount):
@@ -53,34 +42,17 @@ def date_time(time_value):
 # -
 
 for tab in tabs:
-       
-        datasetTitle = 'ONS-GDP-at-current-prices-real-time-database-YBHA'
-        columns=['Vintage','Publication','Estimate Type', 'CDID', 'Seasonal Adjustment', 'Marker']
-        trace.start(datasetTitle, tab, columns, scraper.distributions[0].downloadURL)
-        
         if (tab.name == '1989 - 1999') or (tab.name == '2000 - 2010') or (tab.name == '2011 - 2017') or (tab.name == '2018 -'):
             
             seasonal_adjustment = 'SA'
-            trace.Seasonal_Adjustment("Hardcoded as SA, taken from source")
-            
             code = tab.excel_ref('B3')
-            trace.CDID("Taken from cell ref B3")
-            
-            vintage = tab.excel_ref('A6').expand(DOWN).is_not_blank() 
-            trace.Vintage("Taken from cell A6 down")
-            
+            vintage = tab.excel_ref('A6').expand(DOWN).is_not_blank()        
             estimate_type = tab.excel_ref('B6').expand(RIGHT).is_not_blank()
-            trace.Estimate_Type("Taken from cell B6 across")
-            
             publication = tab.excel_ref('B7').expand(RIGHT).is_not_blank()
-            trace.Publication("Taken from cell B7 across")
                 
             if (tab.name == '2011 - 2017') or (tab.name == '2018 -'):
                 estimate_type = tab.excel_ref('B5').expand(RIGHT).is_not_blank()
-                trace.Estimate_Type("Taken from cell B5 across")
-                
                 publication = tab.excel_ref('B6').expand(RIGHT).is_not_blank()
-                trace.Publication("Taken from cell B6 across")
         
             observations = publication.fill(DOWN).is_not_blank()
         
@@ -89,16 +61,16 @@ for tab in tabs:
                 HDim(estimate_type, 'GDP Estimate Type', DIRECTLY, ABOVE),
                 HDim(publication, 'Publication Date', DIRECTLY, ABOVE),
                 #HDim(code, 'CDID', CLOSEST, ABOVE), #dropped for now
-                #HDimConst('Seasonal Adjustment', seasonal_adjustment), #dropped for now
+                #HDimConst('Seasonal Adjustment', seasonal_adjustment),
+                HDimConst('Measure Type', 'GBP Million'),
             ]
 
             tidy_sheet = ConversionSegment(tab, dimensions, observations)        
             #savepreviewhtml(tidy_sheet, fname=tab.name + "Preview.html")
-            trace.with_preview(tidy_sheet)
-            trace.store("combined_dataframe", tidy_sheet.topandas())
+            tidied_sheets.append(tidy_sheet.topandas())
 
 
-df = trace.combine_and_trace(datasetTitle, "combined_dataframe")
+df = pd.concat(tidied_sheets, ignore_index = True, sort = False)
 df.rename(columns={'OBS' : 'Value','DATAMARKER' : 'Marker'}, inplace=True)
 #df['CDID'] = df['CDID'].map(lambda x: right(x,4)) #dropped for now
 df['Publication Date'].replace('Q3  1990', 'Q3 1990', inplace=True)  #removing space
@@ -107,35 +79,33 @@ df['GDP Reference Period'].replace('Q2 1010', 'Q2 2010', inplace=True) #fixing t
 df["GDP Reference Period"] = df["GDP Reference Period"].apply(date_time)
 df["Publication Date"] = df["Publication Date"].apply(date_time)
 df['Marker'].replace('..', 'unknown', inplace=True)
-df = df.fillna('')
 
-df['Marker'].unique()
-
-# +
-info = json.load(open('info.json')) 
-codelistcreation = info['transform']['codelists'] 
-print(codelistcreation)
-print("-------------------------------------------------------")
-
-codeclass = CSVCodelists()
-for cl in codelistcreation:
-    if cl in df.columns:
-        df[cl] = df[cl].str.replace("-"," ")
-        df[cl] = df[cl].str.capitalize()
-        codeclass.create_codelists(pd.DataFrame(df[cl]), 'codelists', scraper.dataset.family, Path(os.getcwd()).name.lower())
-
-# +
-tidy = df[['GDP Reference Period','Publication Date','Value','GDP Estimate Type','Marker']]
+tidy = df[['GDP Reference Period','Publication Date','Value','GDP Estimate Type', 'Measure Type','Marker']]
 for column in tidy:
     if column in ('GDP Estimate Type'):
         tidy[column] = tidy[column].str.lstrip()
         tidy[column] = tidy[column].str.rstrip()
-        tidy[column] = tidy[column].apply(pathify)
-
-cubes.add_cube(scraper, tidy, "GDP at current prices – real-time database (YBHA)")
 tidy
+
+out = Path('out-old')
+out.mkdir(exist_ok=True)
+tidy.drop_duplicates().to_csv(out / 'observations.csv', index = False)
+
+# +
+scraper.dataset.family = 'trade'
+
+## Adding short metadata to description
+additional_metadata = """ All data is seasonally adjusted.
+
+In July 2018, a new GDP publication model was adopted. Quarterly estimates of GDP have since been published twice a quarter, rather than three times a quarter as happened prior to this.
+"""
+scraper.dataset.description = scraper.dataset.description + additional_metadata
+
+from gssutils.metadata import THEME
+
+with open(out / 'observations.csv-metadata.trig', 'wb') as metadata:
+    metadata.write(scraper.generate_trig())
 # -
 
-
-cubes.output_all()
-trace.render()
+csvw = CSVWMetadata('https://gss-cogs.github.io/family-trade/reference/')
+csvw.create(out / 'observations.csv', out / 'observations.csv-schema.json')
