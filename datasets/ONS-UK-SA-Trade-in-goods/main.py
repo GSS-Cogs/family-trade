9a038a639c579955beceb02e02ae8e0a5f6fdcd2
Andrew Fergusson <andrew.fergusson@gmail.com>
2020-11-26 16:10:31 +0000

    most complete pending datamarker

--- datasets/ONS-UK-SA-Trade-in-goods/main.py
+++ datasets/ONS-UK-SA-Trade-in-goods/main.py
@@ -20,10 +20,13 @@ from gssutils import *
 import json
 
 info = json.load(open('info.json'))
+cubes = Cubes('info.json')
 landingPage = info['landingPage']
 landingPage
 
 scraper = Scraper(landingPage)
+scraper.dataset.family = info['families']
+
 tabs = { tab.name: tab for tab in scraper.distributions[0].as_databaker() }
 # -
 
@@ -43,68 +46,29 @@ for name,tab in tabs.items():
     cs = ConversionSegment(observations, dimensions, processTIMEUNIT=True)
     df = cs.topandas()
     
+    
     # Take the last word of the tab's name and convert it to lower case, assign as value in Flow column in df
-    df['Flow'] = str(name.split()[-1]).lower()
+    df['Flow Directions'] = str(name.split()[-1]).lower()
     
     df.rename(columns={'OBS': 'Value'}, inplace=True)
-    df['Period'] = df['Period'].astype(str)
-    df = df[['ONS Partner Geography', 'Period','Flow','Measure Type','Value' ,'Unit', 'DATAMARKER']]
+    if name.split()[0] == 'Annual':
+        df['Period'] = pd.to_datetime(df['Period'].str[:4], format='%Y').dt.strftime('/id/year/%Y')
+    elif name.split()[0] == 'Monthly':
+        df['Period'] = pd.to_datetime(df['Period'], format='%Y%b').dt.strftime('/id/month/%Y-%m')
+    else:
+        raise ValueError('Unexpected period')
+
     output = pd.concat([output, df])
 
-# +
-import re
-YEAR_RE = re.compile(r'[0-9]{4}')
-YEAR_MONTH_RE = re.compile(r'([0-9]{4})(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)')
-YEAR_QUARTER_RE = re.compile(r'([0-9]{4})(Q[1-4])')
+#-
 
-class Re(object):
-    def __init__(self):
-        self.last_match = None
-    def fullmatch(self,pattern,text):
-        self.last_match = re.fullmatch(pattern,text)
-        return self.last_match
+output.rename(columns={'DATAMARKER': 'Marker'}, inplace=True)
+output['Marker'].replace('N/A', 'not-applicable', inplace=True)
 
-def time2period(t):
-    gre = Re()
-    if gre.fullmatch(YEAR_RE, t):
-        return f"year/{t}"
-    elif gre.fullmatch(YEAR_MONTH_RE, t):
-        year, month = gre.last_match.groups()
-        month_num = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06',
-                     'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'}.get(month)
-        return f"month/{year}-{month_num}"
-    elif gre.fullmatch(YEAR_QUARTER_RE, t):
-        year, quarter = gre.last_match.groups()
-        return f"quarter/{year}-{quarter}"
-    else:
-        print(f"no match for {t}")
+#-
 
-Final_table['Period'] = Final_table.Period.str.replace('\.0', '')
-Final_table['Period'] = Final_table['Period'].apply(time2period)
-Final_table.rename(columns={'DATAMARKER': 'Marker'}, inplace=True)
-Final_table['Marker'].replace('N/A', 'not-applicable', inplace=True)
+cubes.add_cube(scraper, output, info['title'])
 
-# +
-Final_table.rename(columns={'Flow':'Flow Directions'}, inplace=True)
-
-#Flow has been changed to Flow Direction to differentiate from Migration Flow dimension
-# -
-
-from pathlib import Path
-import numpy as np
-out = Path('out')
-out.mkdir(exist_ok=True)
-Final_table.drop_duplicates().to_csv(out / 'observations.csv', index = False)
-
-scraper.dataset.family = 'trade'
-from gssutils.metadata import THEME
-scraper.dataset.theme = THEME['business-industry-trade-energy']
-with open(out / 'observations.csv-metadata.trig', 'wb') as metadata:
-    metadata.write(scraper.generate_trig())
-
-csvw = CSVWMetadata('https://gss-cogs.github.io/family-trade/reference/')
-csvw.create(out / 'observations.csv', out / 'observations.csv-schema.json')
-
-Final_table
-
+#-
+cubes.output_all()
 
