a303c7a6ff1e4311c4808149f310425eee30168f
Osamede Asemota <osamedeasemota@C02D75L5MD6R.home>
2020-12-08 10:48:17 +0000

    Rework done on code Relationship between cdid dimension and observation re-defined Cubes class added

--- datasets/ONS-UK-Trade-in-goods-CPA-08/main.py
+++ datasets/ONS-UK-Trade-in-goods-CPA-08/main.py
@@ -1,34 +1,74 @@
 #!/usr/bin/env python
 # coding: utf-8
+
+# In[1]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
 # %%
-from gssutils import *
-from databaker.framework import *
+
 import pandas as pd
+import numpy as np
+from gssutils import *
+import json
 from gssutils.metadata import THEME
 from gssutils.metadata import *
+from databaker.framework import *
+
+#pd.options.mode.chained_assignment = None
+
 import datetime
-from gssutils.metadata import Distribution, GOV
-pd.options.mode.chained_assignment = None
-import json
 import inspect
 import re
+from os import environ
+
+cubes = Cubes("info.json")
+
+
+# In[2]:
+
 
 def left(s, amount):
     return s[:amount]
 
+
+# In[3]:
+
+
 def right(s, amount):
     return s[-amount:]
+
+
+# In[4]:
+
 
 def mid(s, offset, amount):
     return s[offset:offset+amount]
 
-info = json.load(open('info.json'))
 
-scraper = Scraper(info['landingPage'])
+# In[5]:
+
+
+with open ('info.json') as f:
+    info = json.load(f)
+    
+#info = json.load(open('info.json'))
+
+
+# In[6]:
+
+
+landingPage = info['landingPage']
+scraper = Scraper(landingPage)
+
+scraper.dataset.family = info['families']
 scraper
 
 
 # %%
+
+# In[7]:
 
 
 dist = scraper.distributions[0]
@@ -37,108 +77,105 @@ dist
 
 # %%
 
+# In[8]:
 
-tabs = (t for t in dist.as_databaker())
 
-tidied_sheets = []
+tabs = scraper.distributions[0].as_databaker()
 
-for tab in tabs:
-    
-    if tab.name in ('Index', 'Contact'):
+
+# In[9]:
+
+
+tidied_sheets =[]
+for tab in tabs: 
+    if tab.name in ['Index', 'Contact']:
         continue
         
     print(tab.name)
     
-    cell = tab.filter(contains_string('Total'))
+    cell = tab.filter('Total')
     
     flow_direction = tab.name
-
-    period = cell.shift(2,-1).expand(RIGHT).is_not_blank()
+    #period = cell.shift(2,-1).expand(RIGHT).is_not_blank()
+    period = tab.excel_ref("C4").expand(RIGHT).is_not_blank()
     
-    product = cell.expand(DOWN).is_not_blank()
+    #product = cell.expand(DOWN).is_not_blank() # This was problematic
+    product = tab.excel_ref("A5").expand(DOWN).is_not_blank()
     
     prod_dep = product.regex('[^0-9]+')
     
     prod_cat = product.regex('[0-9]{2}\s{1}[^\.]')
     
-    cdid_code = tab.excel_ref('B4').expand(DOWN) #SOME BLANK VALUES 
-    
-    observations = product.shift(RIGHT).fill(RIGHT).is_not_blank()
-
+    cdid_code = tab.excel_ref('B5').expand(DOWN).is_not_blank() | tab.excel_ref("B194") # Adding a blank value 
+   
+    observations = tab.excel_ref('C5').expand(DOWN).expand(RIGHT).is_not_blank()
+    #observations = product.shift(RIGHT).fill(RIGHT).is_not_blank()
+   
     dimensions = [
-        HDim(period, 'Period', DIRECTLY, ABOVE),
         HDimConst('Flow Directions', flow_direction),
-        HDim(product, 'Product', CLOSEST, ABOVE),
-        HDim(prod_dep, 'Product Department', CLOSEST, ABOVE),
-        HDim(prod_cat, 'Product Category', CLOSEST, ABOVE),
-        HDim(cdid_code, 'CDID', DIRECTLY, LEFT),
         HDimConst('Measure Type', 'GBP Total'),
-        HDimConst('Unit', 'gbp-million')
-        ]
-
-    tidy_sheet = ConversionSegment(tab, dimensions, observations)
-    savepreviewhtml(tidy_sheet, fname="Preview.html")
-
-    tidied_sheets.append(tidy_sheet.topandas())
-    
+        HDimConst('Unit', 'GBP-million'),
         
-
+        HDim(period, 'Period', DIRECTLY, ABOVE),
+        HDim(product, 'Product',  CLOSEST, ABOVE),
+        HDim(prod_dep, 'Product Department', CLOSEST, ABOVE), 
+        HDim(prod_cat, 'Product Category', CLOSEST, ABOVE),
+        HDim(cdid_code, 'CDID', CLOSEST, UP)   
+        ]
+   
+    cs = ConversionSegment(tab, dimensions, observations)
+    tidy_sheet = cs.topandas()
+    #savepreviewhtml(tidy_sheet, fname="Preview.html")
+    tidied_sheets.append(tidy_sheet) # datacube
+    
+    df = pd.concat(tidied_sheets, ignore_index = True, sort = False).fillna('')  
 
 # %%
-
-
 pd.set_option('display.float_format', lambda x: '%.0f' % x)
-
-df = pd.concat(tidied_sheets, ignore_index = True, sort = False).fillna('')
-
+  
 indexNames = df[ df['Product Department'] == 'Residual seasonal adjustment' ].index
 df.drop(indexNames, inplace = True)
 
 df['Period'] = df['Period'].map(lambda x: 'year/' + left(x,4) if 'Q' not in x else 'quarter/' + left(x,4) + '-' + right(x,2))
 
 df['Flow Directions'] = df['Flow Directions'].map(lambda x: right(x, len(x) - 2))
-df['Product Department'] = df['Product Department'].map(lambda x: right(x, len(x) - 2) if 'Total' not in x else x)
 
 df['Product'] = df['Product'].map(lambda x: '' if ('.' not in left(x, 5) and mid(x, 2, 1) == ' ') else x)
 df['Product'] = df['Product'].map(lambda x: 'All' if x == '' else x)
+df['Product'] = df['Product'].map(lambda x: right(x, len(x) - 8) if mid(x, 2, 5) == 'OTHER' else x)
+df['Product'] = df['Product'].map(lambda x: right(x, len(x) - 5).strip() if '.' in x else (right(x, len(x) - 4) if left(x, 2).isnumeric() == True else x))
+
+df['Product Department'] = df['Product Department'].map(lambda x: right(x, len(x) - 2) if 'Total' not in x else x)
 
 df['Product Category'] = df['Product Category'].map(lambda x: 'All' if x == '' else x)
 df['Product Category'] = df['Product Category'].map(lambda x: right(x, len(x) - 3) if left(x, 2).isnumeric() == True else x)
 
-df['Product'] = df['Product'].map(lambda x: right(x, len(x) - 8) if mid(x, 2, 5) == 'OTHER' else x)
-df['Product'] = df['Product'].map(lambda x: right(x, len(x) - 5).strip() if '.' in x else (right(x, len(x) - 4) if left(x, 2).isnumeric() == True else x))
+df = df.replace({'Product' : {
+'3 Processed and preserved fish, crustaceans, molluscs, fruit and vegetables' : 'Processed and preserved fish, crustaceans, molluscs, fruit and vegetables', 
+'-6 Alcoholic beverages' : 'Alcoholic beverages', 
+'6 Manufacture of cement, lime, plaster and articles of concrete, cement and plaster' : 'Manufacture of cement, lime, plaster and articles of concrete, cement and plaster',
+'3 Basic iron and steel' : 'Basic iron and steel', 
+'5 Other basic metals and casting' : 'Other basic metals and casting'}})
 
 df = df.replace({'Product' : {
-    '3 Processed and preserved fish, crustaceans, molluscs, fruit and vegetables' : 'Processed and preserved fish, crustaceans, molluscs, fruit and vegetables', 
-    '-6 Alcoholic beverages' : 'Alcoholic beverages', 
-    '6 Manufacture of cement, lime, plaster and articles of concrete, cement and plaster' : 'Manufacture of cement, lime, plaster and articles of concrete, cement and plaster',
-    '3 Basic iron and steel' : 'Basic iron and steel', 
-    '5 Other basic metals and casting' : 'Other basic metals and casting'}})
+'a-products-of-agriculture-forestry-fishing' : 'products-of-agriculture-forestry-fishing',
+'b-mining-quarrying' : 'mining-quarrying',
+'c-manufactured-products' : 'manufactured-products',
+'d-electricity-gas-steam-air-conditioning' : 'electricity-gas-steam-air-conditioning',
+'e-water-supply-sewerage-waste-management' : 'water-supply-sewerage-waste-management',
+'j-information-communication-services' : 'information-communication-services',
+'m-professional-scientific-technical-services' : 'professional-scientific-technical-services',
+'r-arts-entertainment-recreation' : 'arts-entertainment-recreation',
+'s-other-services' : 'other-services'}})
 
-df.rename(columns={'OBS' : 'Value'}, inplace=True)
-
-df = df[['Period', 'Flow Directions','Product Department','Product Category','Product','CDID', 'Value','Measure Type','Unit']]
-
-for column in df:
-    if column in ('Flow Directions','Product Department','Product Category','Product','Unit'):
-        df[column] = df[column].map(lambda x: pathify(x))
-        
-df = df.replace({'Product' : {
-    'a-products-of-agriculture-forestry-fishing' : 'products-of-agriculture-forestry-fishing',
-    'b-mining-quarrying' : 'mining-quarrying',
-    'c-manufactured-products' : 'manufactured-products',
-    'd-electricity-gas-steam-air-conditioning' : 'electricity-gas-steam-air-conditioning',
-    'e-water-supply-sewerage-waste-management' : 'water-supply-sewerage-waste-management',
-    'j-information-communication-services' : 'information-communication-services',
-    'm-professional-scientific-technical-services' : 'professional-scientific-technical-services',
-    'r-arts-entertainment-recreation' : 'arts-entertainment-recreation',
-    's-other-services' : 'other-services'}})
-        
 df['Product'].unique().tolist()
 
+df.rename(columns={'OBS' : 'Value'}, inplace=True)
+df = df[['Period','Flow Directions','Product','Product Department','Product Category','CDID','Value','Measure Type','Unit']]
 
-# %%
-
+for column in df:
+    if column in ['Flow Directions','Product','Product Department','Product Category','Unit']:
+        df[column] = df[column].map(lambda x: pathify(x))
 
 from IPython.core.display import HTML
 for col in df:
@@ -146,30 +183,11 @@ for col in df:
         df[col] = df[col].astype('category')
         display(HTML(f"<h2>{col}</h2>"))
         display(df[col].cat.categories) 
+        
+    
+cubes.add_cube(scraper, df, info['title'])
+
+cubes.output_all()
 
 
 # %%
-
-
-destinationFolder = Path('out')
-destinationFolder.mkdir(exist_ok=True, parents=True)
-
-TAB_NAME = 'observations'
-
-df.drop_duplicates().to_csv(destinationFolder / f'{TAB_NAME}.csv', index = False)
-
-scraper.dataset.family = 'trade'
-
-with open(destinationFolder / f'{TAB_NAME}.csv-metadata.trig', 'wb') as metadata:
-    metadata.write(scraper.generate_trig())
-
-csvw = CSVWMetadata('https://gss-cogs.github.io/family-trade/reference/')
-csvw.create(destinationFolder / f'{TAB_NAME}.csv', destinationFolder / f'{TAB_NAME}.csv-schema.json')
-df.head(25)
-
-
-# %%
-
-
-
-
