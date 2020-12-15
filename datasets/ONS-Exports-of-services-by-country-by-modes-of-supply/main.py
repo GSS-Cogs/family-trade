1b01472451da2dcfaeb6a6b54b923e26318f6afe
Shannon Black <shannon1995black@gmail.com>
2020-12-15 09:54:37 +0000

    marker update : estimated and cogs-marker

--- datasets/ONS-Exports-of-services-by-country-by-modes-of-supply/main.py
+++ datasets/ONS-Exports-of-services-by-country-by-modes-of-supply/main.py
@@ -147,7 +147,7 @@ tidy_imports = df[["Period", "Country", "Mode", "Direction", "Service Account", 
 tidy_imports
 
 tidy = pd.concat([tidy_exports, tidy_imports], ignore_index=True)
-tidy['Marker'] = 'e'
+tidy['Marker'] = 'estimated'
 tidy['Mode'].unique()
 
 tidy["Country"] = tidy["Country"].apply(pathify)
