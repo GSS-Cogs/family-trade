ebdb3eac18f513a75a862643a3a0909554fa1bb4
Rob Barry <rob.barry@ons.gov.uk>
2021-01-22 09:37:36 +0000

    Hoping this fixes the inconsistency.

--- datasets/ONS-International-trade-in-services-by-subnational-areas-of-the-UK/main.py
+++ datasets/ONS-International-trade-in-services-by-subnational-areas-of-the-UK/main.py
@@ -277,7 +277,9 @@ df['Location'] = df.apply(lambda x: x['NUTS Area Name'] if x['Location'] == 'N/A
 
 
 # +
-df = df.replace({'Country or Origin of Trade' : {'rest-of-the-world' : 'rest-of-world'}})
+df = df.replace({'Country or Origin of Trade' : {'total': 'all-countries',
+                                                 'rest-of-the-world': 'rest-of-world'}})
+
 
 df = df.replace({'Marker' : {'..' : 'Suppressed'}})
 
