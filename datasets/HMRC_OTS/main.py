4a6a8b87852aeae8085f4716eff2dde20caa1ff0
canwaf <andrew.fergusson@me.com>
2021-01-29 16:24:41 +0000

    Initial creation of OTS dataset, info.json/main.py, deletion of old OTS_CN8 which is superceded

--- datasets/HMRC_OTS/main.py
+++ datasets/HMRC_OTS/main.py
@@ -6,121 +6,74 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.7.1
+#       jupytext_version: 1.9.1
 #   kernelspec:
-#     display_name: Python 3
-#     language: python
+#     display_name: Python 3.8.7 64-bit
+#     metadata:
+#       interpreter:
+#         hash: f2b0bb78cbb17f9579fa5e62801b4f61ba0bc1633b5d43780d3380efce1f0d9c
 #     name: python3
 # ---
 
+# +
+import logging
 import json
 import pandas as pd
+
 from gssutils import *
 
-# # Motion due to no current API-based scraper
-
-import requests
-from cachecontrol import CacheControl
-from cachecontrol.caches.file_cache import FileCache
-from cachecontrol.heuristics import ExpiresAfter
-
-# Object manages the session
-sess = requests.session()
-cached_sess = CacheControl(sess, cache=FileCache('.cache'), heuristic=ExpiresAfter(days=7))
-
-
-# Define child of Scraper which rips from API end point and spits out an dataframe with as_pandas()
-class ApiScraper(Scraper):
-    """
-    Inherits the Scraper class and replace the as_pandas() to get the data from the api endpoint, heavy caching requried
-    """
-
-    def as_pandas(self):
-        url = self.distributions[0].downloadURL
-        page = cached_sess.get(url)
-        contents = page.json()
-        df = pd.DataFrame(contents['value'])
-        # print('Fetching data from {url}.'.format(url=page.url))
-        while '@odata.nextLink' in contents.keys():
-            # print('Fetching more data from {url}.'.format(url=contents['@odata.nextLink']))
-            page = cached_sess.get(contents['@odata.nextLink'])
-            contents = page.json()
-            df = df.append(pd.DataFrame(contents['value']))
-        return df
-
-
-# For the remainder of the augmentation process, another method
-def fetch_api_to_df(url: str) -> pd.DataFrame():
-    page = cached_sess.get(url)
-    contents = page.json()
-    df = pd.DataFrame(contents['value'])
-    # print('Fetching data from {url}.'.format(url=page.url))
-    while '@odata.nextLink' in contents.keys():
-        # print('Fetching more data from {url}.'.format(url=contents['@odata.nextLink']))
-        page = cached_sess.get(contents['@odata.nextLink'])
-        contents = page.json()
-        df = df.append(pd.DataFrame(contents['value']))
-    return df
-
-
-
-# # And we begin
+# +
+# logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 
 # +
 infoFileName = 'info.json'
 
 info    = json.load(open(infoFileName))
-scraper = ApiScraper(seed=infoFileName)
+scraper = Scraper(seed=infoFileName)
 cubes   = Cubes(infoFileName)
 
 scraper.dataset.family = info['families']
+
+# + tags=[]
+distro = scraper.distribution(latest=True)
 # -
 
-rts = scraper.as_pandas()
+# Get OData API Chunks
+api_chunks = get_odata_api_chunks(distro)
+logging.debug(f'The chunks found on api are {api_chunks}')
 
-# MonthId 
-# Required Pandas > 1.0
-# df['Period'] = pd.to_datetime(df['MonthId'], format='%Y%m').dt.to_period('Q').dt.strftime('/id/quarter/%Y-%q')
-rts['Period'] = pd.PeriodIndex(pd.to_datetime(rts['MonthId'], format='%Y%m'), freq='Q').astype(str)
-rts['Period'] = rts['Period'].apply(lambda x:'/id/quarter/{year}-{quarter}'.format(year=x[:4], quarter=x[-1:]))
-rts['Period'] = rts['Period'].astype('category')
+# Get PMD Chunks
+pmd_chunks = get_pmd_chunks(distro)
+logging.debug(f'The chunks found on api are {pmd_chunks}')
 
-# FlowTypeId
-# https://api.uktradeinfo.com/FlowType (Strip all strings)
-FlowTypes = fetch_api_to_df(url=info['apiEndpoints']['FlowTypeId']).applymap(lambda x: x.strip() if isinstance(x, str) else x)
-for column in FlowTypes.columns[1:]:
-    FlowTypes[column] = FlowTypes[column].astype('category')
-rts = rts.merge(FlowTypes, on='FlowTypeId').drop(labels=['@odata.type'], axis=1)
-del FlowTypes
+pd.to_datetime('/id/month/2000-01', format='/id/month/%Y-%m').strftime('%Y%m')
 
+test = ['/id/month/2000-01', '/id/month/2000-02', '/id/month/2000-03', '/id/month/2000-05']
 
-# GovRegionId
-# https://api.uktradeinfo.com/Region
-GovRegions = fetch_api_to_df(url=info['apiEndpoints']['GovRegionId'])
-for column in GovRegions.columns[1:]:
-    GovRegions[column] = GovRegions[column].astype('category').str.strip()
-rts = rts.merge(GovRegions, left_on='GovRegionId', right_on='RegionId').drop(labels=['RegionId'], axis=1)
-del GovRegions
+# Get next period to download
+if len(pmd_chunks) != 0:
+    fetch_chunk = min(api_chunks)
+else:
+    pmd_chunks = [int(pd.to_datetime(x, format='/id/month/%Y-%m').strftime('%Y%m')) for x in pmd_chunks]
+    fetch_chunk = min(set(api_chunks)-set(pmd_chunks))
+logging.info(f'Earliest chunk not on PMD but found on API is {fetch_chunk}')
 
-# CountryID
-# https://api.uktradeinfo.com/Country
-Countries = fetch_api_to_df(url=info['apiEndpoints']['CountryId'])
-for column in Countries.columns[1:]:
-    Countries[column] = Countries[column].astype('category')
-rts = rts.merge(Countries, on='CountryId')
-del Countries
+# Fetch the chunk
+df = distro.as_pandas(chunks_wanted=fetch_chunk)
+df
 
+# Determine date formatting (which helps determin the next period to download)
+df['Period'] = pd.to_datetime(df['MonthId'], format="%Y%m").dt.strftime('/id/month/%Y-%m')
 
-# CommoditySitc2Id
-# https://api.uktradeinfo.com/SITC
-SITCs = fetch_api_to_df(url=info['apiEndpoints']['CommoditySitc2Id'])
-for column in SITCs.columns[1:]:
-    SITCs[column] = SITCs[column].astype('category')
-rts = rts.merge(SITCs, left_on='CommoditySitc2Id', right_on='CommoditySitcId').drop(labels=['CommoditySitcId'], axis=1)
-
+# Check if we've only got one chunk
+df['Period'].value_counts()
 
 # Add dataframe is in the cube
-cubes.add_cube(scraper, rts, info['title'])
+cubes.add_cube(scraper, df, scraper.title)
 
+# + tags=["outputPrepend"]
 # Write cube
 cubes.output_all()
+# -
+
+
