1f54c4bdd33e63e5c3057b19a8a7f7c3273591f5
canwaf <andrew.fergusson@me.com>
2021-02-01 14:27:03 +0000

    Aligning change in get_chunks methods

--- datasets/HMRC_OTS/main.py
+++ datasets/HMRC_OTS/main.py
@@ -8,7 +8,7 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.9.1
 #   kernelspec:
-#     display_name: Python 3.8.7 64-bit
+#     display_name: 'Python 3.8.7 64-bit (''gss-utils'': pipenv)'
 #     metadata:
 #       interpreter:
 #         hash: f2b0bb78cbb17f9579fa5e62801b4f61ba0bc1633b5d43780d3380efce1f0d9c
@@ -39,33 +39,27 @@ distro = scraper.distribution(latest=True)
 # -
 
 # Get OData API Chunks
-api_chunks = get_odata_api_chunks(distro)
+api_chunks = distro.get_odata_api_chunks()
 logging.debug(f'The chunks found on api are {api_chunks}')
 
 # Get PMD Chunks
-pmd_chunks = get_pmd_chunks(distro)
+pmd_chunks = distro.get_pmd_chunks()
 logging.debug(f'The chunks found on api are {pmd_chunks}')
 
-pd.to_datetime('/id/month/2000-01', format='/id/month/%Y-%m').strftime('%Y%m')
-
-test = ['/id/month/2000-01', '/id/month/2000-02', '/id/month/2000-03', '/id/month/2000-05']
-
 # Get next period to download
-if len(pmd_chunks) != 0:
+if len(pmd_chunks) == 0:
     fetch_chunk = min(api_chunks)
 else:
     pmd_chunks = [int(pd.to_datetime(x, format='/id/month/%Y-%m').strftime('%Y%m')) for x in pmd_chunks]
     fetch_chunk = min(set(api_chunks)-set(pmd_chunks))
 logging.info(f'Earliest chunk not on PMD but found on API is {fetch_chunk}')
 
-# Fetch the chunk
 df = distro.as_pandas(chunks_wanted=fetch_chunk)
+
 df
 
-# Determine date formatting (which helps determin the next period to download)
 df['Period'] = pd.to_datetime(df['MonthId'], format="%Y%m").dt.strftime('/id/month/%Y-%m')
 
-# Check if we've only got one chunk
 df['Period'].value_counts()
 
 # Add dataframe is in the cube
