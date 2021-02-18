d20c1c26c5a4f2d93e9805e1088368bf2574a35c
Shannon Black <shannon1995black@gmail.com>
2021-02-18 16:50:30 +0000

    iteration error and tracer added

--- datasets/ONS_BoP/main.py
+++ datasets/ONS_BoP/main.py
@@ -13,12 +13,6 @@ scraper = Scraper(seed="info.json")
 scraper 
 
 # %%
-dist  = scraper.distribution(latest=True)
-tabs = dist.as_databaker()
-
-# %%
-
-# %%
 bop_services = {
         "Total":"0",
         "Manufacturing and maintenance services":"1+2",
@@ -36,106 +30,122 @@ bop_services = {
 
 
 # %%
-# ## Reusable Functions
-def format_time(value):
-    """
-    the "value" is a concatenation of TIME and Quarter extracted values per observation, seperated by a single hyphen
-    """
-    value = str(value).strip()
-    if value[-1] == "-":
-        return "year/"+value[:-1]
-    else:
-        return "quarter/"+value
-
-
-# %%
+# Reusable Functions
 class is_one_of(object):
     def __init__(self, allowed):
         self.allowed = allowed
     def __call__(self, xy_cell):
         if xy_cell.value.strip() in self.allowed:
             return True
-        return False
+        return False   
+#Format Date/Quarter
+def left(s, amount):
+    return s[:amount]
+
+def right(s, amount):
+    return s[-amount:]
+
+def date_time(date):
+    if len(date)  == 4:
+        return 'year/' + date
+    elif len(date) == 6:
+        return 'quarter/' + left(date,4) + '-' + right(date,2)
+    else:
+        return "Date Formatting Error Unknown"
 
 
 # %%
+distribution = scraper.distribution(latest = True)
+datasetTitle = distribution.title
+tabs = { tab.name: tab for tab in distribution.as_databaker() }
 
-# ## Extract and Transform
-output = pd.DataFrame()
-for letter in "ABCDEFGHIJKX":#BCDEFGHIJKX":
-    tab = [tab for tab in tabs if tab.name[-1] == letter][0]
-    # Quick grabs
-    Code = tab.excel_ref('C').is_not_blank().is_not_blank()
-    Year = tab.excel_ref('4').is_not_blank().is_not_blank()
-    Quarter = tab.excel_ref('5')
-    Trade = tab.excel_ref('B1').expand(DOWN).filter(is_one_of(["Imports", "Exports", "Balances", "Credits", "Debits", "Balances (net transactions)"]))
-    seasonal = tab.excel_ref('B').filter(is_one_of(["Seasonally adjusted", "Not seasonally adjusted"]))
-    account_type = tab.excel_ref('B1').is_not_blank()
-    Services = tab.excel_ref('B').is_not_blank().is_not_blank() - Trade
-    Services = Services - tab.excel_ref('B1').expand(UP)
-    Currency = tab.excel_ref('N3')
-
-    if letter == 'B' or letter == 'X':
-        observations = Services.waffle(Year).is_not_blank().is_not_blank() - tab.excel_ref('B67').expand(RIGHT).expand(DOWN)
-    else:
-        observations = Services.waffle(Year).is_not_blank().is_not_blank()
+# %%
+for name, tab in tabs.items():
+    columns=['Period', 'CDID', 'Seasonal Adjustment', 'BOP Services', 'Flow Directions', 'Account Type', 'Marker']
+    trace.start(datasetTitle, tab, columns, distribution.downloadURL)
+    if 'Index' in name or 'Records' in name or 'Table R1' in name or 'Table R2' in name or 'Table R3' in name:
+        continue 
+    cdid = tab.excel_ref('C5').expand(DOWN).is_not_blank()
+    trace.CDID("CDID's taken from column C in tab")
     
+    year = tab.excel_ref('C4').expand(RIGHT).is_not_blank()
+    quarter = tab.excel_ref('D5').expand(RIGHT)
+    trace.Period("Taken from Year and Quarter ; rows 4 and 5")
+    
+    flow = tab.excel_ref('B1').expand(DOWN).filter(is_one_of(["Imports", "Exports", "Balances", "Credits", "Debits", "Balances (net transactions)"]))
+    trace.Flow_Directions("flow taken from tab values in column B as one of: Imports, Exports, Balances, Credits, Debits, Balances (net transactions) ")   
+    
+    seasonal_adjustment = tab.excel_ref('B').filter(is_one_of(["Seasonally adjusted", "Not seasonally adjusted"]))
+    trace.Seasonal_Adjustment("Taken from column B as one of the following values: Seasonally Adjusted , Not Seasonally Adjusted")
+    
+    account_type = tab.excel_ref('B1').is_not_blank()
+    trace.Account_Type("Taken from tab title")
+    
+    services = tab.excel_ref('B').is_not_blank() - flow
+    services = services - tab.excel_ref('B1').expand(UP)
+    Currency = tab.excel_ref('N3')
+    
+    if name == 'Table B' or name == 'Table BX':
+        observations = services.waffle(year).is_not_blank() - tab.excel_ref('B67').expand(RIGHT).expand(DOWN)
+    else:
+        observations = services.waffle(year).is_not_blank()
     dimensions = [
                 HDimConst('Geography', 'K02000001'),
-               # HDimConst('Letter', letter),
-                HDim(Year,'TIME',DIRECTLY,ABOVE),
-                HDim(Quarter,'Quarter',DIRECTLY,ABOVE),
-                HDim(Code,'CDID',DIRECTLY,LEFT),
-                HDim(Services,'label',CLOSEST, ABOVE),
+                HDim(year,'Year',DIRECTLY,ABOVE),
+                HDim(quarter,'Quarter',DIRECTLY,ABOVE),
+                HDim(cdid,'CDID',DIRECTLY,LEFT),
+                HDim(services,'BOP Services',CLOSEST, ABOVE),
                 HDim(account_type,'Account Type',CLOSEST, ABOVE),
-                HDim(seasonal,'Seasonal Adjustment',CLOSEST, ABOVE),
-                HDim(Trade,'Flow Directions',CLOSEST,ABOVE)
+                HDim(seasonal_adjustment,'Seasonal Adjustment',CLOSEST, ABOVE),
+                HDim(flow,'Flow Directions',CLOSEST,ABOVE)
     ]
-    cs = ConversionSegment(observations, dimensions, processTIMEUNIT=True)
-   # savepreviewhtml(cs, fname= letter + "PREVIEW.html") 
-    df = cs.topandas()
-    output = pd.concat([output, df], sort=False)
-
-
-# %%
-#Lookup BOP service dimension from the labels
-#df["label"].replace(bop_services, inplace=True) 
-output.rename(columns={"label" : "BOP Services"}, inplace=True)
-output['TIME'] = output['TIME'].str.strip()
-output['Quarter'] = output['Quarter'].str.strip()
-output['CDID'] = output['CDID'].str.strip()
-# Sort out time
-output["Period"] = output["TIME"].astype(float).astype(int).astype(str) + "-" + output["Quarter"].astype(str)
-output["Period"] = output["Period"].map(format_time)
-output.drop(columns = ['TIME', "Quarter"], inplace=True)
-output.rename(columns={"OBS": "Value", 'DATAMARKER' : 'Marker'}, inplace=True)
+    tidy_sheet = ConversionSegment(tab, dimensions, observations)
+    #savepreviewhtml(tidy_sheet, fname= name + "PREVIEW.html")
+    trace.with_preview(tidy_sheet)
+    trace.store("combined_dataframe", tidy_sheet.topandas())
+    
+    
 
 # %%
-output["Flow Directions"].fillna('not-applicable', inplace=True)
-output["Flow Directions"] = output["Flow Directions"].str.strip().apply(pathify)
-output["Account Type"] = output["Account Type"].str.strip().apply(pathify)
-output = output.replace({'Flow Directions' : {'balances-net-transactions' : 'balances' }})
-output["BOP Services"] = output["BOP Services"].str.strip().apply(pathify)
-output = output.replace({'Marker' : {'-' : 'unknown'}})
-output = output.replace({'Seasonal Adjustment' : {' Seasonally adjusted' : 'sa', ' Not seasonally adjusted' : 'nsa' }})
-output["Seasonal Adjustment"].fillna('nsa', inplace=True)
+#Post Processing
+df = trace.combine_and_trace(datasetTitle, "combined_dataframe")
+df.rename(columns={'OBS' : 'Value', 'DATAMARKER' : 'Marker'}, inplace=True)
+df['Year'] = df['Year'].astype(str).replace('\.0', '', regex=True)
+df['Year'] = df['Year'].str.strip()
+df['Quarter'] = df['Quarter'].str.strip()
+df['Period'] = df['Year'] + df['Quarter']
+df["Period"] =  df["Period"].apply(date_time)
+df["Flow Directions"].fillna('not-applicable', inplace=True)
+df = df.replace({'Flow Directions' : {'balances-net-transactions' : 'balances' }})
+df = df.replace({'Marker' : {'-' : 'unknown'}})
+df = df.replace({'Seasonal Adjustment' : {' Seasonally adjusted' : 'sa', ' Not seasonally adjusted' : 'nsa' }})
+df["Seasonal Adjustment"].fillna('nsa', inplace=True)
+
+for column in df:
+    if column in ('Account Type', 'Flow Directions', 'BOP Services'):
+        df[column] = df[column].str.strip()
+        df[column] = df[column].map(lambda x: pathify(x))
 
 # %%
-output = output[[ 'Period', 'CDID', 'Seasonal Adjustment', 'BOP Services', 'Flow Directions', 'Account Type', 'Marker' ,'Value',]].drop_duplicates()
-output
+df = df[[ 'Period', 'CDID', 'Seasonal Adjustment', 'BOP Services', 'Flow Directions', 'Account Type', 'Marker' ,'Value',]].drop_duplicates()
+df
 
 # %%
-output['Account Type'].unique()
+df['Period'].unique()
 
 # %%
-output['Flow Directions'].unique()
+df['Account Type'].unique()
 
 # %%
-output['Seasonal Adjustment'].unique()
+df['Flow Directions'].unique()
 
 # %%
-output['BOP Services'].unique()
+df['Seasonal Adjustment'].unique()
 
 # %%
-cubes.add_cube(scraper, output.drop_duplicates(), dist.title)
+df['BOP Services'].unique()
+
+# %%
+cubes.add_cube(scraper, df.drop_duplicates(), datasetTitle)
 cubes.output_all()
+trace.render("spec_v1.html")
