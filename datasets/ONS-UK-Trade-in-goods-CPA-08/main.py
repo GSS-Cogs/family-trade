fcd4a9ca69e0fc7934c3654064544e4e5cf3c5d0
Osamede Asemota <osamede.asemota@gsscogs.uk>
2021-02-26 09:35:53 +0000

    Transform re-written to meet the requirements on the spec provided for Stage 2 - transform. (#158)

--- datasets/ONS-UK-Trade-in-goods-CPA-08/main.py
+++ datasets/ONS-UK-Trade-in-goods-CPA-08/main.py
@@ -5,19 +5,23 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.3.3
+#       jupytext_version: 1.9.1
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
 
-import string
+# UK trade in goods, CPA (08)
+
+import json
 import pandas as pd
 from gssutils import *
-pd.options.mode.chained_assignment = None 
 
-cubes = Cubes("info.json")
+cubes = Cubes('info.json')
+
+pd.options.mode.chained_assignment = None 
+pd.set_option('display.float_format', lambda x: '%.0f' % x)
 
 
 # +
@@ -27,120 +31,68 @@ def left(s, amount):
 def right(s, amount):
     return s[-amount:]
 
-def mid(s, offset, amount):
-    return s[offset:offset+amount]
-
 
 # -
 
-scraper = Scraper(seed='info.json')
+scraper = Scraper(seed="info.json")
+scraper
+
 distribution = scraper.distribution(latest=True)
 distribution
 
-tabs = (t for t in distribution.as_databaker())
+tabs = distribution.as_databaker()
+
+# +
 trace = TransformTrace()
 title = distribution.title
-columns = ['Period', 'Flow Directions','Product Department','Product Category','Product','CDID', 'Value']
-tidied_sheets = []
+
+scraper.dataset.title = 'UK trade in goods, CPA(08)'
+columns = ['Period', 'Flow Direction', 'Product', 'CDID', 'Unit', 'Value']
 for tab in tabs:
-    if tab.name in ('Index', 'Contact'):
-        continue   
-    
-    trace.start(title, tab, columns, distribution.downloadURL)
-    cell = tab.filter(contains_string('Total'))
-    
-    flow_direction = tab.name
-
-    period = cell.shift(2,-1).expand(RIGHT).is_not_blank()
-    
-    product = cell.expand(DOWN).is_not_blank()
-    
-    cdid_code = tab.excel_ref('B5').expand(DOWN)
-    
-    observations = product.shift(RIGHT).fill(RIGHT).is_not_blank()
-
-    dimensions = [
-        HDim(period, 'Period', DIRECTLY, ABOVE),
-        HDim(cdid_code, 'CDID', DIRECTLY, LEFT),  
-        HDim(product, 'Product', DIRECTLY, LEFT),
-        HDimConst('Flow Directions', flow_direction),
-        ]
-
-    tidy_sheet = ConversionSegment(tab, dimensions, observations)   
-    trace.with_preview(tidy_sheet)
-    trace.store("combined_dataframe", tidy_sheet.topandas())
-
-# +
-pd.set_option('display.float_format', lambda x: '%.0f' % x)
-df = trace.combine_and_trace(title, "combined_dataframe").fillna('')
-df.rename(columns={'OBS' : 'Value'}, inplace=True)
-indexNames = df[ df['Product'] == 'Residual seasonal adjustment' ].index
-df.drop(indexNames, inplace = True)
-df['Period'] = df['Period'].map(lambda x: 'year/' + left(x,4) if 'Q' not in x else 'quarter/' + left(x,4) + '-' + right(x,2))
-df['Flow Directions'] = df['Flow Directions'].map(lambda x: right(x, len(x) - 2))
-
-df['Product'] = df['Product'].map(lambda x: '' if ('.' not in left(x, 5) and mid(x, 2, 1) == ' ') else x)
-df['Product'] = df['Product'].map(lambda x: 'All' if x == '' else x)
-
-df['Product'] = df['Product'].map(lambda x: right(x, len(x) - 8) if mid(x, 2, 5) == 'OTHER' else x)
-df['Product'] = df['Product'].map(lambda x: right(x, len(x) - 5).strip() if '.' in x else (right(x, len(x) - 4) if left(x, 2).isnumeric() == True else x))
-
-# +
-#how it was previously done 
-df = df.replace({'Product' : {
-    '3 Processed and preserved fish, crustaceans, molluscs, fruit and vegetables' : 'Processed and preserved fish, crustaceans, molluscs, fruit and vegetables', 
-    '-6 Alcoholic beverages' : 'Alcoholic beverages', 
-    '6 Manufacture of cement, lime, plaster and articles of concrete, cement and plaster' : 'Manufacture of cement, lime, plaster and articles of concrete, cement and plaster',
-    '3 Basic iron and steel' : 'Basic iron and steel', 
-    '5 Other basic metals and casting' : 'Other basic metals and casting'}})
-
-for column in df:
-    if column in ('Flow Directions','Product Department','Product Category','Product'):
-        df[column] = df[column].map(lambda x: pathify(x))
+    if tab.name not in ['Index', 'Contact']:
+        print(tab.name)
+        trace.start(title, tab, columns, distribution.downloadURL)
         
-df = df.replace({'Product' : {
-    'a-products-of-agriculture-forestry-fishing' : 'products-of-agriculture-forestry-fishing',
-    'b-mining-quarrying' : 'mining-quarrying',
-    'c-manufactured-products' : 'manufactured-products',
-    'd-electricity-gas-steam-air-conditioning' : 'electricity-gas-steam-air-conditioning',
-    'e-water-supply-sewerage-waste-management' : 'water-supply-sewerage-waste-management',
-    'j-information-communication-services' : 'information-communication-services',
-    'm-professional-scientific-technical-services' : 'professional-scientific-technical-services',
-    'r-arts-entertainment-recreation' : 'arts-entertainment-recreation',
-    's-other-services' : 'other-services'}})
+        flow_direction = tab.name.split(' ', 1)[1]
+        
+        period = tab.excel_ref("C4").expand(RIGHT).is_not_blank()
+        product = tab.excel_ref("A5").expand(DOWN).is_not_blank() 
+        cdid = tab.excel_ref('B5').expand(DOWN).is_not_blank()
+        observations = tab.excel_ref('C5').expand(DOWN).expand(RIGHT).is_not_blank() 
 
-# +
-from IPython.display import display, HTML
-from io import BytesIO
-def fetch_table(t):
-    return BytesIO(scraper.session.get('https://github.com/ONS-OpenData/Ref_CDID/raw/master/lookup/' + t).content)
-cdids = pd.concat((
-    pd.read_csv(fetch_table(f'{k}.csv'),
-                       na_values=[''], keep_default_na=False, index_col=[0,7],
-                       dtype={'AREA': str, 'DIRECTION': str, 'BASIS': str,
-                              'PRICE': str, 'SEASADJ': str,
-                              'PRODUCT': str, 'COUNTRY': str},
-                       converters={'COMMODITY': lambda x: str(x).strip()})
-    for k in ['tig_cpa']), sort=False)
-for col in cdids:
-    cdids[col] = cdids[col].astype('category')
-cdids.index = cdids.index.get_level_values('cdid')
+        dimensions = [
+            HDim(period, 'Period', DIRECTLY, ABOVE),
+            HDim(product, 'Product', DIRECTLY, LEFT),
+            HDim(cdid, 'CDID', CLOSEST, ABOVE),  
+            HDimConst('Flow Direction', flow_direction),
+            HDimConst('Measure Type', 'gbp total'),
+            HDimConst('Unit', 'gbp-million')
+            ]
 
-cdids.rename(columns={
-    'AREA': 'ONS Partner Geography',
-    'PRICE': 'Price Classification',
-    'SEASADJ': 'Seasonal Adjustment',
-    'BASIS': 'International Trade Basis'
-}, inplace=True)
-df = pd.merge(df, cdids, how = 'left', left_on = 'CDID', right_on = 'cdid')
-df = df.drop(columns=['PRODUCT', 'DIRECTION'])
-
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)   
+        trace.store("combined_dataframe", tidy_sheet.topandas())
 # -
 
-df = df[[ 'Period', 'CDID', 'International Trade Basis', 'Flow Directions','Product', 'Price Classification', 'Seasonal Adjustment','Value',]].drop_duplicates()
+df = trace.combine_and_trace(title, "combined_dataframe")
+
+# +
+# df = df[df['CDID'] != ' '] 
+# -
+
+df = df[df['OBS'] != 0]
+
+df.rename(columns={'OBS' : 'Value'}, inplace=True)
+df['Value'] = df['Value'].astype(int)
+df['Period'] = df['Period'].map(lambda x: 'year/' + left(x,4) if 'Q' not in x else 'quarter/' + left(x,4) + '-' + right(x,2))
+df['Product'] = df['Product'].map(lambda x: x.split(' ', 1)[0])
+
+df = df[['Period', 'Flow Direction', 'Product', 'CDID', 'Measure Type', 'Unit', 'Value']]
+df['Flow Direction'] = df['Flow Direction'].apply(pathify)
 df
 
-cubes.add_cube(scraper, df.drop_duplicates(), title)
+cubes.add_cube(scraper, df, title)
 cubes.output_all()
 
 trace.render("spec_v1.html")
+
+
