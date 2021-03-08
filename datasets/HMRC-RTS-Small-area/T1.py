751d14fff6071d1ce4f6677760c3e326504d2291
AndrewtGSScogs <75667265+AndrewtGSScogs@users.noreply.github.com>
2021-03-08 10:01:49 +0000

    replacing gbp-total with stats value

--- datasets/HMRC-RTS-Small-area/T1.py
+++ datasets/HMRC-RTS-Small-area/T1.py
@@ -45,8 +45,8 @@ Dimensions = [
             HDim(geography,'HMRC Partner Geography',DIRECTLY,LEFT),
             HDim(nut,'NUTS Geography',DIRECTLY,LEFT),
             HDimConst('SITC 4', 'all'),
-            HDimConst('Measure Type', 'GBP Total'),
-            HDimConst('Unit', 'gbp-million'),
+            HDimConst('Measure Type', 'Statistical Value'),
+            HDimConst('Unit', 'statistical-value'),
             HDimConst('Year', dataset_year)
             ]
 c1 = ConversionSegment(observations, Dimensions, processTIMEUNIT=True)
