cd98c83eb7a4ddb6b1b5066101f8c4604d828cf0
Rob Barry <rob.barry@ons.gov.uk>
2021-03-24 14:54:13 +0000

    Correcting PORT ID used in codelist.

--- datasets/HMRC-overseas-trade-statistics/codelists/generation-scripts/ports.py
+++ datasets/HMRC-overseas-trade-statistics/codelists/generation-scripts/ports.py
@@ -24,7 +24,7 @@ def main():
     ports_data = get_api_data(api_url)
     ports_codelist = pd.DataFrame({
         "Label": [f"{p['PortName']} ({p['PortCodeAlpha']})" for p in ports_data],
-        "Notation": [p["PortId"] for p in ports_data],
+        "Notation": [p["PortCodeNumeric"] for p in ports_data],
         "Parent Notation": [None for _ in ports_data],
         "Sort Priority": range(1, len(ports_data) + 1)
     })
