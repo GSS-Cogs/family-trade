c296e7facf64654a5727f9444ef49a294b7cdbc6
AndrewtGSScogs <75667265+AndrewtGSScogs@users.noreply.github.com>
2021-04-23 10:25:14 +0100

    commenting out supression marker

--- datasets/ONS-Trade-in-goods/main.py
+++ datasets/ONS-Trade-in-goods/main.py
@@ -171,8 +171,9 @@ table['Seasonal Adjustment'] = pd.Series('NSA', index=table.index, dtype='catego
 #table['Measure Type'] = pd.Series('gbp-million', index=table.index, dtype='category') 
 #table['Unit'] = pd.Series('gbp-million', index=table.index, dtype='category')
 
-table['Marker'] = ' '
-table.loc[(table['Value'] == 0), 'Marker'] = 'suppressed'
+#line not needed data does not need to be supressed
+#table['Marker'] = ' '
+#table.loc[(table['Value'] == 0), 'Marker'] = 'suppressed'
 
 table = table[['ONS Partner Geography','Period','Flow','Commodity','Seasonal Adjustment','Value','Marker']]
 table['Flow'] = table['Flow'].map(lambda x: pathify(x))
