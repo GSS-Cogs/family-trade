0c9d96844f31239240140a9241b15ba16d32290d
Shannon Black <shannon1995black@gmail.com>
2021-04-23 10:41:02 +0100

    current-prices not cp

--- datasets/ONS_MRETS/main.py
+++ datasets/ONS_MRETS/main.py
@@ -441,7 +441,7 @@ scraper.dataset.description = scraper.dataset.comment + ' Figures are to 0 decim
 
 with open("info.json", "r") as jsonFile:
     data = json.load(jsonFile)
-    data["transform"]["columns"]["Value"]["measure"] = "http://gss-data.org.uk/def/measure/cp"
+    data["transform"]["columns"]["Value"]["measure"] = "http://gss-data.org.uk/def/measure/current-prices"
     data["transform"]["columns"]["Value"]["unit"] = "http://gss-data.org.uk/def/concept/measurement-units/gbp-million"
     with open("info.json", "w") as jsonFile:
         json.dump(data, jsonFile)
