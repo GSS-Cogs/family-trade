ec3ff506f95b7f29b39cd2270bb7c3d0104bf4c8
santhosh thangavel <santhoshk.thangavel@gmail.com>
2021-11-30 17:30:20 +0000

    Transformation of tab 4. Industry_Age. To be completed

--- datasets/HMRC-trade-in-goods-by-business-characteristics/main.py
+++ datasets/HMRC-trade-in-goods-by-business-characteristics/main.py
@@ -46,46 +46,48 @@ for tab in tabs:
 
 tidied_sheets = []
 
-for tab in tabs:
-    # if tab.name in ["Notes and Contents", "2. Age Group","3. Business Size","4. Industry_Age", "5. Industry_BusinessSize", "6. BusinessSize_Age", "7. Metadata"]:
-    if tab.name not in ["1. Industry Group"]:
-        continue
-    # print(tab.name)
+# +
+# for tab in tabs:
+#     # if tab.name in ["Notes and Contents", "2. Age Group","3. Business Size","4. Industry_Age", "5. Industry_BusinessSize", "6. BusinessSize_Age", "7. Metadata"]:
+#     if tab.name not in ["1. Industry Group"]:
+#         continue
+#     # print(tab.name)
 
-    cell = tab.excel_ref("A1")
+#     cell = tab.excel_ref("A1")
 
-    del_row_21 = tab.filter("Total").expand(RIGHT).is_not_blank()|tab.filter("Total [note 9]").expand(RIGHT).is_not_blank()
+#     del_row_21 = tab.filter("Total").expand(RIGHT).is_not_blank()|tab.filter("Total [note 9]").expand(RIGHT).is_not_blank()
 
-    del_row_22 = del_row_21.fill(DOWN).is_blank()
+#     del_row_22 = del_row_21.fill(DOWN).is_blank()
 
-    del_row_23 = tab.filter(contains_string("by industry group")).expand(RIGHT).is_not_blank()
+#     del_row_23 = tab.filter(contains_string("by industry group")).expand(RIGHT).is_not_blank()
 
-    del_row_24 = tab.filter(contains_string("Export")).fill(LEFT)
+#     del_row_24 = tab.filter(contains_string("Export")).fill(LEFT)
 
-    del_row_25 = tab.filter(contains_string("Industry group")).expand(RIGHT)
+#     del_row_25 = tab.filter(contains_string("Industry group")).expand(RIGHT)
 
-    del_row_54 = tab.filter(contains_string("Source")).expand(DOWN)
+#     del_row_54 = tab.filter(contains_string("Source")).expand(DOWN)
 
-    del_row_56 = tab.filter(contains_string("Notes")).expand(DOWN)
+#     del_row_56 = tab.filter(contains_string("Notes")).expand(DOWN)
 
-    total_del = del_row_21|del_row_22|del_row_23|del_row_24|del_row_25|del_row_54|del_row_56
+#     total_del = del_row_21|del_row_22|del_row_23|del_row_24|del_row_25|del_row_54|del_row_56
 
-    flow = tab.filter("Exports")|tab.filter("Imports")
+#     flow = tab.filter("Exports")|tab.filter("Imports")
 
-    industry_group = cell.shift(0,8).fill(DOWN).is_not_blank().is_not_whitespace()-total_del
+#     industry_group = cell.shift(0,8).fill(DOWN).is_not_blank().is_not_whitespace()-total_del
 
-    count = tab.filter(contains_string("by industry group")) #Can be used latter for multimeasure extraction
+#     count = tab.filter(contains_string("by industry group")) #Can be used latter for multimeasure extraction
 
-    observations = flow.waffle(industry_group)-total_del
-    # savepreviewhtml(observations, fname=tab.name +"Preview.html")
+#     observations = flow.waffle(industry_group)-total_del
+#     # savepreviewhtml(observations, fname=tab.name +"Preview.html")
     
-    dimensions = [
-        HDim(flow, "Flow", DIRECTLY, ABOVE),
-        HDim(industry_group, "Industry Group", DIRECTLY, LEFT),
-        HDim(count, "Count", CLOSEST, ABOVE)
-    ]
-    tidy_sheet = ConversionSegment(tab, dimensions, observations)
-    tidied_sheets.append(tidy_sheet.topandas())
+#     dimensions = [
+#         HDim(flow, "Flow", DIRECTLY, ABOVE),
+#         HDim(industry_group, "Industry Group", DIRECTLY, LEFT),
+#         HDim(count, "Count", CLOSEST, ABOVE)
+#     ]
+#     tidy_sheet = ConversionSegment(tab, dimensions, observations)
+#     tidied_sheets.append(tidy_sheet.topandas())
+# -
 
 for tab in tabs:
     if tab.name not in ["2. Age Group"]:
@@ -165,5 +167,45 @@ for tab in tabs:
     tidy_sheet = ConversionSegment(tab, dimensions, observations)
     tidied_sheets.append(tidy_sheet.topandas())
 
-df = pd.concat(tidied_sheets, ignore_index = True, sort = False)
-df
+for tab in tabs:
+    if tab.name not in ["4. Industry_Age"]:
+        continue
+    # print(tab.name)
+
+    del_row_82 = tab.filter(contains_string("Grand Total [note 9]")).expand(RIGHT).expand(DOWN)
+    
+    # industry_group = tab.filter(contains_string("Industry group")).fill(DOWN).is_not_blank()-del_row_82
+
+    age = tab.filter(contains_string("Age (years)")).fill(DOWN).is_not_blank().is_not_whitespace()-del_row_82
+
+    industry_group = age.shift(LEFT).is_not_blank().is_not_whitespace()
+    # savepreviewhtml(industry_group, fname=tab.name+ "Preview.html")
+
+    flow = tab.filter(contains_string("Exports")).expand(RIGHT).is_not_blank().is_not_whitespace()
+
+    value = tab.filter(contains_string("Value £ m")).fill(DOWN).is_not_blank().is_not_whitespace()-del_row_82
+
+    business_count = tab.filter(contains_string("Business Count")).fill(DOWN).is_not_blank().is_not_whitespace()-del_row_82
+
+    employee_count = tab.filter(contains_string("Employee Count")).fill(DOWN).is_not_blank().is_not_whitespace()-del_row_82
+
+    # observations = value|business_count|employee_count
+
+    required_column_names = tab.filter(contains_string("Value £ m")).expand(RIGHT).is_not_blank().is_not_whitespace()
+
+    observations = age.waffle(required_column_names)
+
+    dimensions = [
+        HDim(industry_group, "Industry Group", CLOSEST, ABOVE),
+        HDim(age, "Age", DIRECTLY, LEFT),
+        # HDim(flow, "Flow", CLOSEST, ABOVE),
+        # HDim(value, "Value", DIRECTLY, ABOVE),
+        # HDim(business_count, "Business Count", DIRECTLY, ABOVE),
+        # HDim(employee_count, "Employee Count", DIRECTLY, ABOVE)
+    ]
+    tidy_sheet = ConversionSegment(tab, dimensions, observations)
+    tidied_sheets.append(tidy_sheet.topandas())
+
+# +
+# df = pd.concat(tidied_sheets, ignore_index = True, sort = False)
+# df
