afe3a99a58fcec7c86c03581ff5cfc71713b323e
santhosh thangavel <santhoshk.thangavel@gmail.com>
2021-12-17 13:22:45 +0000

    No major changes to code

--- datasets/ONS-Trade-in-goods/main.py
+++ datasets/ONS-Trade-in-goods/main.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.4.2
+#       jupytext_version: 1.13.1
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
@@ -209,5 +209,3 @@ for period in years.unique():
         cubes.add_cube(scraper1, table[years == period], csv_name, graph=info_json_dataset_id, override_containing_graph=graph_uri, suppress_catalog_and_dsd_output=True)
 
 cubes.output_all()
-
-
