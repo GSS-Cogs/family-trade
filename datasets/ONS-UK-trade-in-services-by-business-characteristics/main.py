e85832da9a1e09efa2af0d35f0c3c5524a215ff1
RedWalters <jonathan.j.walters@Gmail.com>
2022-01-05 15:30:33 +0000

    updating to transform the latest release and fixing broken pipeline, should also cover the issues raised by QA

--- datasets/ONS-UK-trade-in-services-by-business-characteristics/main.py
+++ datasets/ONS-UK-trade-in-services-by-business-characteristics/main.py
@@ -1,19 +1,9 @@
-# -*- coding: utf-8 -*-
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.4'
-#       jupytext_version: 1.1.1
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
+#!/usr/bin/env python
+# coding: utf-8
 
-# +
+# In[66]:
+
+
 from gssutils import *
 import json
 import numpy as np
@@ -24,7 +14,12 @@ cubes = Cubes("info.json")
 info = json.load(open('info.json'))
 scraper = Scraper(seed = 'info.json')
 scraper
-# -
+
+
+# In[67]:
+
+
+tidied_sheets = []
 
 distribution = scraper.distribution(latest = True)
 title = distribution.title
@@ -32,119 +27,262 @@ tabs = { tab.name: tab for tab in distribution.as_databaker() }
 
 for name, tab in tabs.items():
     datasetTitle = title
-    columns=['Period','Business Size','Ownership', 'Country','Industry', 'Flow', 'Measure type', 'Unit', 'Marker']
-    trace.start(datasetTitle, tab, columns, distribution.downloadURL)
-    
+
     period = tab.name[0:4]
-    trace.Period("Taken from tab name")
-        
+
     if 'Contents' in name:
-        continue   
-    elif len(tab.name) > 4:
-        
+        continue
+    elif tab.name in ['SIC by Ownership', 'SIC by Size']:
+
+        print(tab.name)
+
+        pivot = tab.excel_ref('A3')
+
         country = 'World'
-        trace.Country("Hardcodded as World")
-        
-        #Ownership
-        industry = tab.excel_ref('A2').expand(DOWN)
-        trace.Industry("Taken from columnns with title Industry down. A2 Down")
-        flow = tab.excel_ref('C1').expand(RIGHT) - tab.excel_ref('E1').expand(RIGHT) 
-        trace.Flow("Taken from headers Exports £m or Imports £m")
-        ownership = tab.filter(contains_string('Ownership')).shift(0,1).expand(DOWN)
-        trace.Ownership("Ownership taken from colum headed Ownership down")
-        business_size = 'all'
-        trace.Business_Size("Hardcodded as All")
-        observations_ownership = (tab.excel_ref('C1').expand(RIGHT) - tab.excel_ref('E1').expand(RIGHT)).fill(DOWN).is_not_blank()
-        
-        dimensions = [
-            HDimConst("Period", period),
-            HDimConst("Business size", business_size),
-            HDimConst("Country", country),
-            HDim(industry,'Industry',DIRECTLY,LEFT),
-            HDim(flow, 'Flow',CLOSEST,LEFT),
-            HDim(ownership,'Ownership',DIRECTLY,LEFT),
-        ]
-        tidy_sheet = ConversionSegment(tab, dimensions, observations_ownership)   
-        trace.with_preview(tidy_sheet)
-        trace.store("combined_dataframe", tidy_sheet.topandas())
-        
-        #Business Size
-        industry = tab.excel_ref('F2').expand(DOWN)
-        trace.Industry("Taken from columnns with title Industry down. F2 Down")
-        flow = tab.excel_ref('H1').expand(RIGHT)
-        trace.Flow("Taken from headers Exports £m or Imports £m")
-        business_size = tab.filter(contains_string('Business Size')).shift(0,1).expand(DOWN)
-        trace.Business_Size("Taken from colum headed Business Size down")
-        ownership = 'any'
-        trace.Ownership("Hardcodded as Any")
-        observations_business_size = (tab.excel_ref('H1').expand(RIGHT)).fill(DOWN).is_not_blank()
-        dimensions = [
-            HDimConst("Period", period),
-            HDimConst("Ownership", ownership),
-            HDimConst("Country", country),
-            HDim(industry,'Industry',DIRECTLY,LEFT),
-            HDim(flow, 'Flow',DIRECTLY,ABOVE),
-            HDim(business_size,'Business size',DIRECTLY,LEFT),
-        ]
-        tidy_sheet = ConversionSegment(tab, dimensions, observations_business_size)   
-        trace.with_preview(tidy_sheet)
-        trace.store("combined_dataframe", tidy_sheet.topandas())
-        
-    elif len(tab.name) == 4:
-        country = tab.filter(contains_string('Country')).shift(0,1).expand(DOWN).is_not_blank()
-        trace.Country("Taken from columns with header Country : A11 Down. The first box in tab will have world as country value.")
-        business_size = tab.filter(contains_string('Business Size')).shift(0,1).expand(DOWN) - country
-        trace.Business_Size("Taken from columns with header Buiness Size :B2 Down and B11 Down. The second second box on the tab will have any as the business size value")            
-        ownership = tab.filter(contains_string('Ownership')).shift(0,1).expand(DOWN) - business_size
-        trace.Ownership("Taken from columns with header Ownership :A2 Down and B21 Down. The second 3rd box on the tab will have any as the Ownership value")            
-        flow = tab.expand(DOWN).one_of(['Exports £m','Imports £m'])
-        trace.Flow("Taken from headers Exports £m or Imports £m")
-        industry = "all"
-        trace.Industry("Hardcodded as all")
-        observations = flow.fill(DOWN).is_not_blank() - flow
-        
-        dimensions = [
-            HDimConst("Period", period),
-            HDimConst("Industry", industry),
-            HDim(flow, 'Flow',DIRECTLY,ABOVE),
-            HDim(business_size, 'Business size',CLOSEST,ABOVE),
-            HDim(ownership, 'Ownership',CLOSEST,ABOVE),
-            HDim(country, 'Country',CLOSEST,ABOVE),
-        ]
+
+        industry = pivot.fill(DOWN).is_not_blank()
+
+        if 'ownership' in tab.name.lower():
+            ownership = pivot.shift(2, 0).fill(DOWN).is_not_blank()
+        else:
+            ownership = 'unknown'
+
+        flow = pivot.shift(3, 0).fill(DOWN).is_not_blank()
+
+        period = pivot.shift(4, 0).expand(RIGHT).is_not_blank()
+
+        if 'ownership' in tab.name.lower():
+            business_size = 'all'
+        else:
+            business_size = pivot.shift(2, 0).fill(DOWN).is_not_blank()
+
+        measure_type = 'value-of-trade'
+
+        unit = 'gbp-million'
+
+        observations = period.fill(DOWN).is_not_blank()
+
+        if 'ownership' in tab.name.lower():
+            dimensions = [
+                HDim(period, "Period", DIRECTLY, ABOVE),
+                HDimConst("Business Size", business_size),
+                HDimConst("Country", country),
+                HDim(industry, 'Industry', DIRECTLY, LEFT),
+                HDim(flow, 'Flow', DIRECTLY, LEFT),
+                HDim(ownership, 'Ownership', DIRECTLY, LEFT),
+                HDimConst('Measure Type', measure_type),
+                HDimConst('Unit', unit)
+            ]
+        else:
+            dimensions = [
+                HDim(period, "Period", DIRECTLY, ABOVE),
+                HDim(business_size, 'Business Size', DIRECTLY, LEFT),
+                HDimConst("Country", country),
+                HDim(industry, 'Industry', DIRECTLY, LEFT),
+                HDim(flow, 'Flow', DIRECTLY, LEFT),
+                HDimConst("Ownership", ownership),
+                HDimConst('Measure Type', measure_type),
+                HDimConst('Unit', unit)
+            ]
+
         tidy_sheet = ConversionSegment(tab, dimensions, observations)
-    else:   
+        df = tidy_sheet.topandas()
+        tidied_sheets.append(df)
+        savepreviewhtml(tidy_sheet,fname=tab.name + " Preview.html")
+
+    elif tab.name in ['SIC Count']:
+
+        print(tab.name)
+
+        pivot = tab.excel_ref('A3')
+
+        country = 'World'
+
+        industry = pivot.fill(DOWN).is_not_blank()
+
+        ownership = 'unknown'
+
+        flow = pivot.shift(2, 0).fill(DOWN).is_not_blank()
+
+        period = pivot.shift(3, 0).expand(RIGHT).is_not_blank()
+
+        business_size = 'all'
+
+        measure_type = 'firms-trading'
+
+        unit = 'count'
+
+        observations = period.fill(DOWN).is_not_blank()
+
+        dimensions = [
+            HDim(period, "Period", DIRECTLY, ABOVE),
+            HDimConst("Business Size", business_size),
+            HDimConst("Country", country),
+            HDim(industry, 'Industry', DIRECTLY, LEFT),
+            HDim(flow, 'Flow', DIRECTLY, LEFT),
+            HDimConst("Ownership", ownership),
+            HDimConst('Measure Type', measure_type),
+            HDimConst('Unit', unit)
+        ]
+
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        df = tidy_sheet.topandas()
+        tidied_sheets.append(df)
+        savepreviewhtml(tidy_sheet,fname=tab.name + " Preview.html")
+
+    elif tab.name in ['Size by Ownership', 'Size Ownership Count']:
+
+        print(tab.name)
+
+        pivot = tab.excel_ref('A3')
+
+        country = 'World'
+
+        industry = 'all'
+
+        ownership = pivot.shift(RIGHT).fill(DOWN).is_not_blank()
+
+        flow = pivot.shift(2, 0).fill(DOWN).is_not_blank()
+
+        period = pivot.shift(3, 0).expand(RIGHT).is_not_blank()
+
+        business_size = pivot.fill(DOWN).is_not_blank()
+
+        if 'count' in tab.name.lower():
+            measure_type = 'firms-trading'
+
+            unit = 'count'
+
+        else:
+            measure_type = 'value-of-trade'
+
+            unit = 'gbp-million'
+
+        observations = period.fill(DOWN).is_not_blank()
+
+        dimensions = [
+            HDim(period, "Period", DIRECTLY, ABOVE),
+            HDim(business_size, "Business Size", DIRECTLY, LEFT),
+            HDimConst("Country", country),
+            HDimConst( 'Industry', industry),
+            HDim(flow, 'Flow', DIRECTLY, LEFT),
+            HDim(ownership, "Ownership", DIRECTLY, LEFT),
+            HDimConst('Measure Type', measure_type),
+            HDimConst('Unit', unit)
+        ]
+
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        df = tidy_sheet.topandas()
+        tidied_sheets.append(df)
+        savepreviewhtml(tidy_sheet,fname=tab.name + " Preview.html")
+
+    elif tab.name in ['Region by Ownership', 'Region Ownership Count', 'Region by Size', 'Region Size Count']:
+
+        print(tab.name)
+
+        pivot = tab.excel_ref('A3')
+
+        country = pivot.fill(DOWN).is_not_blank()
+
+        industry = 'all'
+
+        if 'size' in tab.name.lower():
+            ownership = 'all'
+        else:
+            ownership = pivot.shift(RIGHT).fill(DOWN).is_not_blank()
+
+        flow = pivot.shift(2, 0).fill(DOWN).is_not_blank()
+
+        period = pivot.shift(3, 0).expand(RIGHT).is_not_blank()
+
+        if 'size' in tab.name.lower():
+            business_size = pivot.shift(RIGHT).fill(DOWN).is_not_blank()
+        else:
+            business_size = 'all'
+
+        if 'count' in tab.name.lower():
+            measure_type = 'firms-trading'
+
+            unit = 'count'
+
+        else:
+            measure_type = 'value-of-trade'
+
+            unit = 'gbp-million'
+
+        observations = period.fill(DOWN).is_not_blank()
+
+        if 'size' in tab.name.lower():
+            dimensions = [
+                HDim(period, "Period", DIRECTLY, ABOVE),
+                HDim(business_size, "Business Size", DIRECTLY, LEFT),
+                HDim(country, "Country", DIRECTLY, LEFT),
+                HDimConst('Industry', industry),
+                HDim(flow, 'Flow', DIRECTLY, LEFT),
+                HDimConst("Ownership", ownership),
+                HDimConst('Measure Type', measure_type),
+                HDimConst('Unit', unit)
+            ]
+
+        else:
+
+            dimensions = [
+                HDim(period, "Period", DIRECTLY, ABOVE),
+                HDimConst("Business Size", business_size),
+                HDim(country, "Country", DIRECTLY, LEFT),
+                HDimConst('Industry', industry),
+                HDim(flow, 'Flow', DIRECTLY, LEFT),
+                HDim(ownership, "Ownership", DIRECTLY, LEFT),
+                HDimConst('Measure Type', measure_type),
+                HDimConst('Unit', unit)
+            ]
+
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        df = tidy_sheet.topandas()
+        tidied_sheets.append(df)
+        savepreviewhtml(tidy_sheet,fname=tab.name + " Preview.html")
+
+    else:
         continue
 
 
-# +
-#Post Processing 
-df = trace.combine_and_trace(datasetTitle, "combined_dataframe")
-df.rename(columns={'OBS' : 'Value','DATAMARKER' : 'Marker'}, inplace=True)
-df['Marker'].replace('..', 'suppressed', inplace=True)
-trace.Marker("Formatting .. to equal suppressed")
-df['Period'] =  'year/' + df['Period']
-trace.Period("Formatting to year/0000")
-df['Business size'].replace('Country', 'any', inplace=True)
-df['Ownership'].replace('Business Size', 'any', inplace=True)
-df['Country'].replace(np.nan, 'World', inplace=True)
-df["Flow"] = df["Flow"].map(lambda x: "exports" if x == "Exports £m" else ("imports" if x == "Imports £m" else "unknown"))
-df["Business size"] = df["Business size"].map(lambda x: "0-to-49" if x == "Small (0-49)" 
-                                              else ("50-to-249" if x == "Medium (50-249)" 
-                                                    else ("250" if x == "Large (250+)" 
-                                                          else ("unknown-employees" if x == "Unknown" else "any"))))   
-df["Ownership"] = df["Ownership"].map(lambda x: "uk" if x == "Domestic" 
-                                                         else ("foreign" if x == "Foreign" 
-                                                               else ("unknown" if x == "Unknown" else "any")))                                                          
+# In[73]:
 
-df['Country'] = df['Country'].apply(lambda x: 'WW' if 'World' in x else 
-                                      ('RW' if 'Non-EU' in x else 
-                                       ('EU' if 'Total EU28' in x else x)))
+
+#Post Processing
+df = pd.concat(tidied_sheets)
+df.rename(columns={'OBS' : 'Value','DATAMARKER' : 'Marker'}, inplace=True)
+df['Period'] =  'year/' + df['Period']
+df['Value'] = df.apply(lambda x: '0.0' if 'Suppressed' in str(x['Marker']) else x['Value'], axis = 1)
+
+df['Marker'] = df['Marker'].str.replace('Suppressed', 'suppressed')
+
+df["Business Size"] = df["Business Size"].map(lambda x: "0-to-49" if x == "Small (0-49)"
+                                             else ("50-to-249" if x == "Medium (50-249)"
+                                             else ("250" if x == "Large (250+)"
+                                             else ("unknown-employees" if x == "Unknown" else "any"))))
+
+df['Country'] = df['Country'].apply(lambda x: 'WW' if 'World' in x
+                                    else ('RW' if 'Non-EU' in x
+                                    else ('EU' if 'Total EU' in x else x)))
+
+df["Ownership"] = df["Ownership"].map(lambda x: "uk" if x == "Domestic"
+                                    else ("foreign" if x == "Foreign"
+                                    else ("unknown" if x == "Unknown" else "any")))
+
 
 df['Value'] = pd.to_numeric(df['Value'], errors='coerce').astype('Int64')
-df['Industry']= df['Industry'].str.split(" ", n = 1, expand = True) 
 
-df = df[['Period', 'Business size', 'Country', 'Ownership', 'Industry', 'Flow', 'Value', 'Marker']]
-# -
+
+df['Flow'] = df['Flow'].apply(pathify)
+
+df = df[['Period', 'Business Size', 'Country', 'Ownership', 'Industry', 'Flow', 'Value', 'Marker', 'Measure Type', 'Unit']]
+
+df
+
+
+# In[74]:
+
 
 #additional scraper info needed
 comment = "Trade in goods data, including breakdown of imports and exports by Standard Industrial Classification, region (EU and non-EU), business size and by domestic and foreign ownership."
@@ -159,7 +297,7 @@ Business size is defined using the following employment size bands:
    Medium - 50-249 employees
    Large - 250+ employees
    Unknown - number of employees cannot be determined via IDBR
-   
+
 Ownership status is defined as:
    Domestic - ultimate controlling parent company located in the UK
    Foreign - ultimate controlling parent company located outside the UK
@@ -173,7 +311,7 @@ All data is in £ million, current prices
 Rounding
 Some of the totals within this release (e.g. EU, Non EU and world total) may not exactly match data published via other trade releases due to small rounding differences.
 
-Trade Asymmetries 
+Trade Asymmetries
 These data are our best estimate of these bilateral UK trade flows. Users should note that alternative estimates are available, in some cases, via the statistical agencies for bilateral countries or through central databases such as UN Comtrade
 
 """
@@ -190,31 +328,10 @@ for c in df.columns:
 
 cubes.add_cube(scraper, df.drop_duplicates(), datasetTitle)
 cubes.output_all()
-trace.render("spec_v1.html")
 
-import pandas as pd
-df = pd.read_csv("out/uk-trade-in-services-by-business-characteristics.csv")
-df["all_dimensions_concatenated"] = ""
-for col in df.columns.values:
-    if col != "Value":
-        df["all_dimensions_concatenated"] = df["all_dimensions_concatenated"]+df[col].astype(str)
-found = []
-bad_combos = []
-for item in df["all_dimensions_concatenated"]:
-    if item not in found:
-        found.append(item)
-    else:
-        bad_combos.append(item)
-df = df[df["all_dimensions_concatenated"].map(lambda x: x in bad_combos)]
-drop_these_cols = []
-for col in df.columns.values:
-    if col != "all_dimensions_concatenated" and col != "Value":
-        drop_these_cols.append(col)
-for dtc in drop_these_cols:
-    df = df.drop(dtc, axis=1)
-df = df[["all_dimensions_concatenated", "Value"]]
-df = df.sort_values(by=['all_dimensions_concatenated'])
-df.to_csv("./out/duplicates_with_values.csv", index=False)
-print("DONE")
+
+# In[ ]:
+
+
 
 
