c40318268b5ef4e0f906b981219bc27faec4993d
santhosh thangavel <santhoshthangavel@Santhoshs-MacBook-Pro.local>
2022-03-02 17:44:03 +0000

    transformation rewritten to be roboust

--- datasets/ONS-International-trade-in-services-by-subnational-areas-of-the-UK/main.py
+++ datasets/ONS-International-trade-in-services-by-subnational-areas-of-the-UK/main.py
@@ -7,7 +7,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.11.1
+#       jupytext_version: 1.13.7
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
@@ -18,107 +18,96 @@
 from gssutils import * 
 import json 
 
-trace = TransformTrace()
-df = pd.DataFrame()
-cubes = Cubes("info.json")
-
 info = json.load(open('info.json')) 
-scraper = Scraper(seed="info.json")   
-distribution = scraper.distribution(latest=True)
-title = distribution.title
-tabs = { tab.name: tab for tab in distribution.as_databaker() }
-distribution
+metadata = Scraper(seed="info.json")
+# metadata  
 # -
 
-# Remove all sheets except for `9. Tidy Data` and `8. Travel`.
-tabs_to_transform =['8. Travel', '9. Tidy format']
-for name, tab in tabs.items():
-    
-    columns = ["Year", "NUTS1 Area", "Travel Type", "Origin", "Includes Travel", "Industry Grouping", "Flow", "Measure Type", "Unit"]
-    trace.start(title, tab, columns, distribution.downloadURL)
-    
-    if name in tabs_to_transform:
-        #dimensions shared on both tabs
-        year = tab.excel_ref('A1').is_not_blank() #Period
-        trace.Year("Selected as non-blank value in cell A1 (sheet title)")
-        
-        if name in tabs_to_transform[0]:
-            nuts1_area = tab.excel_ref("A5:A17").is_not_blank() #Location
-            trace.NUTS1_Area("Selected as all non-blank values between cell refs A5 and A17")
-            
-            travel_type = tab.excel_ref("B3").expand(RIGHT).is_not_blank()
-            trace.Travel_Type("Selected as all non-blank values from cell ref B3 going right/across.")
-            
-            origin = tab.excel_ref("B4").expand(RIGHT).is_not_blank()
-            trace.Origin("Selected as all non-blank values from cell ref B4 going right/across.")
-            
-            includes_travel = 'includes-travel'
-            trace.Includes_Travel('Hard coded as includes-travel')
-            
-            industry_grouping = 'travel-related-trade'
-            trace.Industry_Grouping('Hardcoded as travel-related-trade')
-            
-            flow = 'imports'
-            trace.Flow('Hardcoded as imports')
-            
-            observations = origin.fill(DOWN).is_not_blank()
-            
-            dimensions = [
-                HDim(year, "Period", CLOSEST, LEFT),
-                HDim(nuts1_area, "Location", CLOSEST, ABOVE),
-                HDim(travel_type, "Travel Type", CLOSEST, LEFT),
-                HDim(origin, "Origin", DIRECTLY, ABOVE),
-                HDimConst("Includes Travel", includes_travel),
-                HDimConst("Industry Grouping", industry_grouping),
-                HDimConst("Flow", flow),
-            ]
-            tidy_sheet = ConversionSegment(tab, dimensions, observations)
-            #savepreviewhtml(tidy_sheet, fname=tab.name + "Preview.html")
-            trace.with_preview(tidy_sheet)
-            trace.store("combined_df", tidy_sheet.topandas())
-           
-        elif name in tabs_to_transform[1]:
-            nuts_level = tab.excel_ref('A5').expand(DOWN).is_not_blank() #used to determine Travel Type and Includes Travel columns 
-            
-            nuts_code = tab.excel_ref('B5').expand(DOWN).is_not_blank() #Location
-            
-            nuts_area_name = tab.excel_ref('C5').expand(DOWN).is_not_blank()
-            
-            industry_grouping = tab.excel_ref('D5').expand(DOWN).is_not_blank()
-            
-            origin = tab.excel_ref('E5').expand(DOWN).is_not_blank()
-            
-            flow = tab.excel_ref('F5').expand(DOWN).is_not_blank()
-            
-            observations = tab.excel_ref('G5').expand(DOWN).is_not_blank()
-            
-            dimensions = [
-                HDim(year, "Period", CLOSEST, LEFT),
-                HDim(nuts_code, "Location", DIRECTLY, LEFT),
-                HDim(nuts_level, "nuts_level", DIRECTLY, LEFT),
-                HDim(nuts_area_name, "nuts_area_name", DIRECTLY, LEFT),
-                HDim(origin, "Origin", DIRECTLY, LEFT),
-                HDim(industry_grouping, "Industry Grouping", DIRECTLY, LEFT),
-                HDim(flow, "Flow", DIRECTLY, LEFT),
-                
-            ]
-            tidy_sheet = ConversionSegment(tab, dimensions, observations)
-            #savepreviewhtml(tidy_sheet, fname=tab.name + "Preview.html")
-            trace.with_preview(tidy_sheet)
-            tidy_sheet = tidy_sheet.topandas()
-            tidy_sheet['Travel Type'] = tidy_sheet.apply(lambda x: 'total' if (x['nuts_level'] == 'NUTS1' and x['Industry Grouping'] == 'Travel') else 'na', axis = 1)
-            tidy_sheet['Includes Travel'] = tidy_sheet['nuts_level'].map(lambda x: 'includes-travel' if 'NUTS1' in x else 'excludes-travel')
-            tidy_sheet['Location'] = tidy_sheet.apply(lambda x: 'http://data.europa.eu/nuts/code/' + x['Location'] if x['Location'] != 'N/A' else x['Location'], axis = 1) 
-            tidy_sheet['Location'] = tidy_sheet.apply(lambda x: 'http://data.europa.eu/nuts/code/UK' if (x['Location'] == 'N/A' and x['nuts_area_name'] == 'United Kingdom') else x['Location'], axis = 1)
-            tidy_sheet['Location'] = tidy_sheet.apply(lambda x: x['nuts_area_name'] if x['Location'] == 'N/A' else x['Location'], axis = 1)
-            tidy_sheet = tidy_sheet.drop(['nuts_level', 'nuts_area_name'], axis=1)
-            trace.store("combined_df", tidy_sheet) 
-    else:
-        continue
+distribution = metadata.distribution(latest=True)
+title = distribution.title
+tabs = { tab.name: tab for tab in distribution.as_databaker() }
+# distribution
 
-df = trace.combine_and_trace(distribution.title, "combined_df")
+# +
+# tab = [tab for name, tab in tabs.items()]
+# -
+
+total_tabs = {tab_name for tab_name in tabs}
+total_tabs
+
+tabs_to_transform =['8. Travel', '9. Tidy format']
+
+if len(set(tabs_to_transform)-(total_tabs)) != 2:
+    raise ValueError(f"Aborting. A tab named{set(tabs_to_transform)-(total_tabs)} required but not found")
+
+for name, tab in tabs.items():
+    print(name)
+
+tidied_sheets =[]
+
+tab = tabs["8.Travel"]
+footer = tab.filter(contains_string("Sources: UK Trade; International Passenger Survey")).expand(DOWN)
+year = tab.filter(contains_string("Total value of travel"))
+nuts1_area = tab.filter("NUTS1 area").fill(DOWN).is_not_blank().is_not_whitespace()-footer
+travel_type = tab.filter("Personal travel-related").expand(RIGHT).is_not_blank().is_not_whitespace()
+origin = tab.filter("EU").expand(RIGHT).is_not_blank().is_not_whitespace()
+includes_travel = 'includes-travel'
+industry_grouping = 'travel-related-trade'
+flow = 'imports'
+observations = nuts1_area.waffle(origin)
+dimensions = [
+    HDim(year, "Period", CLOSEST, LEFT),
+    HDim(nuts1_area, "Location", CLOSEST, ABOVE),
+    HDim(travel_type, "Travel Type", CLOSEST, LEFT),
+    HDim(origin, "Origin", DIRECTLY, ABOVE),
+    HDimConst("Includes Travel", includes_travel),
+    HDimConst("Industry Grouping", industry_grouping),
+    HDimConst("Flow", flow),
+    ]
+tidy_sheet = ConversionSegment(tab, dimensions, observations)
+# savepreviewhtml(tidy_sheet, fname=tab.name + "Preview.html")
+tidied_sheets.append(tidy_sheet.topandas())
+
+
+tab = tabs["9.Tidy format"]
+year = tab.filter(contains_string("Total value of trade"))
+nuts_level = tab.filter(contains_string("ITL level")).fill(DOWN).is_not_blank().is_not_whitespace()
+nuts_code = tab.filter("ITL code").fill(DOWN).is_not_blank().is_not_whitespace()
+nuts_area_name = tab.filter("ITL name").fill(DOWN).is_not_blank().is_not_whitespace()
+flow_direction = tab.filter("Direction of trade").fill(DOWN).is_not_blank().is_not_whitespace()
+origin = tab.filter("Country destination").fill(DOWN).is_not_blank().is_not_whitespace()
+industry_grouping = tab.filter("Industry").fill(DOWN).is_not_blank().is_not_whitespace()
+observations = tab.filter("Value").fill(DOWN).is_not_blank().is_not_whitespace()
+dimensions = [
+    HDim(year, "Period", CLOSEST, LEFT),
+    HDim(nuts_code, "Location", DIRECTLY, LEFT),
+    HDim(nuts_level, "nuts_level", DIRECTLY, LEFT),
+    HDim(nuts_area_name, "nuts_area_name", DIRECTLY, LEFT),
+    HDim(flow_direction, "Flow", DIRECTLY, LEFT),
+    HDim(origin, "Origin", DIRECTLY, LEFT),
+    HDim(industry_grouping, "Industry Grouping", DIRECTLY, LEFT)
+    ]
+tidy_sheet = ConversionSegment(tab, dimensions, observations)
+# savepreviewhtml(tidy_sheet, fname=tab.name + "Preview.html")
+tidied_sheets.append(tidy_sheet.topandas())
+
+df = pd.concat(tidied_sheets, sort = True).fillna('')
+
+# +
+# df
+# -
+
+df['Travel Type'] = df.apply(lambda x: 'total' if (x['nuts_level'] == 'NUTS1' and x['Industry Grouping'] == 'Travel') else 'not-applicable', axis = 1)
+df['Includes Travel'] = df['nuts_level'].map(lambda x: 'includes-travel' if 'NUTS1' in x else 'excludes-travel')
+df['Location'] = df.apply(lambda x: 'http://data.europa.eu/nuts/code/' + x['Location'] if x['Location'] != 'N/A' else x['Location'], axis = 1) 
+df['Location'] = df.apply(lambda x: 'http://data.europa.eu/nuts/code/UK' if (x['Location'] == 'N/A' and x['nuts_area_name'] == 'United Kingdom') else x['Location'], axis = 1)
+df['Location'] = df.apply(lambda x: x['nuts_area_name'] if x['Location'] == 'N/A' else x['Location'], axis = 1)
+df = df.drop(['nuts_level', 'nuts_area_name'], axis=1)
+
 df.rename(columns= {'OBS':'Value', 'DATAMARKER' : 'Marker'}, inplace=True)
 df['Marker'] = df['Marker'].replace('..', 'suppressed')
+df["Marker"] = df["Marker"].str.replace("n/a", "not-applicable")
+df["Flow"] = df["Flow"].str.replace("imports", "Imports")
 df["Period"]= df["Period"].str.split(",", n = 1, expand = True)[1]
 df['Period'] = df['Period'].str.strip()
 df['Period'] = df.apply(lambda x: 'year/' + x['Period'], axis = 1)
@@ -161,19 +150,10 @@ df = df.replace({'Location' : {'North East' : 'http://data.europa.eu/nuts/code/U
                 'Industry Grouping' : {'travel': 'travel-related-trade', 'Travel' : 'travel-related-trade'}
                 })
 
-COLUMNS_TO_NOT_PATHIFY = ['Value', 'Location', 'Period', 'Marker']
-for col in df.columns.values.tolist():
-    if col in COLUMNS_TO_NOT_PATHIFY:
-        continue
-    try:
-        df[col] = df[col].apply(pathify)
-    except Exception as err:
-        raise Exception('Failed to pathify column "{}".'.format(col)) from err
-
-df = df[['Period', 'Location', 'Industry Grouping', 'Origin', 'Flow', 'Travel Type', 'Includes Travel', 'Value', 'Marker']]
 df
 
+df['Travel Type'].unique()
 
-cubes.add_cube(scraper, df.drop_duplicates(), title)
-cubes.output_all()
-trace.render()
+df.to_csv("observations.csv", index = False)
+catalog_metadata = metadata.as_csvqb_catalog_metadata()
+catalog_metadata.to_json_file('catalog-metadata.json')
