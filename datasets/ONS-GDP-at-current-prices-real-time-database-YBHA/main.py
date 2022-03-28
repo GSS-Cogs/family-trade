093195bcd0d6d9ef5c5e3b295cbb0d04c08acdb8
Santhosh kumar <34086991+santhosh-thangavel@users.noreply.github.com>
2022-03-28 12:24:31 +0100

    ONS-GDP-at-current-prices-real-time-database-YBHA failed (#352)
    * removed trace function and cubes class from transformation
    
    * info.json updated
    
    * work in progress
    
    * trying to convert abbreviated months to respective quarters
    
    * converted monts to quarters
    
    * codelist mapping
    
    * post processing completed
    
    * code refactoring
    
    Co-authored-by: santhosh thangavel <santhoshthangavel@Santhoshs-MacBook-Pro.local>

--- datasets/ONS-GDP-at-current-prices-real-time-database-YBHA/main.py
+++ datasets/ONS-GDP-at-current-prices-real-time-database-YBHA/main.py
@@ -7,7 +7,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.3.3
+#       jupytext_version: 1.13.7
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
@@ -19,19 +19,13 @@ from gssutils import *
 import json 
 from urllib.parse import urljoin
 
-cubes = Cubes("info.json")
-trace = TransformTrace()
 df = pd.DataFrame()
 tidied_sheets = []
 # -
 
 info = json.load(open('info.json')) 
-scraper = Scraper(seed="info.json")   
-scraper 
-
-distribution = scraper.distributions[0]
-tabs = (t for t in distribution.as_databaker())
-
+metadata = Scraper(seed="info.json")   
+# metadata
 
 # +
 def left(s, amount):
@@ -52,91 +46,103 @@ def date_time(time_value):
 
 # -
 
+distribution = metadata.distributions[0]
+tabs = (t for t in distribution.as_databaker())
+
 for tab in tabs:
        
         datasetTitle = 'ONS-GDP-at-current-prices-real-time-database-YBHA'
-        columns=['Vintage','Publication','Estimate Type', 'CDID', 'Seasonal Adjustment', 'Marker']
-        trace.start(datasetTitle, tab, columns, scraper.distributions[0].downloadURL)
         
-        if (tab.name == '1989 - 1999') or (tab.name == '2000 - 2010') or (tab.name == '2011 - 2017') or (tab.name == '2018 -'):
+        if (tab.name == '1989 - 1999') or (tab.name == '2000 - 2010') or (tab.name == '2011 - 2017') or (tab.name == '2018 - '):
+            print(tab.name)
             
             seasonal_adjustment = 'SA'
-            trace.Seasonal_Adjustment("Hardcoded as SA, taken from source")
-            
-            code = tab.excel_ref('B3')
-            trace.CDID("Taken from cell ref B3")
-            
-            vintage = tab.excel_ref('A6').expand(DOWN).is_not_blank() 
-            trace.Vintage("Taken from cell A6 down")
-            
-            estimate_type = tab.excel_ref('B6').expand(RIGHT).is_not_blank()
-            trace.Estimate_Type("Taken from cell B6 across")
-            
-            publication = tab.excel_ref('B7').expand(RIGHT).is_not_blank()
-            trace.Publication("Taken from cell B7 across")
-                
-            if (tab.name == '2011 - 2017') or (tab.name == '2018 -'):
-                estimate_type = tab.excel_ref('B5').expand(RIGHT).is_not_blank()
-                trace.Estimate_Type("Taken from cell B5 across")
-                
-                publication = tab.excel_ref('B6').expand(RIGHT).is_not_blank()
-                trace.Publication("Taken from cell B6 across")
-        
-            observations = publication.fill(DOWN).is_not_blank()
-        
+
+            vintage = tab.filter("Publication date and time period").fill(DOWN).is_not_blank().is_not_whitespace()
+
+            estimate_type_publication = tab.filter("Publication date and time period").fill(RIGHT).is_not_blank().is_not_whitespace()
+
+            observations = vintage.waffle(estimate_type_publication).is_not_blank()
+
             dimensions = [
                 HDim(vintage, 'GDP Reference Period', DIRECTLY, LEFT),
-                HDim(estimate_type, 'GDP Estimate Type', DIRECTLY, ABOVE),
-                HDim(publication, 'Publication Date', DIRECTLY, ABOVE),
-                #HDim(code, 'CDID', CLOSEST, ABOVE), #dropped for now
-                #HDimConst('Seasonal Adjustment', seasonal_adjustment), #dropped for now
+                HDim(estimate_type_publication, 'GDP Estimate Type Publication', DIRECTLY, ABOVE)
             ]
-
-            tidy_sheet = ConversionSegment(tab, dimensions, observations)        
-            #savepreviewhtml(tidy_sheet, fname=tab.name + "Preview.html")
-            trace.with_preview(tidy_sheet)
-            trace.store("combined_dataframe", tidy_sheet.topandas())
+            tidy_sheet = ConversionSegment(tab, dimensions, observations)
+            # savepreviewhtml(tidy_sheet, fname=tab.name + "Preview.html")
+            tidied_sheets.append(tidy_sheet.topandas())
 
 
-df = trace.combine_and_trace(datasetTitle, "combined_dataframe")
-df.rename(columns={'OBS' : 'Value','DATAMARKER' : 'Marker'}, inplace=True)
-#df['CDID'] = df['CDID'].map(lambda x: right(x,4)) #dropped for now
+df = pd.concat(tidied_sheets, sort = True).fillna('')
+
+df[['Publication Date', 'GDP Estimate Type']] = df['GDP Estimate Type Publication'].str.rsplit(n=1, expand = True)
+
+df.drop(['GDP Estimate Type Publication'], axis = 1)
+df[['GDP Reference Period', 'Publication Date', 'GDP Estimate Type', 'OBS']]
+
 df['Publication Date'].replace('Q3  1990', 'Q3 1990', inplace=True)  #removing space
-df['Publication Date'].replace('Q 2004', 'Q4 2004', inplace=True) #fixing typo
-df['GDP Reference Period'].replace('Q2 1010', 'Q2 2010', inplace=True) #fixing typo
-df["GDP Reference Period"] = df["GDP Reference Period"].apply(date_time)
-df["Publication Date"] = df["Publication Date"].apply(date_time)
-df['Marker'].replace('..', 'unknown', inplace=True)
-df = df.fillna('')
+df['Publication Date'].replace('Q 2004', 'Q4 2004', inplace=True)
 
-df['Marker'].unique()
+df.rename(columns={'OBS' : 'Value'}, inplace=True)
 
-# info = json.load(open('info.json')) 
-# codelistcreation = info['transform']['codelists'] 
-# print(codelistcreation)
-# print("-------------------------------------------------------")
-#
-# codeclass = CSVCodelists()
-# for cl in codelistcreation:
-#     if cl in df.columns:
-#         df[cl] = df[cl].str.replace("-"," ")
-#         df[cl] = df[cl].str.capitalize()
-#         codeclass.create_codelists(pd.DataFrame(df[cl]), 'codelists', scraper.dataset.family, Path(os.getcwd()).name.lower())
+df['GDP Estimate Type'].unique()
+
+df[['Month', 'Year']] = df['Publication Date'].str.rsplit(pat = "-" ,n=1, expand = True)
+
+check_condition = ~(df['Year'].isna())
+df['Year'].where(check_condition, df['Month'], inplace = True)
+
+df[['Month', 'Year']] = df['Month'].str.rsplit(pat = ' ' ,n=1, expand = True)
+
+df[['again_month', 'again_year']] = df['Publication Date'].str.rsplit(pat = "-" ,n=1, expand = True)
+
+check_condition = ~(df['Year'].isna())
+df['Year'].where(check_condition, df['again_year'], inplace = True)
+
+df['again_month'] = df['again_month']+' '+df['again_year']
+
+df = df.drop(columns = ['GDP Estimate Type Publication', 'again_month', 'again_year'])
+
+df['Month'].replace('June', 'Jun', inplace = True)
+
 
 # +
-tidy = df[['GDP Reference Period','Publication Date','Value','GDP Estimate Type','Marker']]
-for column in tidy:
-    if column in ('GDP Estimate Type'):
-        tidy[column] = tidy[column].str.lstrip()
-        tidy[column] = tidy[column].str.rstrip()
-        tidy[column] = tidy[column].apply(pathify)
+def month_abrev_to_quarter(some_month_abrev_string):
+    month_dict = {"Jan": "Q1",
+              "Feb": "Q1",
+              "Mar": "Q1",
+              "Apr": "Q2",
+              "May": "Q2",
+              "Jun": "Q2",
+              "Jul": "Q3",
+              "Aug": "Q3",
+              "Sep": "Q3",
+              "Oct": "Q4",
+              "Nov": "Q4",
+              "Dec": "Q4"}
+    find_abrev = filter(lambda abrev_month: abrev_month in some_month_abrev_string, month_dict.keys())
+    for abrev in find_abrev:
+        some_month_abrev_string = some_month_abrev_string.replace(abrev, str(month_dict[abrev]))
+    return some_month_abrev_string
 
-tidy['GDP Estimate Type'] = tidy['GDP Estimate Type'].replace({"m1": "M1", "m2": "M2", "qna": "QNA"})
-
-cubes.add_cube(scraper, tidy, "GDP at current prices – real-time database (YBHA)")
-tidy
+df['Month'] = df['Month'].map(month_abrev_to_quarter)
 # -
 
+df['Publication Date'] = df['Month']+' '+df['Year']
 
-cubes.output_all()
-trace.render()
+df = df.drop(columns = ['Month', 'Year'])
+
+df.drop_duplicates(subset = df.columns.difference(['Value']), inplace = True)
+
+duplicate_df = df[df.duplicated(['GDP Reference Period', 'Value', 'Publication Date',
+       'GDP Estimate Type'], keep = False)]
+duplicate_df
+
+df["GDP Reference Period"] = df["GDP Reference Period"].apply(date_time)
+df["Publication Date"] = df["Publication Date"].apply(date_time)
+
+df[['GDP Reference Period', 'GDP Estimate Type', 'Publication Date', 'Value']]
+
+df.to_csv("observations.csv", index = False)
+catalog_metadata = metadata.as_csvqb_catalog_metadata()
+catalog_metadata.to_json_file('catalog-metadata.json')
