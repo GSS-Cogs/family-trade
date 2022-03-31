804853ffb7dca09d10053b12f75214782c3dab31
“rpm-data-dev” <richard.p.morgan@ons.gov.uk>
2022-03-31 15:16:06 +0000

    fixed the bug due to extra tabs and updated code
    Also I ran the file and it created the output files as expected

--- datasets/ONS-balance-of-payments/main.py
+++ datasets/ONS-balance-of-payments/main.py
@@ -4,10 +4,7 @@
 from gssutils import *
 import json
 import numpy as np
-from csvcubed.models.cube.qb.catalog import CatalogMetadata
 
-#TODO: look to remove the use of this trace object
-trace = TransformTrace()
 
 info_json_file = 'info.json'
 
@@ -37,6 +34,7 @@ class is_one_of(object):
         if xy_cell.value.strip() in self.allowed:
             return True
         return False   
+
 #Format Date/Quarter
 def left(s, amount):
     return s[:amount]
@@ -44,59 +42,60 @@ def left(s, amount):
 def right(s, amount):
     return s[-amount:]
 
-def date_time(date):
+def date_time(date: str) -> str:
+    '''
+    function to format numeric date to descriptive
+    e.g. '2022' becomes 'year/2022'
+    '''
     if len(date)  == 4:
         return 'year/' + date
     elif len(date) == 6:
         return 'quarter/' + left(date,4) + '-' + right(date,2)
     else:
         return "Date Formatting Error Unknown"
-
+# %%
 
 
 # get first landing page details
 metadata = Scraper(seed = info_json_file)
 # display(metadata) #  to see exactly the data we are loading
-
-
 # %%
+
+
 distribution = metadata.distribution(latest = True)
-datasetTitle = distribution.title
-tabs = { tab.name: tab for tab in distribution.as_databaker() }
+tabs = distribution.as_databaker()
 
-## TODO: [uncomment in future] keep tabs we're interested in
-#tabs = [x for x in tabs if x.name not in ('tig_ind_ex final', 'tig_ind_im') ]
+datasetTitle = distribution.title
+
+# keep tabs we're interested in. The Annex A and Annex B tabs weren't in  previous code so maybe new additions?
+tabs = [x for x in tabs if x.name not in ('Index', 'Records','Table R1', 'Table R2','Table R3','Annex A', 'Annex B') ]
+
+# In[23]:
+
+# to collate the transformed files later
+tidied_sheets = []
 
 # %%
-for name, tab in tabs.items():
+for tab in tabs:
     columns=['Period', 'CDID', 'Seasonal Adjustment', 'BOP Services', 'Flow Directions', 'Measure Type', 'Marker']
-    trace.start(datasetTitle, tab, columns, distribution.downloadURL)
-    if 'Index' in name or 'Records' in name or 'Table R1' in name or 'Table R2' in name or 'Table R3' in name:
-        continue 
 
-    print(name)
-
+    # example value: "BOKI"
     cdid = tab.excel_ref('C5').expand(DOWN).is_not_blank()
-    trace.CDID("CDID's taken from column C in tab")
 
     year = tab.excel_ref('C4').expand(RIGHT).is_not_blank()
     quarter = tab.excel_ref('D5').expand(RIGHT)
-    trace.Period("Taken from Year and Quarter ; rows 4 and 5")
-    
+
     flow = tab.excel_ref('B1').expand(DOWN).filter(is_one_of(["Imports", "Exports", "Balances", "Credits", "Debits", "Balances (net transactions)"])) | tab.excel_ref('A1')
-    trace.Flow_Directions("flow taken from tab values in column B as one of: Imports, Exports, Balances, Credits, Debits, Balances (net transactions) ")   
     
     seasonal_adjustment = tab.excel_ref('B').filter(is_one_of(["Seasonally adjusted", "Not seasonally adjusted"])) | tab.excel_ref('A1')
-    trace.Seasonal_Adjustment("Taken from column B as one of the following values: Seasonally Adjusted , Not Seasonally Adjusted")
     
     measure_type = tab.excel_ref('B1').is_not_blank()
-    trace.Measure_Type("Taken from tab title")
     
     services = tab.excel_ref('B').is_not_blank() - flow
     services = services - tab.excel_ref('B1').expand(UP)
     Currency = tab.excel_ref('N3')
     
-    if name == 'Table B' or name == 'Table BX':
+    if tab.name == 'Table B' or tab.name == 'Table BX':
         observations = services.waffle(year).is_not_blank() - tab.excel_ref('B67').expand(RIGHT).expand(DOWN)
     else:
         observations = services.waffle(year).is_not_blank()
@@ -110,16 +109,20 @@ for name, tab in tabs.items():
                 HDim(seasonal_adjustment,'Seasonal Adjustment',CLOSEST, ABOVE),
                 HDim(flow,'Flow Directions',CLOSEST,ABOVE)
     ]
-    tidy_sheet = ConversionSegment(tab, dimensions, observations)
-    savepreviewhtml(tidy_sheet, fname= name + "PREVIEW.html")
-    trace.with_preview(tidy_sheet)
-    trace.store("combined_dataframe", tidy_sheet.topandas())
-    
-    
+    cs = ConversionSegment(tab, dimensions, observations)
 
+    # # create preview files of each tabs data
+    # savepreviewhtml(cs, fname= tab.name + "PREVIEW.html")
+    
+    tidy_sheet = cs.topandas()
+    tidied_sheets.append(tidy_sheet)
+
+ # %%
+#convert separate tabs into one dataframe
+df = pd.concat(tidied_sheets, sort = True).fillna('')
 # %%
 #Post Processing
-df = trace.combine_and_trace(datasetTitle, "combined_dataframe")
+
 df.rename(columns={'OBS' : 'Value', 'DATAMARKER' : 'Marker'}, inplace=True)
 df['Year'] = df['Year'].astype(str).replace('\.0', '', regex=True)
 df['Year'] = df['Year'].str.strip()
@@ -155,6 +158,7 @@ df = df.replace({"BOP Services": {
 }})
 
 # %%
+# TODO: i think i remember Andrew saying not to drop duplicates as if there are any something is wrong. To the PR reviewer - should i remove it?
 df = df[[ 'Period', 'CDID', 'Seasonal Adjustment', 'BOP Services', 'Flow Directions', 'Measure Type', 'Marker' ,'Value',]].drop_duplicates()
 df
 
