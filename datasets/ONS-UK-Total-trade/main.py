6ea52d7c95c21ee434fa7342cc9e3e17a900f8c7
bailed22 <david.bailey@gsscogs.uk>
2022-06-16 10:09:24 +0000

    minor change to description

--- datasets/ONS-UK-Total-trade/main.py
+++ datasets/ONS-UK-Total-trade/main.py
@@ -24,7 +24,6 @@ metadata.dataset.family = 'trade'
 
 distribution = metadata.distribution(latest=True)
 
-
 tabs = {tab.name: tab for tab in metadata.distribution(latest=True).as_databaker()}
 
 
@@ -72,14 +71,27 @@ df = pd.concat(tidied_sheets, sort = True).fillna('')
 
 #Post Processing 
 df.rename(columns={'OBS' : 'Value','DATAMARKER' : 'Marker'}, inplace=True)
-df['Trade Type'] = df['Trade Type'].apply(lambda x: 'all' if 'Total Trade' in x else 
-                                      ('goods' if 'Trade in Goods' in x else 
-                                       ('services' if 'Trade in Services' in x else x)))
+df['Trade Type'] = df['Trade Type'].apply(lambda x: 'total' if 'Total Trade' in x else 
+                                    ('goods' if 'Trade in Goods' in x else 
+                                    ('services' if 'Trade in Services' in x else x)))
 df['Period'] = df['Period'].astype(str).replace('\.0', '', regex=True)
 df['Period'] =  df["Period"].apply(date_time)
 df = df[['Period', 'Country', 'Flow', 'Trade Type', 'Marker', 'Value']]
 
 df['Flow'] = df['Flow'].map(lambda x: 'export'  if 'Exports' in x else 'import')
+
+add_to_des = """
+These tables have been produced to provide an aggregated quarterly goods and services estimate and combines the most recent estimates for goods and services split by country.
+Data for goods and services is consistent for annual whole world totals and quarters (from Q1 2016) with the trade data published in the Quarterly National Accounts, Quarterly Sector Accounts and Quarterly Balance of Payments on 31st March 2022.
+These data are our best estimate of these bilateral UK trade flows. Users should note that alternative estimates are available, in some cases, via the statistical agencies for bilateral countries or through central databases such as:
+UN Comtrade.
+Some data for countries have been marked with N/A. This is because Trade in Goods do not collate data from these countries, therefore only Trade in Services is reflected within total trade for these countries or vice versa.
+The data within these tables are also consistent with the below releases:
+For Trade in Goods the data is consistent with UK Trade: February 2022 publication on 11th April 2022
+For Trade in Services the data is consistent with UK Trade in services by partner country: October to December 2021 publication on 28th April 2022
+Data is in current price (CP), £m and is non-seasonal adjusted
+"""
+metadata.dataset.description = metadata.dataset.description + add_to_des
 
 df.to_csv('observations.csv', index=False)
 catalog_metadata = metadata.as_csvqb_catalog_metadata()
