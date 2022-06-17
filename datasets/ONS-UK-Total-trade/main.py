14db99b55c2d35a698045ab38bd83c969496d0e3
bailed22 <95625808+bailed22@users.noreply.github.com>
2022-06-17 10:30:20 +0100

    ONS-UK-Total-trade (#390)
    * change to new legacy pipeline
    
    * change to info.jsonv1.1.0
    
    * minor change to description
    
    * minor change to trade type codelist
    
    * minor change to main.py
    
    * minor change to country dimension

--- datasets/ONS-UK-Total-trade/main.py
+++ datasets/ONS-UK-Total-trade/main.py
@@ -6,28 +6,25 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.3.3
+#       jupytext_version: 1.13.8
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3.9.12 64-bit
 #     language: python
 #     name: python3
 # ---
 
 # UK trade in services: all countries, non-seasonally adjusted
 
-# +
+import pandas as pd
 from gssutils import *
-import json
-import numpy as np
 
-trace = TransformTrace()
-df = pd.DataFrame()
-cubes = Cubes("info.json")
-scraper = Scraper(json.load(open('info.json'))['landingPage'])
-scraper
-# -
+metadata = Scraper(seed='info.json')
 
-tabs = { tab.name: tab for tab in scraper.distribution(latest=True).as_databaker() }
+metadata.dataset.family = 'trade'
+
+distribution = metadata.distribution(latest=True)
+
+tabs = {tab.name: tab for tab in metadata.distribution(latest=True).as_databaker()}
 
 
 # +
@@ -44,69 +41,59 @@ def date_time (date):
     else:
         return date
 
+
+# -
+
+tidied_sheets = []
 for name, tab in tabs.items():
     datasetTitle = 'uk-total-trade-all-countries-non-seasonally-adjusted'
-    columns=['Period','Country','Flow','Trade Type', 'Marker']
-    trace.start(datasetTitle, tab, columns, scraper.distributions[0].downloadURL)
     
-    if 'Index' in name or '7 Contact Sheet' in name:
+    if 'Index' in name or '7 Contact Sheet' in name or 'Notes' in name:
         continue
-    observations = tab.excel_ref('C7').expand(DOWN).expand(RIGHT).is_not_blank().is_not_whitespace()
-    period = tab.excel_ref('C4').expand(RIGHT).is_not_blank().is_not_whitespace()
-    trace.Period("Taken from row 4")
-    flow = tab.fill(DOWN).one_of(['Exports','Imports'])
-    trace.Flow("Either one of Exports or Imports: rows 7 to 251 are Exports and rows 255 to 499 are Imports")
-    country = tab.excel_ref('A7').expand(DOWN).is_not_blank().is_not_whitespace()
-    trace.Country("2 letter code taken from column A")
-    trade_type = tab.excel_ref('B1')
-    trace.Trade_Type("Taken from sheet title ")
+    period = tab.excel_ref("B3").expand(DOWN).filter(contains_string("Country")).fill(RIGHT).is_not_blank().is_not_whitespace()
+    flow = tab.excel_ref("A3").expand(DOWN).filter(contains_string("by Country")).is_not_blank().is_not_whitespace()
+    country = tab.excel_ref('B4').fill(DOWN).is_not_blank().is_not_whitespace()- tab.excel_ref('B4').fill(DOWN).filter(contains_string("Country"))
+    trade_type = tab.excel_ref('A1')
+    observations = period.fill(DOWN).is_not_blank().is_not_whitespace()-period
+    
     dimensions = [
         HDim(period,'Period',DIRECTLY,ABOVE),
         HDim(country,'Country',DIRECTLY,LEFT),
         HDim(flow, 'Flow',CLOSEST,ABOVE),
-        HDim(trade_type, 'Trade Type',CLOSEST,LEFT),
+        HDim(trade_type, 'Trade Type',CLOSEST,LEFT)
     ]
-    tidy_sheet = ConversionSegment(tab, dimensions, observations)   
-    #savepreviewhtml(tidy_sheet, fname=tab.name + "Preview.html")
-    trace.with_preview(tidy_sheet)
-    trace.store("combined_dataframe", tidy_sheet.topandas())
-# -
+    tidy_sheet = ConversionSegment(tab, dimensions, observations) 
+    tidy_sheet = tidy_sheet.topandas()
+    tidied_sheets.append(tidy_sheet)
+    # savepreviewhtml(tidy_sheet,fname=tab.name + "Preview.html")
+
+df = pd.concat(tidied_sheets, sort = True).fillna('')
 
 #Post Processing 
-df = trace.combine_and_trace(datasetTitle, "combined_dataframe")
 df.rename(columns={'OBS' : 'Value','DATAMARKER' : 'Marker'}, inplace=True)
-#df['Value'] = df['Value'].astype(float)
-df['Value'] = pd.to_numeric(df.Value, errors='coerce')
-df['Marker'].replace('N/A', 'not-collated', inplace=True)
-trace.Marker("replacing N/A with not-collated")
-df['Flow'] = df['Flow'].map(lambda s: s.lower().strip())
-df["Country"] = df["Country"].map(lambda x: pathify(x).upper())
 df['Trade Type'] = df['Trade Type'].apply(lambda x: 'total' if 'Total Trade' in x else 
-                                      ('goods' if 'Trade in Goods' in x else 
-                                       ('services' if 'Trade in Services' in x else x)))
-trace.Trade_Type("Replacing Trade Type output to one of; total, goods, services if the title contains the one of those words.")
+                                    ('goods' if 'Trade in Goods' in x else 
+                                    ('services' if 'Trade in Services' in x else x)))
+df['Marker'].replace('N/A', 'not-collated', inplace=True)
 df['Period'] = df['Period'].astype(str).replace('\.0', '', regex=True)
-trace.Period('Formatingyear values into year/{yr} (year/2020) and quarter values into quarter/{qtr} (quarter/Q1)')
 df['Period'] =  df["Period"].apply(date_time)
-df = df[['Period', 'Country', 'Flow', 'Trade Type', 'Value', 'Marker']]
+df = df[['Period', 'Country', 'Flow', 'Trade Type', 'Marker', 'Value']]
 
-#additional scraper info needed
-scraper.dataset.family = 'trade'
+df['Flow'] = df['Flow'].map(lambda x: 'export'  if 'Exports' in x else 'import')
+
 add_to_des = """
 These tables have been produced to provide an aggregated quarterly goods and services estimate and combines the most recent estimates for goods and services split by country.
-Data for goods and services is consistent for annual whole world totals and quarters (from Q1 2016) with the trade data published in the Quarterly National Accounts, Quarterly Sector Accounts and Quarterly Balance of Payments on 30th September 2020.
+Data for goods and services is consistent for annual whole world totals and quarters (from Q1 2016) with the trade data published in the Quarterly National Accounts, Quarterly Sector Accounts and Quarterly Balance of Payments on 31st March 2022.
 These data are our best estimate of these bilateral UK trade flows. Users should note that alternative estimates are available, in some cases, via the statistical agencies for bilateral countries or through central databases such as:
 UN Comtrade.
-Some data for countries have been marked with N/A. This is because Trade in Goods do not collate data from these countries, therefore only Trade in Services is reflected within total trade for these countries
+Some data for countries have been marked with N/A. This is because Trade in Goods do not collate data from these countries, therefore only Trade in Services is reflected within total trade for these countries or vice versa.
 The data within these tables are also consistent with the below releases:
-For Trade in Goods the data is consistent with UK Trade: August 2020 publication on 9th October 2020
-For Trade in Services the data is consistent with UK Trade in services by partner country: April to June 2020 publication on 4th November 2020
+For Trade in Goods the data is consistent with UK Trade: February 2022 publication on 11th April 2022
+For Trade in Services the data is consistent with UK Trade in services by partner country: October to December 2021 publication on 28th April 2022
+Data is in current price (CP), £m and is non-seasonal adjusted
 """
-scraper.dataset.description = scraper.dataset.description + add_to_des
+metadata.dataset.description = metadata.dataset.description + add_to_des
 
-cubes.add_cube(scraper, df.drop_duplicates(), "ons-uk-total-trade")
-cubes.output_all()
-trace.render("spec_v1.html")
-
-
-df.dtypes
+df.to_csv('observations.csv', index=False)
+catalog_metadata = metadata.as_csvqb_catalog_metadata()
+catalog_metadata.to_json_file('catalog-metadata.json')
