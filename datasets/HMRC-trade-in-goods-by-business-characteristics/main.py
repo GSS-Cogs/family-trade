824c512d6f1a73c1907beae38795e6484ff10495
Santhosh kumar <34086991+santhosh-thangavel@users.noreply.github.com>
2022-06-21 13:08:44 +0100

    HMRC-trade-in-goods-by-business-characteristics (#391)
    * removed cubes class and unwanted code lines
    
    * CSV Cubed uplift
    
    * changes to mapping
    
    * changes to mapping
    
    * changes to year mapping
    
    * changes to attribute mapping and industry group mapping
    
    * deleted pathify code lines and modified flow dimension
    
    * code changes
    
    Co-authored-by: santhosh thangavel <santhoshthangavel@Santhoshs-MacBook-Pro.local>

--- datasets/HMRC-trade-in-goods-by-business-characteristics/main.py
+++ datasets/HMRC-trade-in-goods-by-business-characteristics/main.py
@@ -6,9 +6,9 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.13.0
+#       jupytext_version: 1.13.8
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: Python 3.9.12 64-bit
 #     language: python
 #     name: python3
 # ---
@@ -20,24 +20,22 @@ import requests
 import pandas as pd
 import numpy as np
 
-scraper = Scraper(seed = 'info.json')
-scraper
+metadata = Scraper(seed = 'info.json')
+metadata
 # -
 
 info = json.load(open('info.json'))
 
-cubes = Cubes('info.json')
+metadata.select_dataset(title = lambda x: x.endswith('data tables'), latest = True)
+metadata.dataset.family = info["families"]
 
-scraper.select_dataset(title = lambda x: x.endswith('data tables'), latest = True)
-scraper.dataset.family = info["families"]
-
-datasetTitle = scraper.title
+datasetTitle = metadata.title
 datasetTitle = "UK trade in goods by business characteristics - data tables"
 
-distribution = scraper.distribution(title = lambda t : 'data tables' in t, latest = True)
+distribution = metadata.distribution(title = lambda t : 'data tables' in t, latest = True)
 distribution
 
-tabs = {tab.name: tab for tab in scraper.distribution(title = lambda t : 'data tables' in t).as_databaker()}
+tabs = {tab.name: tab for tab in metadata.distribution(title = lambda t : 'data tables' in t).as_databaker()}
 list(tabs)
 
 tidied_sheets = []
@@ -84,24 +82,6 @@ df = pd.concat(tidied_sheets, sort = True)
 
 df.rename(columns= {'OBS':'Value', 'Period':'Year', 'Flow':'Flow Directions', 'DATAMARKER':'Marker'}, inplace = True)
 
-df['Flow Directions'] = df['Flow Directions'].apply(pathify)
-
-
-# +
-df['Country'] = df['Country'].apply(pathify)
-
-#df['Country'].unique()
-# -
-
-
-df['Zone'] = df['Zone'].apply(pathify)
-
-df['Business Size'] = df['Business Size'].apply(pathify)
-
-df['Age'] = df['Age'].apply(pathify)
-
-df['Industry Group'] = df['Industry Group'].apply(pathify)
-
 df["Employee Count"] = df["Employee Count"].apply(lambda x: str(x).split(".")[0])
 df["Business Count"] = df["Business Count"].apply(lambda x:str(x).split(".")[0])
 
@@ -133,20 +113,29 @@ df['Country'] = df['Country'].map({
 })
 
 df['Zone'] = df['Zone'].map({ 
-    'eu': 'B5', 'non-eu': 'D5', 'world': 'W1'
+    'EU': 'B5', 'Non-EU': 'D5', 'World': 'W1'
 })
 
 df = df.rename(columns={'Flow Directions': "Flow", "Business Size": "Number of Employees", "Age": "Age of Business"})
 
-df['Flow'].loc[(df['Flow'] == 'import')] = 'imports'
-df['Flow'].loc[(df['Flow'] == 'export')] = 'exports'
+df["Flow"].replace({"Export":"Exports", "Import":"Imports"}, inplace = True)
+
+df["Flow"].replace({"Export":"Exports", "Import":"Imports"}, inplace = True)
+
 df['Value'].loc[(df['Value'] == '')] = 0
 df['Value'] = df['Value'].astype(int)
 
-scraper.dataset.comment = 'UK trade in goods by business characteristics data details international trade in goods by industry, age and size of business'
+# metadata.dataset.comment = 'UK trade in goods by business characteristics data details international trade in goods by industry, age and size of business'
+metadata.dataset.comment = metadata.catalog.description
+metadata.dataset.comment
 
-scraper.dataset.description = """HM Revenue and Customs has linked the overseas trade statistics (OTS) trade in goods data with the Office for National Statistics (ONS) business statistics data, sourced from the Inter-Departmental Business Register (IDBR). These experimental statistics releases gives some expanded analyses showing overseas trade by business characteristics, which provides information about the businesses that are trading those goods. This release focuses on trade by industry group, age of business and size of business (number of employees) This is a collection of all experimental UK trade in goods statistics by business characteristics available on GOV.UK.
+metadata.dataset.description = f"""
+HM Revenue and Customs has linked the overseas trade statistics (OTS) trade in goods data with the Office for National Statistics (ONS) business statistics data, sourced from the Inter-Departmental Business Register (IDBR). 
+These experimental statistics releases gives some expanded analyses showing overseas trade by business characteristics, which provides information about the businesses that are trading those goods. 
+This release focuses on trade by industry group, age of business and size of business (number of employees) 
+This is a collection of all experimental UK trade in goods statistics by business characteristics available on GOV.UK.
 """
 
-cubes.add_cube(scraper, df.drop_duplicates(), datasetTitle)
-cubes.output_all()
+df.to_csv("observations.csv", index = False)
+catalog_metadata = metadata.as_csvqb_catalog_metadata()
+catalog_metadata.to_json_file('catalog-metadata.json')
