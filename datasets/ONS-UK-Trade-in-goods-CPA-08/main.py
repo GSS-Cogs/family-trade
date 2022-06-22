e9d7bc0800e5ed4d08657298d6c04b8b135c528a
bailed22 <95625808+bailed22@users.noreply.github.com>
2022-06-22 13:05:45 +0100

    ONS-UK-Trade-in-goods-CPA-08 (#392)
    * change legacy pipeline to csvcubed pipeline
    
    * change to new info.json v1.1.0
    
    * minor change to main.py
    
    * minor change to info.json

--- datasets/ONS-UK-Trade-in-goods-CPA-08/main.py
+++ datasets/ONS-UK-Trade-in-goods-CPA-08/main.py
@@ -5,43 +5,36 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.13.0
+#       jupytext_version: 1.13.8
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: Python 3.9.12 64-bit
 #     language: python
 #     name: python3
 # ---
 
 # UK trade in goods, CPA (08)
 
-# +
-import json
 import pandas as pandas
 from gssutils import *
 
-pd.options.mode.chained_assignment = None 
-pd.set_option('display.float_format', lambda x: '%.0f' % x) 
-
-cubes = Cubes('info.json')
-info = json.load(open('info.json'))
-landingPage = info['landingPage']
 metadata = Scraper(seed="info.json")
+
 distribution = metadata.distribution(latest = True)
+
 title = distribution.title
-distribution
-# -
 
 tabs = distribution.as_databaker()
-tidied_sheets = []
 
+tidied_sheets = []
 for tab in tabs:
-    if tab.name not in ['Index', 'Contact']:
+    if tab.name not in ['Table of contents', 'Cover sheet', 'Notes']:
 
         flow_direction = tab.name.split(' ', 1)[1]
-        period = tab.excel_ref("C4").expand(RIGHT).is_not_blank()
-        product = tab.excel_ref("A5").expand(DOWN).is_not_blank() 
+        period = tab.excel_ref("C4").expand(RIGHT).is_not_blank().is_not_whitespace()
+        # savepreviewhtml(period,fname=tab.name + "Preview.html")
+        product = tab.excel_ref("A5").expand(DOWN).is_not_blank().is_not_whitespace()
         cdid = tab.excel_ref('B5').expand(DOWN)
-        observations = tab.excel_ref('C5').expand(DOWN).expand(RIGHT).is_not_blank() 
+        observations = tab.excel_ref('C5').expand(DOWN).expand(RIGHT).is_not_blank().is_not_whitespace() 
 
         dimensions = [
             HDim(period, 'Period', DIRECTLY, ABOVE),
@@ -65,30 +58,33 @@ def right(s, amount):
 # -
 
 df = pd.concat(tidied_sheets, sort=True)
+
+#Post Processing 
 df = df[df['OBS'] != 0]
 df.rename(columns={'OBS' : 'Value'}, inplace=True)
 df['Value'] = df['Value'].astype(int)
 df['Period'] = df['Period'].map(lambda x: 'year/' + left(x,4) if 'Q' not in x else 'quarter/' + left(x,4) + '-' + right(x,2))
 df['Product'] = df['Product'].map(lambda x: x.split(' ', 1)[0])
-
+df['Flow Direction'] = df['Flow Direction'].apply(pathify)
 df['CDID'] = df['CDID'].map(lambda x: x if len(x.strip()) > 0 else 'not-applicable')
 
 df = df[['Period', 'Flow Direction', 'Product', 'CDID', 'Value']]
-df['Flow Direction'] = df['Flow Direction'].apply(pathify)
-df
 
 # +
 metadata.dataset.title = "UK Trade in goods by Classification of Product by Activity, time series dataset, Quarterly and Annual"
 
 metadata.dataset.comment = "Publication tables, UK trade in goods, CPA (08). Additional information for UK trade in goods by classification of product by activity. Current price, seasonally adjusted."
-
-metadata.dataset.description = """
-Publication tables, UK trade in goods, CPA (08). 
-Additional information for UK trade in goods by classification of product by activity. 
-Current price, seasonally adjusted.
-This publication includes a residual line which is used to account for any residual between the seasonally adjusted aggregate of 'Total Classification of Product by Activity (CPA)' and the seasonally adjusted aggregate of 'Total Standard International Trade Classification (SITC)' that is published in the UK Trade Statistical Bulletin, Gross Domestic Product (GDP), Balance of Payments (BoP) and Pink Book releases. The seasonal adjustment residual will account for differences at the aggregate level that are introduced because of differences in CPA and SITC classifications at the level at which seasonal adjustment is applied.
-"""
 # -
 
-cubes.add_cube(metadata, df, metadata.dataset.title)
-cubes.output_all()
+add_to_des = """
+Publication tables, UK trade in goods, CPA (08). 
+UK trade in goods by classification of product by activity, time series dataset, quarterly and annual up to and including 2022 Q1
+Additional informtion for the value of UK imports and exports of goods grouped by product. Goods are attributed to the activity of which they are the principal products
+Unit pounds million, current price, seasonally adjusted.
+This publication includes a residual line which is used to account for any residual between the seasonally adjusted aggregate of 'Total Classification of Product by Activity (CPA)' and the seasonally adjusted aggregate of 'Total Standard International Trade Classification (SITC)' that is published in the UK Trade Statistical Bulletin, Gross Domestic Product (GDP), Balance of Payments (BoP) and Pink Book releases. The seasonal adjustment residual will account for differences at the aggregate level that are introduced because of differences in CPA and SITC classifications at the level at which seasonal adjustment is applied.
+"""
+metadata.dataset.description = metadata.dataset.description + add_to_des
+
+df.to_csv('observations.csv', index=False)
+catalog_metadata = metadata.as_csvqb_catalog_metadata()
+catalog_metadata.to_json_file('catalog-metadata.json')
