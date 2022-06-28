1b688c7da1240d49c71cf6f5b2c9cef77be437b2
bailed22 <david.bailey@gsscogs.uk>
2022-06-28 10:02:43 +0000

    change main.py to new legacy pipeline

--- datasets/ONS-International-exports-of-services-from-subnational-areas-of-the-UK/main.py
+++ datasets/ONS-International-exports-of-services-from-subnational-areas-of-the-UK/main.py
@@ -6,52 +6,40 @@
 #     text_representation:
 #       extension: .py
 #       format_name: light
-#       format_version: '1.4'
-#       jupytext_version: 1.1.1
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3.9.12 64-bit
 #     language: python
 #     name: python3
 # ---
 
 # ONS-International-exports-of-services-from-subnational-areas-of-the-UK
 
-# +
+import pandas as pandas
 from gssutils import *
-import json
-import numpy as np 
 
-cubes = Cubes("info.json")
-trace = TransformTrace()
-info = json.load(open('info.json'))
-scraper = Scraper(seed = 'info.json')
-scraper
-# -
+metadata = Scraper(seed="info.json")
 
-distribution = scraper.distribution(latest = True)
+distribution = metadata.distribution(latest = True)
+
+title = distribution.title
+
 tabs = {tab.name: tab for tab in distribution.as_databaker()}
 
-data_download = distribution.downloadURL
-datasetTitle = distribution.title
 columns = ['Period', 'Export Services', 'Service Origin Geography', 'Flow Directions', 'Service Destination', 'Marker', 'Sheet Name']
 
 # +
-# tab 1a
-tab = tabs['1a'] #set tab as tab 1a
-trace.start(datasetTitle, tab, columns, data_download) #start tracer for tab 1a   
+tidied_sheets = []
+
+# from tab 1a
+tab = tabs['1a'] 
 cell = tab.excel_ref('A4')
-
 industry = cell.fill(DOWN).is_not_blank().is_not_whitespace()
-trace.Export_Services("Defined from cell ref A4 down")
-
 geography = cell.fill(RIGHT).is_not_blank().is_not_whitespace()
-trace.Service_Origin_Geography("Defined from cell ref A4 and right")
-
-trace.Service_Destination("Hardcoded as all")
-
 sheet = tab.name
-
 observations = geography.fill(DOWN).is_not_blank().is_not_whitespace()
+
 dimensions = [
     HDim(industry, 'Export Services', DIRECTLY, LEFT),
     HDim(geography, 'Service Origin Geography', DIRECTLY, ABOVE),
@@ -60,30 +48,18 @@ dimensions = [
     HDimConst("Sheet", tab.name)
 ]
 tidy_sheet = ConversionSegment(tab, dimensions, observations)
-savepreviewhtml(tidy_sheet,fname = tab.name+ "Preview.html")
-trace.with_preview(tidy_sheet)
-#store 1a and appending to combined_dataframe
-trace.store("combined_dataframe", tidy_sheet.topandas())
+df = tidy_sheet.topandas()
+tidied_sheets.append(df)
 
-
-# +
-# tab 1b
-tab = tabs['1b'] #set tab as tab 1b
-trace.start(datasetTitle, tab, columns, data_download) #start tracer for tab 1b
+#from tab 1b
+tab = tabs['1b'] 
 cell = tab.excel_ref('A5')
-
 industry = cell.fill(DOWN).is_not_blank().is_not_whitespace()
-trace.Export_Services("Defined from cell ref A5 Down ")
-
 origin = cell.shift(0,-1).fill(RIGHT).is_not_blank().is_not_whitespace() 
-trace.Service_Origin_Geography("Defined from cell ref A4 across")
-
 destination = cell.fill(RIGHT).is_not_blank().is_not_whitespace() 
-trace.Service_Destination("Defined from cell A5 across")
-
 sheet = tab.name
-
 observations = destination.fill(DOWN).is_not_blank().is_not_whitespace() 
+
 dimensions = [
             HDim(industry,'Export Services',DIRECTLY,LEFT),
             HDim(destination, 'Service Destination',DIRECTLY,ABOVE),
@@ -92,28 +68,17 @@ dimensions = [
             HDimConst("Sheet", tab.name)
 ]  
 tidy_sheet = ConversionSegment(tab, dimensions, observations)   
-savepreviewhtml(tidy_sheet, fname=tab.name + "Preview.html")
-trace.with_preview(tidy_sheet)
-#store 1b and appending to combined_dataframe
-trace.store("combined_dataframe", tidy_sheet.topandas())
+df = tidy_sheet.topandas()
+tidied_sheets.append(df)
 
-# +
-# tab 2a
-tab = tabs['2a'] #set tab as tab 2a
-trace.start(datasetTitle, tab, columns, data_download) #start tracer for tab 2a
+#from tab 2a
+tab = tabs['2a'] 
 cell = tab.excel_ref('A5')
-
 industry = cell.shift(1,0).fill(RIGHT).is_not_blank().is_not_whitespace()
-trace.Export_Services("Defined from cell ref B5 Down ")
-
 geography = cell.fill(DOWN).is_not_blank().is_not_whitespace()  
-trace.Service_Origin_Geography("Defined from cell ref A5 down")
-
-trace.Service_Destination("Hardcoded as all")
-
 sheet = tab.name
-
 observations = industry.fill(DOWN).is_not_blank().is_not_whitespace() 
+
 dimensions = [
             HDim(industry,'Export Services',DIRECTLY,ABOVE),
             HDim(geography, 'Service Origin Geography',DIRECTLY,LEFT),
@@ -122,28 +87,18 @@ dimensions = [
             HDimConst("Sheet", tab.name)
 ]  
 tidy_sheet = ConversionSegment(tab, dimensions, observations)   
-savepreviewhtml(tidy_sheet, fname=tab.name + "Preview.html")
-trace.with_preview(tidy_sheet)
-#store 2a and appending to combined_dataframe
-trace.store("combined_dataframe", tidy_sheet.topandas())
+df = tidy_sheet.topandas()
+tidied_sheets.append(df)
 
-# +
-# tab 2b
-tab = tabs['2b'] #set tab as 2b
-trace.start(datasetTitle, tab, columns, data_download) #start tracer for tab 2b
+#from tab 2b
+tab = tabs['2b'] 
 cell = tab.excel_ref("A5")
-
 industry = cell.shift(0,-1).fill(RIGHT).is_not_blank().is_not_whitespace()
-trace.Export_Services("Defined from cell ref C4 across")
-
 destination = cell.shift(1,0).fill(RIGHT).is_not_blank().is_not_whitespace()
-trace.Service_Destination("Defined from cell ref B5 across")
 origin = cell.fill(DOWN).is_not_blank().is_not_whitespace()
-trace.Service_Origin_Geography("Defined from cell ref B5 down")
-
 sheet = tab.name
-
 observations = destination.fill(DOWN).is_not_blank().is_not_whitespace()
+
 dimensions = [
     HDim(industry, "Export Services", CLOSEST, LEFT),
     HDim(destination, "Service Destination", DIRECTLY, ABOVE),
@@ -152,28 +107,18 @@ dimensions = [
     HDimConst("Sheet", tab.name)
 ]
 tidy_sheet = ConversionSegment(tab, dimensions, observations)
-savepreviewhtml(tidy_sheet, fname = tab.name + "Preview.html")
-trace.with_preview(tidy_sheet)
-#store 2b and appending to combined_dataframe
-trace.store("combined_dataframe", tidy_sheet.topandas())
-# # +
-#tab 3
-tab = tabs['3'] #start tracer for tab 3
-trace.start(datasetTitle, tab, columns, data_download)
+df = tidy_sheet.topandas()
+tidied_sheets.append(df)
+
+#from tab 3
+tab = tabs['3'] 
 cell = tab.excel_ref("A4")
-
-trace.Export_Services("Hard coded as all services")
-
 origin = cell.fill(DOWN).is_not_blank().is_not_whitespace()
-trace.Service_Origin_Geography("Defined from cell ref A4 down")
-
 destination = cell.shift(1,0).fill(RIGHT).is_not_blank().is_not_whitespace() \
             .filter(lambda x: type(x.value) != 'Percentage' not in x.value)
-trace.Service_Destination("Defined from cell ref B4 across excluding percentage values")
-
 sheet = tab.name
-
 observations = destination.fill(DOWN).is_not_blank().is_not_whitespace()
+
 dimensions = [
     HDimConst('Export Services', 'all services'),
     HDim(origin, 'Service Origin Geography', DIRECTLY, LEFT),
@@ -182,26 +127,17 @@ dimensions = [
     HDimConst("Sheet", tab.name)
 ]
 tidy_sheet = ConversionSegment(tab, dimensions, observations)
-savepreviewhtml(tidy_sheet, fname = tab.name + "Preview.html")
-trace.with_preview(tidy_sheet)
-#stor 3 and appending to combined_dataframe
-trace.store("combined_dataframe", tidy_sheet.topandas())
-# # +
-# tab 4a
-tab = tabs['4a'] #set tab as tab 4a
-trace.start(datasetTitle, tab, columns, data_download) #start traer for tab 4a
+df = tidy_sheet.topandas()
+tidied_sheets.append(df)
+
+#from tab 4a
+tab = tabs['4a'] 
 cell = tab.excel_ref("A4")
-
 industry = cell.fill(DOWN).is_not_blank().is_not_whitespace()
-trace.Export_Services("Defined from cell ref A4 down")
-
 geography = cell.fill(RIGHT).is_not_blank().is_not_whitespace()
-trace.Service_Origin_Geography("Defined from cell ref across")
-trace.Service_Destination("Hardcoded as all")
-
 sheet = tab.name
-
 observations = geography.fill(DOWN).is_not_blank().is_not_whitespace() 
+
 dimensions = [
     HDim(industry, 'Export Services', DIRECTLY, LEFT ),
     HDim(geography, 'Service Origin Geography', DIRECTLY, ABOVE),
@@ -209,28 +145,18 @@ dimensions = [
     HDimConst("Sheet", tab.name)
 ]
 tidy_sheet = ConversionSegment(tab, dimensions,observations)
-savepreviewhtml(tidy_sheet, fname = tab.name + 'Preview.html')
-trace.with_preview(tidy_sheet)
-# store 4a and appending to combined_dataframe
-trace.store("combined_dataframe", tidy_sheet.topandas())
-# # +
-# tab 4b
-tab = tabs['4b'] #set tab as tab 4b
-trace.start(datasetTitle, tab, columns, data_download) #start tracer for tab 4b
+df = tidy_sheet.topandas()
+tidied_sheets.append(df)
+
+#from tab 4b
+tab = tabs['4b'] 
 cell = tab.excel_ref('A5')
-
 industry = cell.fill(DOWN).is_not_blank().is_not_whitespace()
-trace.Export_Services("Defined from cell ref A5 down")
-
 origin = cell.shift(0,-1).fill(RIGHT).is_not_blank().is_not_whitespace()  
-trace.Service_Origin_Geography("Defined from cell ref A6 across")
-
 destination = cell.fill(RIGHT).is_not_blank().is_not_whitespace() 
-trace.Service_Destination("Defined from cell ref A5 across")
-
 sheet = tab.name
-
 observations = destination.fill(DOWN).is_not_blank().is_not_whitespace() 
+
 dimensions = [
             HDim(industry,'Export Services',DIRECTLY,LEFT),
             HDim(destination, 'Service Destination',DIRECTLY,ABOVE),
@@ -238,33 +164,21 @@ dimensions = [
             HDimConst("Sheet", tab.name)
 ]  
 tidy_sheet = ConversionSegment(tab, dimensions, observations)   
-savepreviewhtml(tidy_sheet, fname=tab.name + "Preview.html")
-trace.with_preview(tidy_sheet)
-#store 4b and appending to combined_dataframe
-trace.store("combined_dataframe", tidy_sheet.topandas())
+df = tidy_sheet.topandas()
+tidied_sheets.append(df)
 # -
 
+
+df = pd.concat(tidied_sheets, sort=True)
+
 #post processing
-df = trace.combine_and_trace(datasetTitle, "combined_dataframe")
-
 df.rename(columns={'OBS' : 'Value','DATAMARKER' : 'Marker'}, inplace=True)
-
-df.rename(columns={'OBS' : 'Value','DATAMARKER' : 'Marker'}, inplace=True)
-
 df['Period'] = "year/2017"
-trace.Period("Hard coded as year/2017")
-
 df['Export Services'] = df['Export Services'].apply(pathify)
-
 df["Service Destination"] = df["Service Destination"].apply(pathify)
-
-df['Service Destination'] = df['Service Destination'].map(
-    lambda x: { 'total' : 'all', 'row' :'rest-of-world'}.get(x, x))
-
-df['Marker'] = df['Marker'].map(lambda x: { '..' : 'suppressed' }.get(x, x))
-
+df['Service Destination'] = df['Service Destination'].map(lambda x: { 'total' : 'all', 'row' :'rest-of-world'}.get(x, x)) 
+df['Marker'] = df['Marker'].map(lambda x: { '..' : 'suppressed' }.get(x, x))  
 df['Flow Directions'] = 'exports'
-trace.Flow_Directions("Adding Flow directions column to dataframe with value exports")
 
 #Changing notation for nuts 1 values one. (Tabs 1a and 1b)
 f1=((df['NUTS'] =='nuts1/'))
@@ -283,22 +197,7 @@ df.loc[f1,'Service Origin Geography'] = df.loc[f1,'Service Origin Geography'].ma
         'Wales':'UKL',
         'Scotland':'UKM',
         'Northern Ireland':'UKN'      
-         }.get(x, x))
-
-# +
-#df['Service Origin Geography'].unique()
-# -
-
-#adding in the nuts?/ notation to each value in Service Origin Geography
-df['NUTS'].replace(np.nan, '', inplace=True)
-
-df['Service Origin Geography'] = df['NUTS'] + df['Service Origin Geography']
-
-# +
-#df['Service Origin Geography'] = df['Service Origin Geography'].apply(pathify)
-
-# +
-#df['Service Origin Geography'].unique()
+         }.get(x, x))        
 
 # +
 df = df.replace({'Service Origin Geography' : {
@@ -322,18 +221,13 @@ df = df.replace({'Service Origin Geography' : {
     'Swansea Bay' : 'http://statistics.data.gov.uk/id/statistical-geography/W42000004',
     'Tees Valley' : 'http://statistics.data.gov.uk/id/statistical-geography/E47000006',
     'West Midlands' : 'http://statistics.data.gov.uk/id/statistical-geography/E47000007',
-    'West of England' : 'http://statistics.data.gov.uk/id/statistical-geography/E47000009' }})
-
-#df['Service Origin Geography'].unique()
+    'West of England' : 'http://statistics.data.gov.uk/id/statistical-geography/E47000009'
+     }})   
 # -
 
-#define the column order output into dataframe called tidy
-tidy = df[['Period','Export Services','Service Origin Geography','Service Destination', 'Flow Directions', 'Value','Marker']]
-tidy.head(5)
+df = df[['Period','Export Services','Service Origin Geography', 'Service Destination', 'Flow Directions', 'Marker', 'Value']]
 
-#scraper.dataset.title
-#scraper.dataset.description
-scraper.dataset.description = scraper.dataset.description + """
+add_to_des = """
 Sources: UK Balance of Payments - The Pink Book; International Trade in Services; UK Trade in services by industry, country and service type: 2016 to 2017
 
 The estimates shown in this workbook are experimental. While we have made some important changes (please refer to the article),
@@ -347,17 +241,18 @@ Link to related publications:
 * www.ons.gov.uk/economy/nationalaccounts/balanceofpayments/articles/uktradeinservicesbyindustrycountryandservicetype/2016to2017
 
 Footnotes:
+* The symbol ".." denotes values that have been suppressed for reasons of confidentiality or reliability.
 * Smaller geographic breakdowns may not sum exactly to aggregated totals due to rounding.
 * The European Union consists of 28 member countries including the United Kingdom. For trade purposes, this includes all 27 countries other than the UK as well as the European Central Bank and European Institutions.
 * The industrial groups presented in this analysis are based on the UK Standard Industrial Classification 2007, although some changes have been made. Primary and utilities represents SIC07 section A, B, D and E; Non-manufacturing production services represents SIC07 section A, B, D, E and F; section G has been split into two parts (Wholesale and motor trades, and Retail); Other services comprises of O, P, Q, R, S and unknown/unallocated industries. 
 * The broader industry groups consist of production industries (SIC07 A-E), and two groups of services: business-based professional services (SIC07 L-N) and all other services (SIC07 G-K and O-S). 
 * For further information about the industrial classification, please see: https://www.ons.gov.uk/methodology/classificationsandstandards/ukstandardindustrialclassificationofeconomicactivities/uksic2007
 """
+metadata.dataset.description = metadata.dataset.description + add_to_des
 
-#output cube and spec
-cubes.add_cube(scraper, tidy.drop_duplicates(), distribution.title)
-cubes.output_all()
-trace.render("spec_v1.html")
+df.to_csv('observations.csv', index=False)
+catalog_metadata = metadata.as_csvqb_catalog_metadata()
+catalog_metadata.to_json_file('catalog-metadata.json')
 
 #  Keeping for reference
 #  Unit = gbp-million
