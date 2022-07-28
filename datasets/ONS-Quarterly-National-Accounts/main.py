682d52e644553dea0554d6c1f00fd77008971470
santhosh thangavel <santhoshthangavel@Santhoshs-MacBook-Pro.local>
2022-07-28 15:09:42 +0100

    adding 7th cube

--- datasets/ONS-Quarterly-National-Accounts/main.py
+++ datasets/ONS-Quarterly-National-Accounts/main.py
@@ -1,4 +1,4 @@
-# %%
+# +
 from gssutils import *
 import json
 import copy 
@@ -6,10 +6,9 @@ import numpy as np
 
 df = pd.DataFrame()
 metadata = Scraper(seed = 'national_account_aggregates-info.json')
-# %%
+# -
 distribution = metadata.distribution(latest = True)
 tabs = { tab.name: tab for tab in distribution.as_databaker() }
-# %%
 #grouping tab into topics to iterate through by their names
 national_account_aggregates = ['A1 AGGREGATES', 'A2 AGGREGATES']
 output_indicators = ['B1 CVM OUTPUT', 'B2 CVM OUTPUT']
@@ -21,7 +20,7 @@ inventories = ['G1 INVENTORIES', 'G2 INVENTORIES']
 trade = ['H1 TRADE', 'H2 TRADE']
 tidied_sheets = []
 
-# %%
+# +
 def with_indices_overrides(indices_dimension):
     """
     Adding a cellvalue overrides to each cell within the dimension AFTER
@@ -176,7 +175,7 @@ def with_flow_overrides(flow_dimension):
                 flow_dimension.AddCellValueOverride(cell, cell_checked[0].value)  
     return flow_dimension
 
-# %%
+# +
 for name, tab in tabs.items():
     #shared dimensions across all tabs
     seasonal_adjustment = tab.excel_ref('A5').expand(DOWN).filter(contains_string('Seasonally'))
@@ -391,68 +390,68 @@ for name, tab in tabs.items():
     else:
         continue
 
-# Tabs transformed and appended to tidied_sheets to make it easier to understand for a DM.. hopefully 
-# Things to note, I have done no post processing atm due to this being a little annoying and want clarity from a DM first. 
+# # Tabs transformed and appended to tidied_sheets to make it easier to understand for a DM.. hopefully 
+# # Things to note, I have done no post processing atm due to this being a little annoying and want clarity from a DM first. 
+# #
+# # ##### National Accounts aggregates 
+# #     tidied_sheets[0] (A1 AGGREGATES)
+# #     tidied_sheets[1] (A2 AGGREGATES)
+# #   
+# # ##### Output indicators
+# #     tidied_sheets[2] (B1 CVM OUTPUT)
+# #     tidied_sheets[3] (B2 CVM OUTPUT)
+# #     
+# # ##### Expenditure Indicators 
+# #     tidied_sheets[4] (C1 Expenditure)
+# #     tidied_sheets[5] (C2 Expenditure)
+# #    
+# # ##### Income indicators
+# #     tidied_sheets[6] (D Income)
+# #
+# # ##### Household Expenditure Indicators
+# #     tidied_sheets[7] (E1 Expenditure)
+# #     tidied_sheets[8] (E1 Expenditure)
+# #     tidied_sheets[9] (E1 Expenditure)
+# #     tidied_sheets[10](E1 Expenditure)
+# #
+# #
+# # ##### Gross Fixed Capitol 
+# #     tidied_sheets[11](F1 GFCF)
+# #     tidied_sheets[12](F1 GFCF)
+# #      
+# #      Note another dimension will need to be added during post processsing called something like 'Sector' which will either be: UK National or UK dommestic, depending on the value in Capital Formation dimension.
+# #
+# # ##### Inventories
+# #     tidied_sheets[13](G1 Inventories)
+# #     tidied_sheets[14](G1 Inventories)
+# #
+# #     
+# # ##### Trade 
+# #     tidied_sheets[15](H1 TRADE)
+# #     tidied_sheets[16](H2 TRADE)
+# #     
+# #     Note I will need to do a bit of wrangling to fix the flow dimension in post processing, this is due to some tables using a horrible centered headings for flow values. 
+# #     
+# # ##### Other 
+# #
+# #     The following tabs were not included when this was previously done, is this still the case ?
+# #     
+# #     
+# #      'L GVAbp',
+# #      'M Alignment adjustments',
+# #      'N Financial Year Variables',
+# #      'O Selected imp def',
+# #      'P GDP per head',
+# #      'R Quarterly Revisions',
+# #      'AA Annex A',
+# #      'AB Annex B',
+# #      'AC Annex C',
+# #      'AD Annex D'
+# #      'AE Annex E',
+# #      'AF Annex F',
+# #      'AG Annex G'
 
-# ##### National Accounts aggregates 
-#     tidied_sheets[0] (A1 AGGREGATES)
-#     tidied_sheets[1] (A2 AGGREGATES)
-#   
-# ##### Output indicators
-#     tidied_sheets[2] (B1 CVM OUTPUT)
-#     tidied_sheets[3] (B2 CVM OUTPUT)
-#     
-# ##### Expenditure Indicators 
-#     tidied_sheets[4] (C1 Expenditure)
-#     tidied_sheets[5] (C2 Expenditure)
-#    
-# ##### Income indicators
-#     tidied_sheets[6] (D Income)
-#
-# ##### Household Expenditure Indicators
-#     tidied_sheets[7] (E1 Expenditure)
-#     tidied_sheets[8] (E1 Expenditure)
-#     tidied_sheets[9] (E1 Expenditure)
-#     tidied_sheets[10](E1 Expenditure)
-#
-#
-# ##### Gross Fixed Capitol 
-#     tidied_sheets[11](F1 GFCF)
-#     tidied_sheets[12](F1 GFCF)
-#      
-#      Note another dimension will need to be added during post processsing called something like 'Sector' which will either be: UK National or UK dommestic, depending on the value in Capital Formation dimension.
-#
-# ##### Inventories
-#     tidied_sheets[13](G1 Inventories)
-#     tidied_sheets[14](G1 Inventories)
-#
-#     
-# ##### Trade 
-#     tidied_sheets[15](H1 TRADE)
-#     tidied_sheets[16](H2 TRADE)
-#     
-#     Note I will need to do a bit of wrangling to fix the flow dimension in post processing, this is due to some tables using a horrible centered headings for flow values. 
-#     
-# ##### Other 
-#
-#     The following tabs were not included when this was previously done, is this still the case ?
-#     
-#     
-#      'L GVAbp',
-#      'M Alignment adjustments',
-#      'N Financial Year Variables',
-#      'O Selected imp def',
-#      'P GDP per head',
-#      'R Quarterly Revisions',
-#      'AA Annex A',
-#      'AB Annex B',
-#      'AC Annex C',
-#      'AD Annex D'
-#      'AE Annex E',
-#      'AF Annex F',
-#      'AG Annex G'
-
-
+# + endofcell="--"
 import numpy as np
 
 def strip_superscripts(dataset, dimension):
@@ -469,7 +468,7 @@ def strip_superscripts(dataset, dimension):
         return dataset
 
 
-# +
+# # +
 
 def prefix_refperiod(dataset, dimension):
     try:
@@ -497,9 +496,10 @@ def convet_dimension_to_int(dataset, dimension):
     except Exception as e:
         print('convet_dimension_to_int: ' + str(e))
         return dataset
+# --
 
 
-# %%
+# +
 a2 = tidied_sheets[1]
 
 a2 = tidied_sheets[1]
@@ -539,7 +539,7 @@ a2 = a2.rename(columns={'Indices':'Estimate Type', 'Gross':'Aggregate'})
 # a2["Estimate Type"] = a2["Estimate Type"].replace({"Chained Volume Measure (Reference year 2019)" : "chained-volume-measure"})
 # a2["Estimate Type"].unique()
 
-# %%
+# + endofcell="--"
 mainTitle = metadata.dataset.title
 maincomme = metadata.dataset.comment
 maindescr = metadata.dataset.description
@@ -562,8 +562,9 @@ a2["Estimate Type"].unique()
 a2.to_csv("national_account_aggregates-observations.csv", index = False)
 catalog_metadata = metadata.as_csvqb_catalog_metadata()
 catalog_metadata.to_json_file('national_account_aggregates-catalog-metadata.json')
+# --
 
-# %%
+# +
 b1 = tidied_sheets[2]
  # Only use the main value data for now, CVMs
 try:
@@ -594,7 +595,7 @@ b1['Industry'] = b1['Industry'].apply(pathify)
 
 b1 = convet_dimension_to_int(b1, 'Value')
 
-# %%
+# +
 b2 = tidied_sheets[3]
 try:
     b2 = b2.loc[b2['Percentage Change'].isna()] 
@@ -641,7 +642,7 @@ b1b2.to_csv("output_indicators-observations.csv", index = False)
 catalog_metadata = metadata.as_csvqb_catalog_metadata()
 catalog_metadata.to_json_file('output_indicators-catalog-metadata.json')
 
-# %%
+# +
 
 c1 = tidied_sheets[4]
 
@@ -731,6 +732,8 @@ c1c2.to_csv("expenditure_indicators-observations.csv", index = False)
 catalog_metadata = metadata.as_csvqb_catalog_metadata()
 catalog_metadata.to_json_file('expenditure_indicators-catalog-metadata.json')
 
+# + endofcell="---"
+
 
 d1 = tidied_sheets[6]
 
@@ -779,9 +782,13 @@ Other income includes mixed income and the operating surplus of the non-corporat
 """
 # --
 
-# +
 
 d1.columns
+
+
+# ---
+
+d1["Category of Income"].unique()
 
 # +
 
@@ -789,7 +796,10 @@ d1.to_csv("income_indicators-observations.csv", index = False)
 catalog_metadata = metadata.as_csvqb_catalog_metadata()
 catalog_metadata.to_json_file('income_indicators-catalog-metadata.json')
 
+
 # +
+
+# # +
 
 e1 = tidied_sheets[7]
 
@@ -859,7 +869,7 @@ e2['Expenditure Category'] = 'uk-domestic'
 e2cdids = e2['CDID'].unique()
 e2.head(5)
 
-# +
+
 e3 = tidied_sheets[9]
 
 try:
@@ -890,7 +900,7 @@ e3 = convet_dimension_to_int(e3, 'Value')
 e3cdids = e3['CDID'].unique()
 e3.head(50)
 
-# +
+
 e4 = tidied_sheets[10]
 
 try:
@@ -922,12 +932,12 @@ e4['Expenditure Category'] = 'uk-domestic'
 e4cdids = e4['CDID'].unique()
 e4.head(5)
 
-# +
+
 
 e1e2e3e4 = pd.concat([e1, e2, e3, e4])
 e1e2e3e4.head(10)
 
-# +
+
 
 metadata.dataset.title = mainTitle + ' - Household final consumption by purpose and goods and services at Current Prices & Chained Volume Measures (E1, E2, E3, E4)'
 metadata.dataset.comment = maincomme + ' - Household final consumption by purpose and goods and services at Current Prices & Chained Volume Measures (E1, E2, E3, E4) - Seasonally Adjusted'
@@ -940,22 +950,23 @@ UK National: Final consumption by UK Households in the UK & abroad.
 UK National & Domestic: Final consumption expenditure in the UK by UK & foreign households and final consumption by UK Households in the UK & abroad.
 """
 
-# +
+
 
 e1e2e3e4.columns
 
-# +
+
 
 e1e2e3e4["Expenditure Category"].unique()
 
-# +
+
+
+# -
 
 e1e2e3e4.to_csv("household_expenditure_indicators-observations.csv", index = False)
 catalog_metadata = metadata.as_csvqb_catalog_metadata()
 catalog_metadata.to_json_file("household_expenditure_indicators-catalog-metadata.json")
 
-
-
+# +
 f1 = tidied_sheets[11]
 
 try:
@@ -991,7 +1002,7 @@ f1['Analysis'] = f1['Analysis'].apply(pathify)
 f1cdids = f1['CDID'].unique()
 f1.head(5)
 
-
+# + endofcell="--"
 f2 = tidied_sheets[12]
 
 try:
@@ -1048,21 +1059,22 @@ Dwellings: Includes new dwellings and improvements to dwellings.
 Other buildings and structures: Including costs associated with the transfer of ownership of buildings, dwellings and non-produced assets.
 """
 
+
+# --
+
 # +
 
 f1f2.columns
 
-# +
+
+# -
 
 f1f2["Analysis"].unique()
-
-# +
 
 # There are duplicates in this DataFrame.
 duplicate_df_f1f2 = f1f2[f1f2.duplicated(['Value', 'CDID', 'Analysis', 'Period', 'Capital Formation','Economic Concept'], keep = False)]
 # duplicate_df_f1f2.sort_values(by = ['Value']).to_csv("f1f2duplicates.csv")
 # duplicate_df_f1f2
-# -
 
 f1f2.drop_duplicates(subset = f1f2.columns.difference(['Value']), inplace = True)
 
@@ -1075,107 +1087,101 @@ f1f2.to_csv("gross_fixed_capitol-observations.csv", index = False)
 catalog_metadata = metadata.as_csvqb_catalog_metadata()
 catalog_metadata.to_json_file("gross_fixed_capitol-catalog-metadata.json")
 
-# # +
+# +
+g1 = tidied_sheets[13]
 
-# g1 = tidied_sheets[13]
+try:
+    g1 = g1.loc[g1['Percentage Change'].isna()] 
+except:
+    ind = 13 
 
-# try:
-#     g1 = g1.loc[g1['Percentage Change'].isna()] 
-# except:
-#     ind = 13 
+g1 = strip_superscripts(g1, 'Sector')
 
-# g1 = strip_superscripts(g1, 'Sector')
-
-# g1['Sector'].loc[g1['CDID'].isin(['FAGF','FADY','FALR','NZTP'])] = 'Manufacturing - ' + g1['Sector'].loc[g1['CDID'].isin(['FAGF','FADY','FALR','NZTP'])].astype(str)
-# g1['Sector'].loc[g1['CDID'].isin(['FBHE','FAHA'])] = 'Distributive trades - ' + g1['Sector'].loc[g1['CDID'].isin(['FBHE','FAHA'])].astype(str)
-# g1['Sector'].loc[g1['CDID'].isin(['CAEX'])] = 'Total' 
-
-
-# g1['Economic Concept'] = 'current-price'
-
-# g1 = prefix_refperiod(g1, 'Period')
-
-# try:
-#     g1.drop(['Seasonal Adjustment','Percentage Change','measure'], axis=1, inplace=True)
-# except:
-#     ind = 13   
-
-# g1 = g1.rename(columns={'OBS':'Value'})
-
-# g1 = convet_dimension_to_int(g1, 'Value')
-# g1['Level of inventories held at end-December 2019'] = g1['Level of inventories held at end-December 2019'].str.replace('.0','')
-
-# g1['Sector'] = g1['Sector'].apply(pathify)
-# g1['Industry'] = g1['Industry'].apply(pathify)
-
-# g1cdids = g1['CDID'].unique()
-# g1.head(5)
-
-# # +
-
-# g2 = tidied_sheets[14]
-
-# try:
-#     g2 = g2.loc[g2['Percentage Change'].isna()] 
-# except:
-#     ind = 14
-
-# g2 = strip_superscripts(g2, 'Sector')
-
-# g2['Sector'].loc[g2['CDID'].isin(['FBNF','FBNG','FBNH','DHBM'])] = 'Manufacturing - ' + g2['Sector'].loc[g2['CDID'].isin(['FBNF','FBNG','FBNH','DHBM'])].astype(str)
-# g2['Sector'].loc[g2['CDID'].isin(['FAJX','FBYN'])] = 'Distributive trades - ' + g2['Sector'].loc[g2['CDID'].isin(['FAJX','FBYN'])].astype(str)
-# g2['Sector'].loc[g2['CDID'].isin(['CAFU'])] = 'Total' 
+g1['Sector'].loc[g1['CDID'].isin(['FAGF','FADY','FALR','NZTP'])] = 'Manufacturing - ' + g1['Sector'].loc[g1['CDID'].isin(['FAGF','FADY','FALR','NZTP'])].astype(str)
+g1['Sector'].loc[g1['CDID'].isin(['FBHE','FAHA'])] = 'Distributive trades - ' + g1['Sector'].loc[g1['CDID'].isin(['FBHE','FAHA'])].astype(str)
+g1['Sector'].loc[g1['CDID'].isin(['CAEX'])] = 'Total' 
 
 
-# g2['Economic Concept'] = 'chained-volume-measure'
+g1['Economic Concept'] = 'current-price'
 
-# g2 = prefix_refperiod(g2, 'Period')
+g1 = prefix_refperiod(g1, 'Period')
 
-# try:
-#     g2.drop(['Seasonal Adjustment','Percentage Change','measure'], axis=1, inplace=True)
-# except:
-#     ind = 14  
+try:
+    g1.drop(['Seasonal Adjustment','Percentage Change','measure'], axis=1, inplace=True)
+except:
+    ind = 13   
 
-# g2 = g2.rename(columns={'OBS':'Value'})
+g1 = g1.rename(columns={'OBS':'Value'})
 
-# g2 = convet_dimension_to_int(g2, 'Value')
-# g2['Level of inventories held at end-December 2019'] = g2['Level of inventories held at end-December 2019'].str.replace('.0','')
+g1 = convet_dimension_to_int(g1, 'Value')
+g1['Level of inventories held at end-December 2019'] = g1['Level of inventories held at end-December 2019'].str.replace('.0','')
 
-# g2['Sector'] = g2['Sector'].apply(pathify)
-# g2['Industry'] = g2['Industry'].apply(pathify)
+g1['Sector'] = g1['Sector'].apply(pathify)
+g1['Industry'] = g1['Industry'].apply(pathify)
 
-# g2cdids = g2['CDID'].unique()
-# g2.head(5)
-# # -
+g1cdids = g1['CDID'].unique()
+g1.head(5)
 
-# g1g2 = pd.concat([g1,g2])
-# del g1g2['Industry']
 
-# # +
+g2 = tidied_sheets[14]
 
-# metadata.dataset.title = mainTitle + ' - Change in inventories at current prices and chained volume measures (G1, G2)'
-# metadata.dataset.comment = maincomme + ' - Change in inventories at current prices and chained volume measures (G1, G2) - Seasonally Adjusted'
-# metadata.dataset.description = maindescr + """
-# Change in inventories at current prices and chained volume measures (G1, G2)
-# Data has been seasonally adjusted
-# Estimates are given to the nearest £ million but cannot be regarded as accurate to this degree.
-# Wholesaling and retailing estimates exclude the motor trades.
-# Total change in inventories: Quarterly alignment adjustment included in this series.
-# Please note, inventories estimates for 2020 Q4 at this stage face additional uncertainty due to the lower data content. 
-# """
-# # -
+try:
+    g2 = g2.loc[g2['Percentage Change'].isna()] 
+except:
+    ind = 14
 
-# g1g2.columns
+g2 = strip_superscripts(g2, 'Sector')
 
-# g1g2.head(5)
+g2['Sector'].loc[g2['CDID'].isin(['FBNF','FBNG','FBNH','DHBM'])] = 'Manufacturing - ' + g2['Sector'].loc[g2['CDID'].isin(['FBNF','FBNG','FBNH','DHBM'])].astype(str)
+g2['Sector'].loc[g2['CDID'].isin(['FAJX','FBYN'])] = 'Distributive trades - ' + g2['Sector'].loc[g2['CDID'].isin(['FAJX','FBYN'])].astype(str)
+g2['Sector'].loc[g2['CDID'].isin(['CAFU'])] = 'Total' 
 
-# # +
 
-# g1g2.to_csv("inventories-observations.csv", index = False)
-# catalog_metadata = metadata.as_csvqb_catalog_metadata()
-# catalog_metadata.to_json_file("inventories-catalog-metadata.json")
+g2['Economic Concept'] = 'chained-volume-measure'
 
-# # +
+g2 = prefix_refperiod(g2, 'Period')
+
+try:
+    g2.drop(['Seasonal Adjustment','Percentage Change','measure'], axis=1, inplace=True)
+except:
+    ind = 14  
+
+g2 = g2.rename(columns={'OBS':'Value'})
+
+g2 = convet_dimension_to_int(g2, 'Value')
+g2['Level of inventories held at end-December 2019'] = g2['Level of inventories held at end-December 2019'].str.replace('.0','')
+
+g2['Sector'] = g2['Sector'].apply(pathify)
+g2['Industry'] = g2['Industry'].apply(pathify)
+
+g2cdids = g2['CDID'].unique()
+g2.head(5)
+
+g1g2 = pd.concat([g1,g2])
+del g1g2['Industry']
+
+
+metadata.dataset.title = mainTitle + ' - Change in inventories at current prices and chained volume measures (G1, G2)'
+metadata.dataset.comment = maincomme + ' - Change in inventories at current prices and chained volume measures (G1, G2) - Seasonally Adjusted'
+metadata.dataset.description = maindescr + """
+Change in inventories at current prices and chained volume measures (G1, G2)
+Data has been seasonally adjusted
+Estimates are given to the nearest £ million but cannot be regarded as accurate to this degree.
+Wholesaling and retailing estimates exclude the motor trades.
+Total change in inventories: Quarterly alignment adjustment included in this series.
+Please note, inventories estimates for 2020 Q4 at this stage face additional uncertainty due to the lower data content. 
+"""
+
+g1g2.columns
+
+g1g2.head(5)
+# -
+
+g1g2.to_csv("inventories-observations.csv", index = False)
+catalog_metadata = metadata.as_csvqb_catalog_metadata()
+catalog_metadata.to_json_file("inventories-catalog-metadata.json")
+
+# +
 
 # h1 = tidied_sheets[15]
 
@@ -1285,4 +1291,6 @@ catalog_metadata.to_json_file("gross_fixed_capitol-catalog-metadata.json")
 # #fldrpth = '/users/leigh/Development/family-trade/reference/codelists/'
 # #dm.search_for_codes_using_levenshtein_and_fuzzywuzzy(tidied_sheets[ind]['Sector'].unique(), fldrpth, 'Notation', 'sector', 3, 0.8)
 # #dm.search_codelists_for_codes(d1['Category of Income'].unique(), fldrpth, 'Notation', 'Category of Income')
-# %%
+# -
+
+
